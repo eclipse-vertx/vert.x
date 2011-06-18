c8b2575e64d208dafcc95b1b3bf2eea28429b3a8
purplefox <timvolpe@gmail.com>
2011-06-18 20:02:45 +0100

    make example into proper script with classpath

--- src/examples/ruby/echo_server.rb
+++ src/examples/ruby/echo_server.rb
@@ -1,13 +1,8 @@
+#!/usr/bin/env CLASSPATH=$CLASSPATH:../../../target/node.x.jar:../../core/resources/jars/netty-3.2.4.Final.jar jruby echo_server.rb
+
 require "../../api/ruby/net"
 
-puts "node.x echo server"
-
-server = Net.create_server{|socket| socket.data{ |data|
-                                            puts "received data #{data}"
-                                            socket.write(data)
-                                            }
-                          }.listen(8080, "127.0.0.1")
-
+server = Net.create_server{|socket| socket.data{|data| socket.write(data)}}.listen(8080, "127.0.0.1")
 
 #Prevent script from exiting
 STDIN.gets                      
--- src/examples/ruby/echo_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export CLASSPATH=$CLASSPATH:../../../target/node.x.jar:../../core/resources/jars/netty-3.2.4.Final.jar
-
-jruby echo_server.rb
