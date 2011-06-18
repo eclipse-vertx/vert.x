dd3c75fc19c7f4859faf64de5bc53e45c747c0d4
purplefox <timvolpe@gmail.com>
2011-06-18 20:07:03 +0100

    updated example

--- src/examples/ruby/echo_server.rb
+++ src/examples/ruby/echo_server.rb
@@ -2,7 +2,7 @@
 
 require "../../api/ruby/net"
 
-server = Net.create_server{|socket| socket.data{|data| socket.write(data)}}.listen(8080, "127.0.0.1")
+Net.create_server{|socket| socket.data{|data| socket.write(data)}}.listen(8080, "127.0.0.1")
 
 #Prevent script from exiting
 STDIN.gets                      
