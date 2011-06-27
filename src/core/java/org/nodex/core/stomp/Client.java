34e886b51b2affd73fec20ab1667a4e784ce328f
purplefox <timvolpe@gmail.com>
2011-06-27 22:56:00 +0100

    stomp client

--- src/core/java/org/nodex/core/stomp/Client.java
+++ src/core/java/org/nodex/core/stomp/Client.java
@@ -1,9 +1,16 @@
 package org.nodex.core.stomp;
 
+import org.nodex.core.Callback;
+import org.nodex.core.net.Socket;
+
 public class Client {
-
-
-
+  public static void connect(int port, String host, final Callback<Connection> connectCallback) {
+    org.nodex.core.net.Client.connect(port, host, new Callback<Socket>() {
+      public void onEvent(Socket sock) {
+        connectCallback.onEvent(new Connection(sock));
+      }
+    });
+  }
 }
 
 
