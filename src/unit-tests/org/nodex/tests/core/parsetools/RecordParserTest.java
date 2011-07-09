6dfece68d6fd57411b9e35ba4ab95c0e13d5118a
purplefox <timvolpe@gmail.com>
2011-07-09 10:36:19 +0100

    record parser test

--- src/unit-tests/org/nodex/tests/core/parsetools/RecordParserTest.java
+++ src/unit-tests/org/nodex/tests/core/parsetools/RecordParserTest.java
@@ -7,6 +7,9 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 /**
@@ -27,38 +30,65 @@ public class RecordParserTest {
   }
 
   @Test
-  public void delimitedSingleChar() {
-    String line = "This is a line";
-    //doTestDelimited(line, "\n", "UTF-8", 1, line);
-    doTestDelimited(line, "\n", "UTF-8", line.length() / 2, line);
-    //doTestDelimited(line, "\n", "UTF-8", line.length(), line);
-    //doTestDelimited(line, "\n", "UTF-8", line.length() - 1, line);
-    //doTestDelimited(line, "\n", "UTF-8", line.length() + 1, line);
+  public void delimited() {
+    delimited("\n");
   }
 
-  private void doTestDelimited(final String input, String delim, final String enc, int chunkSize, final String... output) {
-    final String[] results = new String[output.length];
+  private void delimited(String delim) {
+    int lines = 100;
+    String[] expected = new String[lines];
+    StringBuilder inp = new StringBuilder();
+
+    //We create lines of length zero to <lines> and shuffle them
+    List<String> lineList = new ArrayList<String>();
+    for (int i = 0; i < lines; i++) {
+      StringBuilder line = new StringBuilder();
+      for (int j = 0; j < i; j++) {
+        line.append('X'); //FIXME this should be random
+      }
+      lineList.add(line.toString());
+    }
+    Collections.shuffle(lineList);
+    expected = lineList.toArray(expected);
+    for (int i = 0; i < lines; i++) {
+      inp.append(expected[i]).append(delim);
+    }
+
+    String sinp = inp.toString();
+
+    //We then try every combination of chunk size up to twice the input string length, again in a random order
+    List<Integer> chunkSizes = new ArrayList<Integer>();
+    for (int i = 1; i < sinp.length() * 2; i++) {
+      chunkSizes.add(i);
+    }
+    Collections.shuffle(chunkSizes);
+    for (Integer i: chunkSizes) {
+      doTestDelimited(sinp, "\n", "UTF-8", i, expected);
+    }
+  }
+
+  private void doTestDelimited(final String input, String delim, final String enc, int chunkSize, final String... expected) {
+    final String[] results = new String[expected.length];
     DataHandler out = new DataHandler() {
+      int pos;
       public void onData(Buffer buff) {
-        int pos = 0;
         String str = buff.toString(enc);
         results[pos++] = str;
       }
     };
     RecordParser parser = RecordParser.newDelimited(delim, enc, out);
-    String inpLine = input + delim;
     int pos = 0;
-    while (pos < inpLine.length()) {
-      int end = pos + chunkSize -1;
-      end = end < inpLine.length() ? end : inpLine.length() - 1;
-      String sub = inpLine.substring(pos, end);
-      System.out.println("sub is:" + sub + "END");
+    while (pos < input.length()) {
+      int end = pos + chunkSize;
+      end = end <= input.length() ? end : input.length();
+      String sub = input.substring(pos, end);
+      //System.out.println("sub is:" + sub + "END");
       parser.onData(Buffer.fromString(sub));
       pos += chunkSize;
     }
 
-    for (int i = 0; i < output.length; i++) {
-      assert output[i].equals(results[i]);
+    for (int i = 0; i < expected.length; i++) {
+      assert expected[i].equals(results[i]) : "Expected:" + expected[i] + " length:" + expected[i].length() + "\nActual:" + results[i];
     }
   }
 
