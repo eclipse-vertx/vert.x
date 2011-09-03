bce754c4479853f099354a776911ac04afdfe56b
purplefox <timvolpe@gmail.com>
2011-09-03 10:12:30 +0100

    ruby sendfile example

---
+++ src/examples/ruby/sendfile/sendfile.rb
@@ -0,0 +1,24 @@
+# Copyright 2002-2011 the original author or authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+# this file except in compliance with the License. You may obtain a copy of the
+# License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+require "core/http"
+require "core/nodex"
+include Http
+
+Nodex::go{
+  Server.new.request_handler { |req|
+    filename = req.uri == "/" ? "index.html" : "." << req.uri
+    req.response.send_file(filename)
+  }.listen(8080)
+}
+
+puts "hit enter to exit"
+STDIN.gets
