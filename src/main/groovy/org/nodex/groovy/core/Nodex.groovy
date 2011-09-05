995edfdde6faa312cbd53f0610e547a576abe565
Tim Fox <tim@rabbitmq.com>
2011-09-05 16:12:11 +0100

    tweak to groovy example

--- src/main/groovy/org/nodex/groovy/core/Nodex.groovy
+++ src/main/groovy/org/nodex/groovy/core/Nodex.groovy
@@ -1,12 +1,10 @@
 package org.nodex.groovy.core
 
-import org.nodex.core.Nodex
-
 class Nodex {
 
-  static org.nodex.core.Nodex j_instance = org.nodex.core.Nodex.instance
+  static def j_instance = org.nodex.core.Nodex.instance
 
-  static def go(Closure closure) {
+  static def go(closure) {
     j_instance.go(new java.lang.Runnable() {
       public void run() {
         closure.call()
