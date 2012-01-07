87f822e30311817a4722f428e40bf87ab116b8ae
purplefox <timvolpe@gmail.com>
2012-01-07 21:51:33 +0000

    wip

---
+++ src/newtests/java/org/vertx/tests/RubyNetTest.java
@@ -0,0 +1,34 @@
+package org.vertx.tests;
+
+import org.junit.Test;
+import org.vertx.java.core.app.AppType;
+import org.vertx.java.core.logging.Logger;
+import org.vertx.java.newtests.TestBase;
+
+/**
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+public class RubyNetTest extends TestBase {
+
+  private static final Logger log = Logger.getLogger(RubyNetTest.class);
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    startApp(AppType.RUBY, "test_client.rb");
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    super.tearDown();
+  }
+
+  @Test
+  public void test1() throws Exception {
+    startApp(AppType.RUBY, "echo_server.rb");
+    startTest();
+    log.info("*****test1 complete");
+  }
+
+
+}
