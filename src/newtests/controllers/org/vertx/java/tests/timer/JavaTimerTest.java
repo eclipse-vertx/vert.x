b3f2a4325d4f0080e16682ab4d9d7d5216c4eabd
Purple Fox <timvolpe@gmail.com>
2012-01-11 12:33:09 +0000

    timer test

---
+++ src/newtests/controllers/org/vertx/java/tests/timer/JavaTimerTest.java
@@ -0,0 +1,42 @@
+package org.vertx.java.tests.timer;
+
+import org.junit.Test;
+import org.vertx.java.core.app.AppType;
+import org.vertx.java.core.logging.Logger;
+import org.vertx.java.newtests.TestBase;
+import vertx.tests.timer.TestClient;
+
+/**
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+public class JavaTimerTest extends TestBase {
+
+private static final Logger log = Logger.getLogger(JavaTimerTest.class);
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    startApp(AppType.JAVA, TestClient.class.getName());
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    super.tearDown();
+  }
+
+  @Test
+  public void testOneOff() throws Exception {
+    startTest(getMethodName());
+  }
+
+  @Test
+  public void testPeriodic() throws Exception {
+    startTest(getMethodName());
+  }
+
+  @Test
+  public void testTimings() throws Exception {
+    startTest(getMethodName());
+  }
+
+}
