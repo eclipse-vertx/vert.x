8f3230cbb61c48a08a91cdbc35cf6ac65e5f44f4
Purple Fox <timvolpe@gmail.com>
2012-01-11 13:16:44 +0000

    mainly file system context tests

---
+++ src/newtests/java/vertx/tests/globalhandlers/TestClient.java
@@ -0,0 +1,23 @@
+package vertx.tests.globalhandlers;
+
+import org.vertx.java.newtests.TestClientBase;
+
+
+/**
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+public class TestClient extends TestClientBase {
+
+  @Override
+  public void start() {
+    super.start();
+    tu.appReady();
+  }
+
+  @Override
+  public void stop() {
+    super.stop();
+  }
+
+
+}
