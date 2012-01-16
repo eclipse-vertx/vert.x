4d0f949f8237ae0ae205d4647940d070f3a7aedf
Purple Fox <timvolpe@gmail.com>
2012-01-16 13:46:42 +0000

    tests

---
+++ src/newtests/controllers/org/vertx/java/tests/globalhandlers/RubyGlobalHandlersTest.java
@@ -0,0 +1,25 @@
+package org.vertx.java.tests.globalhandlers;
+
+import org.vertx.java.core.app.AppType;
+import org.vertx.java.newtests.TestBase;
+
+/**
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+public class RubyGlobalHandlersTest extends TestBase {
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    startApp(AppType.RUBY, "globalhandlers/test_client.rb");
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    super.tearDown();
+  }
+
+  public void test_global_handlers() {
+    startTest(getMethodName());
+  }
+}
---
+++ src/newtests/controllers/org/vertx/java/tests/shareddata/RubySharedDataTest.java
@@ -0,0 +1,29 @@
+package org.vertx.java.tests.shareddata;
+
+import org.vertx.java.core.app.AppType;
+import org.vertx.java.newtests.TestBase;
+
+/**
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+public class RubySharedDataTest extends TestBase {
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    startApp(AppType.RUBY, "shareddata/test_client.rb");
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    super.tearDown();
+  }
+
+  public void test_hash() {
+    startTest(getMethodName());
+  }
+
+  public void test_set() {
+    startTest(getMethodName());
+  }
+}
