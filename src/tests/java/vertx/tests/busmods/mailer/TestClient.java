066d9cf2cd1be80ac0868936cad94ab61cc7b7ca
Purple Fox <timvolpe@gmail.com>
2012-01-19 18:25:45 +0000

    more mailer work

--- src/tests/java/vertx/tests/busmods/mailer/TestClient.java
+++ src/tests/java/vertx/tests/busmods/mailer/TestClient.java
@@ -27,80 +27,123 @@ public class TestClient extends TestClientBase {
     super.stop();
   }
 
-  public void testSimple() throws Exception {
-
+  public void testSendMultiple() throws Exception {
     final int numMails = 10;
-
     Handler<Message> replyHandler = new Handler<Message>() {
       int count;
       public void handle(Message message) {
         tu.checkContext();
         Map<String, Object> json = sender.toJson(message);
         tu.azzert(json.get("status").equals("ok"));
-
         if (++count == numMails) {
           tu.testComplete();
         }
       }
     };
-
     String user = System.getProperty("user.name");
     for (int i = 0; i < numMails; i++) {
-      Map<String, Object> map = new HashMap<>();
-      map.put("address", "testMailer");
-      map.put("from", user + "@localhost");
-      map.put("to", user + "@localhost");
-      map.put("subject", "This is a test");
-      map.put("body", "This is the body\nof the mail");
+      Map<String, Object> map = createBaseMessage();
       sender.sendJSON(map, replyHandler);
     }
   }
 
-  public void testInvalidFrom() throws Exception {
+  public void testSendWithSingleRecipient() throws Exception {
+    Map<String, Object> map = new HashMap<>();
+    String rec = System.getProperty("user.name") + "@localhost";
+    map.put("to", rec);
+    send(map, null);
+  }
 
+  public void testSendWithRecipientList() throws Exception {
+    Map<String, Object> map = new HashMap<>();
+    String rec = System.getProperty("user.name") + "@localhost";
+    String[] recipients = new String[] { rec, rec, rec };
+    map.put("to", recipients);
+    send(map, null);
+  }
+
+  public void testSendWithSingleCC() throws Exception {
+    Map<String, Object> map = new HashMap<>();
+    String rec = System.getProperty("user.name") + "@localhost";
+    map.put("to", rec);
+    map.put("cc", rec);
+    send(map, null);
+  }
+
+  public void testSendWithCCList() throws Exception {
+    Map<String, Object> map = new HashMap<>();
+    String rec = System.getProperty("user.name") + "@localhost";
+    String[] recipients = new String[] { rec, rec, rec };
+    map.put("cc", recipients);
+    send(map, null);
+  }
+
+  public void testSendWithSingleBCC() throws Exception {
+    Map<String, Object> map = new HashMap<>();
+    String rec = System.getProperty("user.name") + "@localhost";
+    map.put("to", rec);
+    map.put("bcc", rec);
+    send(map, null);
+  }
+
+  public void testSendWithBCCList() throws Exception {
+    Map<String, Object> map = new HashMap<>();
+    String rec = System.getProperty("user.name") + "@localhost";
+    String[] recipients = new String[] { rec, rec, rec };
+    map.put("bcc", recipients);
+    send(map, null);
+  }
+
+  public void testInvalidSingleFrom() throws Exception {
+    Map<String, Object> map = new HashMap<>();
+    map.put("from", "wqdqwd qwdqwd qwdqwd ");
+    send(map, "Invalid from");
+  }
+
+  public void testInvalidSingleRecipient() throws Exception {
+    Map<String, Object> map = new HashMap<>();
+    map.put("to", "wqdqwd qwdqwd qwdqwd ");
+    send(map, "Invalid to");
+  }
+
+  public void testInvalidRecipientList() throws Exception {
+    Map<String, Object> map = new HashMap<>();
+    String[] recipients = new String[] { "tim@localhost", "qwdqwd qwdqw d", "qwdkiwqdqwd d" };
+    map.put("to", recipients);
+    send(map, "Invalid to");
+  }
+
+  private void send(Map<String, Object> overrides, final String error) throws Exception {
     Handler<Message> replyHandler = new Handler<Message>() {
       int count;
       public void handle(Message message) {
         tu.checkContext();
         Map<String, Object> json = sender.toJson(message);
-        tu.azzert(json.get("status").equals("error"));
-        tu.azzert(((String) json.get("message")).startsWith("Invalid from address"));
+        if (error == null) {
+          tu.azzert(json.get("status").equals("ok"));
+        } else {
+          tu.azzert(json.get("status").equals("error"));
+          tu.azzert(((String) json.get("message")).startsWith(error));
+        }
         tu.testComplete();
       }
     };
+    Map<String, Object> map = createBaseMessage();
+    map.putAll(overrides);
+    sender.sendJSON(map, replyHandler);
+  }
 
+  private Map<String, Object> createBaseMessage() {
     String user = System.getProperty("user.name");
     Map<String, Object> map = new HashMap<>();
     map.put("address", "testMailer");
-    map.put("from", " dwqd qdw wdq d d");
+    map.put("from", user + "@localhost");
     map.put("to", user + "@localhost");
     map.put("subject", "This is a test");
     map.put("body", "This is the body\nof the mail");
-    sender.sendJSON(map, replyHandler);
+    return map;
   }
 
-  public void testInvalidTo() throws Exception {
-
-    Handler<Message> replyHandler = new Handler<Message>() {
-      int count;
-      public void handle(Message message) {
-        tu.checkContext();
-        Map<String, Object> json = sender.toJson(message);
-        tu.azzert(json.get("status").equals("error"));
-        tu.azzert(((String)json.get("message")).startsWith("Invalid recipients"));
-        tu.testComplete();
-      }
-    };
-
-    String user = System.getProperty("user.name");
-    Map<String, Object> map = new HashMap<>();
-    map.put("address", "testMailer");
-    map.put("to", " dwqd qdw wdq d d");
-    map.put("from", user + "@localhost");
-    map.put("subject", "This is a test");
-    map.put("body", "This is the body\nof the mail");
-    sender.sendJSON(map, replyHandler);
-  }
 
 
 
