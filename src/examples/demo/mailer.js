a14d60b7a68d7329caba881eff582290b4e19d90
purplefox <timvolpe@gmail.com>
2012-01-25 22:39:24 +0000

    more on demo

--- src/examples/demo/mailer.js
+++ src/examples/demo/mailer.js
@@ -1,12 +1,12 @@
 load('vertx.js');
 
-//var mailer = new vertx.Mailer("demo.mailer", "localhost");
+var mailer = new vertx.Mailer("demo.mailer", "localhost");
 
 // To use a gmail account, uncomment the following and substitute your username and password
 // var mailer = new vertx.Mailer('demo.mailer', 'smtp.googlemail.com', 465, true, true, 'username', 'password');
 
-//mailer.start();
+mailer.start();
 
 function vertxStop() {
-  //mailer.stop();
+  mailer.stop();
 }
