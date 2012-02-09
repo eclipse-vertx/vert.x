6171e813dbf7108607245f87b4becbeaf9ab60c5
Purple Fox <timvolpe@gmail.com>
2012-02-09 10:56:18 +0000

    some work on deployment

--- src/main/java/org/vertx/java/core/app/VerticleManager.java
+++ src/main/java/org/vertx/java/core/app/VerticleManager.java
@@ -34,20 +34,22 @@ public class VerticleManager {
 
   public static VerticleManager instance = new VerticleManager();
 
-  private Map<String, AppMetaData> appMeta = new HashMap<>();
-  private Map<String, List<VerticleHolder>> apps = new HashMap();
-  private CountDownLatch stopLatch = new CountDownLatch(1);
-  private final Map<Long, AppConfig> configMap = new ConcurrentHashMap<>();
+  private Map<String, Deployment> deployments = new HashMap();
+  private final Map<Long, String> contextMap = new ConcurrentHashMap<>();
 
-  private static class AppConfig {
-    final String appName;
+  private CountDownLatch stopLatch = new CountDownLatch(1);
+
+  private static class Deployment {
     final JsonObject config;
     final String path;
+    final List<VerticleHolder> verticles = new ArrayList<>();
+    final List<String> childDeployments = new ArrayList<>();
+    final String parentDeploymentName;
 
-    private AppConfig(String appName, JsonObject config, String path) {
-      this.appName = appName;
+    private Deployment(JsonObject config, String path, String parentDeploymentName) {
       this.config = config;
       this.path = path;
+      this.parentDeploymentName = parentDeploymentName;
     }
   }
 
@@ -70,34 +72,44 @@ public class VerticleManager {
   }
 
   public JsonObject getConfig() {
-    AppConfig conf = configMap.get(Vertx.instance.getContextID());
-    return conf == null ? null : conf.config;
+    String deploymentName = getDeploymentName();
+    if (deploymentName != null) {
+      Deployment deployment = deployments.get(deploymentName);
+      if (deployment != null) {
+        return deployment.config;
+      }
+    }
+    return null;
   }
 
-  public String getAppName() {
-    AppConfig conf = configMap.get(Vertx.instance.getContextID());
-    return conf == null ? null : conf.appName;
+  public String getDeploymentName() {
+    Long contextID = Vertx.instance.getContextID();
+    return contextID == null ? null : contextMap.get(contextID);
   }
 
-  public String getAppPath() {
-    AppConfig conf = configMap.get(Vertx.instance.getContextID());
-    return conf == null ? null : conf.path;
+  public String getDeploymentPath() {
+    String deploymentName = getDeploymentName();
+    if (deploymentName != null) {
+      Deployment deployment = deployments.get(deploymentName);
+      if (deployment != null) {
+        return deployment.path;
+      }
+    }
+    return null;
   }
 
-  public String deploy(boolean worker, String name, final String main, final JsonObject config, final String thePath,
-                       int instances,
-                       final Handler<Void> doneHandler)
+  public synchronized String deploy(boolean worker, String name, final String main,
+                                    final JsonObject config, final String thePath,
+                                    int instances,
+                                    final Handler<Void> doneHandler)
   {
-    if (appMeta.containsKey(name)) {
+    if (deployments.containsKey(name)) {
       throw new IllegalStateException("There is already a deployment with name: " + name);
     }
 
-    log.debug("Deploying name : " + name  + " main: " + main +
-             " instances: " + instances);
-
     final String path = thePath == null ? "." : thePath;
 
-    //Infer the app type
+    //Infer the main type
 
     VerticleType type = VerticleType.JAVA;
     if (main.endsWith(".js")) {
@@ -134,21 +146,24 @@ public class VerticleManager {
       urls[index++] = url;
     }
 
-    final String appName = name == null ?  "app-" + UUID.randomUUID().toString() : name;
+    final String deploymentName = name == null ?  "deployment-" + UUID.randomUUID().toString() : name;
 
-    final VerticleFactory appFactory;
+    log.debug("Deploying name : " + deploymentName  + " main: " + main +
+                 " instances: " + instances);
+
+    final VerticleFactory verticleFactory;
       switch (type) {
         case JAVA:
-          appFactory = new JavaVerticleFactory();
+          verticleFactory = new JavaVerticleFactory();
           break;
         case RUBY:
-          appFactory = new JRubyVerticleFactory();
+          verticleFactory = new JRubyVerticleFactory();
           break;
         case JS:
-          appFactory = new RhinoVerticleFactory();
+          verticleFactory = new RhinoVerticleFactory();
           break;
         case GROOVY:
-          appFactory = new GroovyVerticleFactory();
+          verticleFactory = new GroovyVerticleFactory();
           break;
         default:
           throw new IllegalArgumentException("Unsupported type: " + type);
@@ -161,7 +176,6 @@ public class VerticleManager {
 
       void started() {
         if (count.incrementAndGet() == instCount) {
-          log.debug("Started " + instCount + " instances ok");
           if (doneHandler != null) {
             doneHandler.handle(null);
           }
@@ -171,32 +185,37 @@ public class VerticleManager {
 
     final AggHandler aggHandler = new AggHandler();
 
+    String parentDeploymentName = getDeploymentName();
+    Deployment deployment = new Deployment(config == null ? null : config.copy(), path, parentDeploymentName);
+    deployments.put(deploymentName, deployment);
+    if (parentDeploymentName != null) {
+      Deployment parent = deployments.get(parentDeploymentName);
+      parent.childDeployments.add(deploymentName);
+    }
+
     for (int i = 0; i < instances; i++) {
 
-      // Launch the app instance
+      // Launch the verticle instance
 
       Runnable runner = new Runnable() {
         public void run() {
 
-          Verticle app;
+          Verticle verticle;
           try {
-            app = appFactory.createVerticle(main, new ParentLastURLClassLoader(urls, getClass()
+            verticle = verticleFactory.createVerticle(main, new ParentLastURLClassLoader(urls, getClass()
                 .getClassLoader()));
           } catch (Throwable t) {
             log.error("Failed to create verticle", t);
-            internalUndeploy(appName, doneHandler);
+            internalUndeploy(deploymentName, doneHandler);
             return;
           }
 
-          configMap.put(Vertx.instance.getContextID(),
-              new AppConfig(appName, config == null ? null : config.copy(), path));
-
           try {
-            addApp(appName, app);
-            app.start();
+            addVerticle(deploymentName, verticle);
+            verticle.start();
           } catch (Throwable t) {
             log.error("Unhandled exception in verticle start", t);
-            internalUndeploy(appName, doneHandler);
+            internalUndeploy(deploymentName, doneHandler);
           }
           aggHandler.started();
         }
@@ -209,9 +228,8 @@ public class VerticleManager {
       }
 
     }
-    appMeta.put(appName, new AppMetaData(urls, main));
 
-    return appName;
+    return deploymentName;
   }
 
   private static class AggHandler extends SimpleHandler {
@@ -229,11 +247,11 @@ public class VerticleManager {
   }
 
   public synchronized void undeployAll(final Handler<Void> doneHandler) {
-    if (appMeta.isEmpty()) {
+    if (deployments.isEmpty()) {
       doneHandler.handle(null);
     } else {
-      AggHandler aggHandler = new AggHandler(appMeta.size(), doneHandler);
-      Set<String> names = new HashSet<>(appMeta.keySet()); // Avoid comod exception
+      AggHandler aggHandler = new AggHandler(deployments.size(), doneHandler);
+      Set<String> names = new HashSet<>(deployments.keySet()); // Avoid comod exception
       for (String name: names) {
         undeploy(name, aggHandler);
       }
@@ -241,18 +259,24 @@ public class VerticleManager {
   }
 
   public synchronized void undeploy(String name, final Handler<Void> doneHandler) {
-    if (appMeta.get(name) == null) {
+    if (deployments.get(name) == null) {
       throw new IllegalArgumentException("There is no deployment with name " + name);
     }
     internalUndeploy(name, doneHandler);
   }
 
   private void internalUndeploy(String name, final Handler<Void> doneHandler) {
-    List<VerticleHolder> list = apps.get(name);
-    if (list != null) {
-      final AggHandler aggHandler = doneHandler == null ? null : new AggHandler(list.size(), doneHandler);
+    Deployment deployment = deployments.remove(name);
 
-      for (final VerticleHolder holder: list) {
+    // Depth first - undeploy children first        TODO
+//    for (String childDeployment: deployment.childDeployments) {
+//      internalUndeploy(childDeployment, null); //TODO agg handler doesn't wait for all children
+//    }
+
+    if (!deployment.verticles.isEmpty()) {
+      final AggHandler aggHandler = doneHandler == null ? null : new AggHandler(deployment.verticles.size(), doneHandler);
+
+      for (final VerticleHolder holder: deployment.verticles) {
         VertxInternal.instance.executeOnContext(holder.contextID, new Runnable() {
           public void run() {
             VertxInternal.instance.setContextID(holder.contextID);
@@ -261,50 +285,42 @@ public class VerticleManager {
             } catch (Exception e) {
               log.error("Unhandled exception in verticle stop", e);
             }
-            //FIXME - we need to destroy the context, but not until after the app has fully stopped which may
-            //be asynchronous, e.g. if the app needs to close servers
+            //FIXME - we need to destroy the context, but not until after the deployment has fully stopped which may
+            //be asynchronous, e.g. if the deployment needs to close servers
             //VertxInternal.instance.destroyContext(holder.contextID);
             if (aggHandler != null) {
               aggHandler.handle(null);
             }
 
-            // Remove config
-            configMap.remove(holder.contextID);
+            // Remove context mapping
+            contextMap.remove(holder.contextID);
           }
         });
       }
     }
-    appMeta.remove(name);
-    apps.remove(name);
+
+    if (deployment.parentDeploymentName != null) {
+      Deployment parent = deployments.get(deployment.parentDeploymentName);
+      if (parent != null) {
+        parent.childDeployments.remove(name);
+      }
+    }
   }
 
 
   public synchronized Map<String, Integer> listInstances() {
     Map<String, Integer> map = new HashMap<>();
-    for (Map.Entry<String, List<VerticleHolder>> entry: apps.entrySet()) {
-      map.put(entry.getKey(), entry.getValue().size());
+    for (Map.Entry<String, Deployment> entry: deployments.entrySet()) {
+      map.put(entry.getKey(), entry.getValue().verticles.size());
     }
     return map;
   }
 
-  // Must be sychronized since called directly after app is deployed from different thread
-  private synchronized void addApp(String name, Verticle app) {
-    List<VerticleHolder> list = apps.get(name);
-    if (list == null) {
-      list = new ArrayList<>();
-      apps.put(name, list);
-    }
-    list.add(new VerticleHolder(Vertx.instance.getContextID(), app));
-  }
-
-  private static class AppMetaData {
-    final URL[] urls;
-    final String main;
-
-    private AppMetaData(URL[] urls, String mainClass) {
-      this.urls = urls;
-      this.main = mainClass;
-    }
+  // Must be synchronized since called directlyfrom different thread
+  private synchronized void addVerticle(String name, Verticle verticle) {
+    Deployment deployment = deployments.get(name);
+    deployment.verticles.add(new VerticleHolder(Vertx.instance.getContextID(), verticle));
+    contextMap.put(Vertx.instance.getContextID(), name);
   }
 
   private static class VerticleHolder {
