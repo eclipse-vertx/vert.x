5a57503c332d2866fc7186168da7d2520a747c6e
Purple Fox <timvolpe@gmail.com>
2012-02-14 11:39:41 +0000

    some tweaks to JS and clean up ignore files

--- src/examples/javascript/scratch/scratch.js
+++ src/examples/javascript/scratch/scratch.js
@@ -2,17 +2,3 @@ load('vertx.js');
 
 log.println("in scratch");
 
-foo();
-
-function foo() {
-  bar();
-}
-
-function bar() {
-  quux();
-}
-
-
-function quux() {
-  throw "foo"
-}
--- src/tests/resources/bar.txt
+++
@@ -1,1 +0,0 @@
-bar bar black sheep
