aa85cebd2f61ece3868474bdf475de841323bb9a
Purple Fox <timvolpe@gmail.com>
2012-02-16 09:17:39 +0000

    renamed cluster config

--- src/main/java/org/vertx/java/core/eventbus/spi/hazelcast/HazelcastClusterManager.java
+++ src/main/java/org/vertx/java/core/eventbus/spi/hazelcast/HazelcastClusterManager.java
@@ -1,10 +1,22 @@
 package org.vertx.java.core.eventbus.spi.hazelcast;
 
+import com.hazelcast.config.Config;
+import com.hazelcast.config.XmlConfigBuilder;
 import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
 import org.vertx.java.core.eventbus.spi.AsyncMultiMap;
 import org.vertx.java.core.eventbus.spi.ClusterManager;
+import org.vertx.java.core.logging.Logger;
+import org.vertx.java.core.logging.LoggerFactory;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 
 /**
@@ -12,24 +24,57 @@ import java.util.Set;
  */
 public class HazelcastClusterManager implements ClusterManager {
 
+  private static final Logger log = LoggerFactory.getLogger(HazelcastClusterManager.class);
+
+  private static HazelcastInstance instance;
+
+  private static synchronized HazelcastInstance getHazelcast() {
+    if (instance == null) {
+      System.setProperty("hazelcast.mancenter.enabled", "false");
+      System.setProperty("hazelcast.memcache.enabled", "false");
+      System.setProperty("hazelcast.rest.enabled", "false");
+      System.setProperty("hazelcast.wait.seconds.before.join", "0");
+      Config cfg;
+      InputStream is =
+          HazelcastClusterManager.class.getClassLoader().getResourceAsStream("cluster.xml");
+      if (is != null) {
+        InputStream bis = null;
+        try {
+          bis = new BufferedInputStream(is);
+          cfg = new XmlConfigBuilder(bis).build();
+        } finally {
+          try {
+            if (bis != null) bis.close();
+          } catch (Exception ignore) {
+          }
+        }
+      } else {
+        log.warn("Cannot find cluster.xml. Using default cluster configuration");
+        cfg = null;
+      }
+      //We use the default instance
+      instance = Hazelcast.init(cfg);
+    }
+    return instance;
+  }
+
+  private HazelcastInstance hazelcast;
+
   public HazelcastClusterManager() {
-    System.setProperty("hazelcast.mancenter.enabled", "false");
-    System.setProperty("hazelcast.memcache.enabled", "false");
-    System.setProperty("hazelcast.rest.enabled", "false");
-    System.setProperty("hazelcast.wait.seconds.before.join", "0");
+    hazelcast = getHazelcast();
   }
 
   public AsyncMultiMap getMultiMap(String name) {
-    com.hazelcast.core.MultiMap map = Hazelcast.getMultiMap(name);
+    com.hazelcast.core.MultiMap map = hazelcast.getMultiMap(name);
     return new HazelcastAsyncMultiMap(map);
   }
 
   public Set getSet(String name) {
-    return Hazelcast.getSet(name);
+    return hazelcast.getSet(name);
   }
 
   public Map getMap(String name) {
-    return Hazelcast.getMap(name);
+    return hazelcast.getMap(name);
   }
 
   public void close() {
