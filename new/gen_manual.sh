49b708c2c861c982d33fa150acab016cfc765b67
Purple Fox <timvolpe@gmail.com>
2012-02-28 11:27:28 +0000

    docs/website

--- new/gen_manual.sh
+++ new/gen_manual.sh
@@ -6,7 +6,9 @@
 ./gen_section.sh core_manual_ruby.md core_manual_ruby_templ.html web/core_manual_ruby.html
 ./gen_section.sh core_manual_java.md core_manual_java_templ.html web/core_manual_java.html
 ./gen_section.sh install.md install_manual_templ.html web/install.html
-./gen_section.sh web_tutorial.md web_tutorial_templ.html web/web_tutorial.html
+./gen_section.sh js_web_tutorial.md js_web_tutorial_templ.html web/js_web_tutorial.html
+./gen_section.sh ruby_web_tutorial.md ruby_web_tutorial_templ.html web/ruby_web_tutorial.html
+./gen_section.sh java_web_tutorial.md java_web_tutorial_templ.html web/java_web_tutorial.html
 
 
 
