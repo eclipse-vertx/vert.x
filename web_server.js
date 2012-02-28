dbdf89519f357265cf24259133bf8235603c28c4
Purple Fox <timvolpe@gmail.com>
2012-02-28 17:16:50 +0000

    new website

--- web_server.js
+++ web_server.js
@@ -5,9 +5,9 @@ var server = new vertx.HttpServer();
 // Also serve the static resources
 server.requestHandler(function(req) {
   if (req.path == '/') {
-    req.response.sendFile('web/index.html');
+    req.response.sendFile('index.html');
   } else if (req.path.indexOf('..') == -1) {
-    req.response.sendFile('web' + req.path);
+    req.response.sendFile("." + req.path);
   }
 }).listen(8181, 'localhost');
 
