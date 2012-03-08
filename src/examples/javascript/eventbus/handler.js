8578534b064a86d1f30f02261280815a5303d14b
Purple Fox <timvolpe@gmail.com>
2012-03-08 13:04:10 +0000

    tweak to example

--- src/examples/javascript/eventbus/handler.js
+++ src/examples/javascript/eventbus/handler.js
@@ -5,6 +5,8 @@ var eb = vertx.EventBus;
 var address = 'example.address'
 var creditsAddress = 'example.credits'
 
+var batchSize = 10000;
+
 var received = 0
 var count = 0
 
@@ -12,20 +14,20 @@ var start = null;
 
 var handler = function(message) {
   received++;
-  if (received == 1000) {
+  if (received == batchSize) {
     eb.send(creditsAddress, null);
     received = 0;
   }
   count++;
-  if (count % 10000 == 0) {
-    stdout.println("Received " + count);
+  if (count % batchSize == 0) {
+    // stdout.println("Received " + count);
     if (start == null) {
       start = new Date();
     } else {
       var now = new Date();
       var elapsed = now.getTime() - start.getTime();
       var rate = 1000 * (count + 0.0) / (elapsed);
-      //stdout.println("rate: " + rate + " msgs/sec");
+      stdout.println("rate: " + rate + " msgs/sec");
     }
   }
 }
