7d1d9fb26f5235678f2fce89dfe652625ca5794a
Purple Fox <timvolpe@gmail.com>
2012-04-10 16:50:55 +0100

    added groovy stuff

--- gen_site.sh
+++ gen_site.sh
@@ -5,6 +5,7 @@
 ./gen_section.sh core_manual_js.md core_manual_js_templ.html core_manual_js.html
 ./gen_section.sh core_manual_ruby.md core_manual_ruby_templ.html core_manual_ruby.html
 ./gen_section.sh core_manual_java.md core_manual_java_templ.html core_manual_java.html
+./gen_section.sh core_manual_groovy.md core_manual_groovy_templ.html core_manual_groovy.html
 ./gen_section.sh install.md install_manual_templ.html install.html
 ./gen_section.sh js_web_tutorial.md js_web_tutorial_templ.html js_web_tutorial.html
 ./gen_section.sh ruby_web_tutorial.md ruby_web_tutorial_templ.html ruby_web_tutorial.html
