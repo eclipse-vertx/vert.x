98624f0e0407df5ca3968a166115b97f29ec27c2
Mustafa <mpaltun@gmail.com>
2012-04-18 23:36:34 +0300

    set vertx.mods path on vertx.bat

--- src/scripts/vertx.bat
+++ src/scripts/vertx.bat
@@ -7,6 +7,7 @@ setlocal enabledelayedexpansion
 
 for %%? in ("%~dp0..") do set VERTX_HOME=%%~f?
 set VERTX_CP=%VERTX_HOME%\conf;
+set VERTX_MODS=%VERTX_HOME%\mods
 for %%a in ("%VERTX_HOME%\lib\jars\*.jar") do set VERTX_CP=!VERTX_CP!%%a;
 for /d %%a in ("%VERTX_HOME%\lib\*") do set VERTX_CP=!VERTX_CP!%%a;
 
