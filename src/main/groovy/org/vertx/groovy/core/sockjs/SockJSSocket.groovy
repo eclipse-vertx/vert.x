5af9d67cffe9bfcdcfbd30c7072a2ec35ec7aa3d
Purple Fox <timvolpe@gmail.com>
2012-04-25 18:00:47 +0100

    added writeHandlerID to groovy SockJSSocket

--- src/main/groovy/org/vertx/groovy/core/sockjs/SockJSSocket.groovy
+++ src/main/groovy/org/vertx/groovy/core/sockjs/SockJSSocket.groovy
@@ -63,5 +63,16 @@ abstract class SockJSSocket implements ReadStream, WriteStream {
     this
   }
 
+  /**
+   * When a {@code SockJSSocket} is created it automatically registers an event handler with the event bus, the ID of that
+   * handler is given by {@code writeHandlerID}.<p>
+   * Given this ID, a different event loop can send a buffer to that event handler using the event bus and
+   * that buffer will be received by this instance in its own event loop and written to the underlying socket. This
+   * allows you to write data to other sockets which are owned by different event loops.
+   */
+  String getWriteHandlerID() {
+    jSocket.writeHandlerID
+  }
+
 }
 
