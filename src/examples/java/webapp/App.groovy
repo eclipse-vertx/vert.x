d666854be575aadae10ed00a6b3e8a55d473da87
Purple Fox <timvolpe@gmail.com>
2012-04-27 10:30:45 +0100

    web app

--- src/examples/java/webapp/App.groovy
+++ src/examples/java/webapp/App.groovy
@@ -16,19 +16,33 @@
 
 package webapp
 
-// Our application config
+// Our application config - you can maintain it here or alternatively you could
+// stick it in a app.json text file and specify that when starting the verticle
 
+// Configuration for the persistor
 def persistorConf = [
-  address: 'demo.persistor',
-  db_name: 'test_db'
+  address: 'demo.persistor',  // The event bus address at which the persistor listens
+  db_name: 'test_db'          // The name of the MongoDB database
 ]
 
+// Configuration for the web server
 def webServerConf = [
+
+  // Normal web server stuff
+
   port: 8080,
   host: 'localhost',
   ssl: true,
+
+  // Configuration for the event bus client side bridge
+  // This bridges messages from the client side to the server side event bus
   bridge: true,
+
+  // Address of persistor - the user/password info is stored in the database
+  // so we need this so the bridge can handle logins
   persistor_address: 'demo.persistor',
+
+  // This defines which messages from the client we will let through
   permitted: [
     // Allow calls to get static album data from the persistor
     [
@@ -41,7 +55,7 @@ def webServerConf = [
     // And to place orders
     [
       'address' : 'demo.persistor',
-      'requires_auth' : true,
+      'requires_auth' : true,  // User must be logged in to send let these through
       'match' : [
         'action' : 'save',
         'collection' : 'orders'
@@ -50,15 +64,21 @@ def webServerConf = [
   ]
 ]
 
+// Now we deploy the modules that we need
+
 container.with {
 
-  // Deploy the busmods
+  // Deploy a MongoDB persistor module
 
   deployVerticle('mongo-persistor', persistorConf, 1, {
+
+    // And when it's deployed run a script to load it with some reference
+    // data for the demo
+
     deployVerticle('StaticData.groovy')
   })
 
-  // Start the web server
+  // Start the web server, with the config we defined above
 
   deployVerticle('web-server', webServerConf)
 
