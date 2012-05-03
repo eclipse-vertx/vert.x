f594aee69e128c6058a84a51e1c207c7331e9313
Purple Fox <timvolpe@gmail.com>
2012-05-03 07:33:38 +0100

    Don't include Groovy scripts in distro

--- src/scripts/vertx.bat
+++ src/scripts/vertx.bat
@@ -8,6 +8,6 @@ setlocal enabledelayedexpansion
 for %%? in ("%~dp0..") do set VERTX_HOME=%%~f?
 set VERTX_CP=%VERTX_HOME%\conf;
 for %%a in ("%VERTX_HOME%\lib\jars\*.jar") do set VERTX_CP=!VERTX_CP!%%a;
-for /d %%a in ("%VERTX_HOME%\lib\*") do set VERTX_CP=!VERTX_CP!%%a;
+set VERTX_CP=%VERTX_CP%;%VERTX_HOME%\lib\ruby;%VERTX_HOME%\lib\javascript;
 
 java -Djava.util.logging.config.file=%VERTX_HOME%\conf\logging.properties -Djruby.home=%JRUBY_HOME% -Dvertx.mods=%VERTX_MODS% -Dvertx.install=%VERTX_HOME% -cp %VERTX_CP% org.vertx.java.deploy.impl.cli.VertxMgr %*
