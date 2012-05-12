688ada4e0c346eb8893188bdc13040fa41765512
Pid <pid@pidster.com>
2012-05-12 12:46:00 +0100

    migrate shareddata to impl, extract interface

--- src/main/java/org/vertx/java/core/shareddata/SharedData.java
+++ src/main/java/org/vertx/java/core/shareddata/SharedData.java
@@ -13,92 +13,37 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.vertx.java.core.shareddata;
 
-import org.vertx.java.core.logging.Logger;
-import org.vertx.java.core.logging.impl.LoggerFactory;
-import org.vertx.java.core.shareddata.impl.SharedMap;
-import org.vertx.java.core.shareddata.impl.SharedSet;
-
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 /**
- * Sometimes it is desirable to share immutable data between different event loops, for example to implement a
- * cache of data.<p>
- * This class allows instances of shared data structures to be looked up and used from different event loops.<p>
- * The data structures themselves will only allow certain data types to be stored into them. This shields you from
- * worrying about any thread safety issues might occur if mutable objects were shared between event loops.<p>
- * The following types can be stored in a shareddata data structure:<p>
- * <pre>
- *   {@link String}
- *   {@link Integer}
- *   {@link Long}
- *   {@link Double}
- *   {@link Float}
- *   {@link Short}
- *   {@link Byte}
- *   {@link Character}
- *   {@code byte[]} - this will be automatically copied, and the copy will be stored in the structure.
- *   {@link org.vertx.java.core.buffer.Buffer} - this will be automatically copied, and the copy will be stored in the
- *   structure.
- * </pre>
+ * @author pidster
  *
- * @author <a href="http://tfox.org">Tim Fox</a>
  */
-public class SharedData {
+public interface SharedData {
 
-  private static final Logger log = LoggerFactory.getLogger(SharedData.class);
+	/**
+	 * Return a {@code Map} with the specific {@code name}. All invocations of this method with the same value of {@code name}
+	 * are guaranteed to return the same {@code Map} instance. <p>
+	 */
+	public abstract <K, V> ConcurrentMap<K, V> getMap(String name);
 
-  private ConcurrentMap<Object, SharedMap<?, ?>> maps = new ConcurrentHashMap<>();
-  private ConcurrentMap<Object, SharedSet<?>> sets = new ConcurrentHashMap<>();
+	/**
+	 * Return a {@code Set} with the specific {@code name}. All invocations of this method with the same value of {@code name}
+	 * are guaranteed to return the same {@code Set} instance. <p>
+	 */
+	public abstract <E> Set<E> getSet(String name);
 
-  /**
-   * Return a {@code Map} with the specific {@code name}. All invocations of this method with the same value of {@code name}
-   * are guaranteed to return the same {@code Map} instance. <p>
-   */
-  public <K, V> ConcurrentMap<K, V> getMap(String name) {
-    SharedMap<K, V> map = (SharedMap<K, V>) maps.get(name);
-    if (map == null) {
-      map = new SharedMap<>();
-      SharedMap prev = maps.putIfAbsent(name, map);
-      if (prev != null) {
-        map = prev;
-      }
-    }
-    return map;
-  }
+	/**
+	 * Remove the {@code Map} with the specifiec {@code name}.
+	 */
+	public abstract boolean removeMap(Object name);
 
-  /**
-   * Return a {@code Set} with the specific {@code name}. All invocations of this method with the same value of {@code name}
-   * are guaranteed to return the same {@code Set} instance. <p>
-   */
-  public <E> Set<E> getSet(String name) {
-    SharedSet<E> set = (SharedSet<E>) sets.get(name);
-    if (set == null) {
-      set = new SharedSet<>();
-      SharedSet prev = sets.putIfAbsent(name, set);
-      if (prev != null) {
-        set = prev;
-      }
-    }
-    return set;
-  }
-
-  /**
-   * Remove the {@code Map} with the specifiec {@code name}.
-   */
-  public boolean removeMap(Object name) {
-    return maps.remove(name) != null;
-  }
-
-  /**
-   * Remove the {@code Set} with the specifiec {@code name}.
-   */
-  public boolean removeSet(Object name) {
-    return sets.remove(name) != null;
-  }
+	/**
+	 * Remove the {@code Set} with the specifiec {@code name}.
+	 */
+	public abstract boolean removeSet(Object name);
 
 }
