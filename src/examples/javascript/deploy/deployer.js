36c73d7177c8b7a3324e5d42e76291b63e4f7c1a
Pid <pid@pidster.com>
2012-05-12 18:11:49 +0100

    remove old examples from git

--- src/examples/javascript/deploy/deployer.js
+++
@@ -1,5 +0,0 @@
-load('vertx.js')
-
-var config = {name: 'tim', age: 823823};
-
-var id = vertx.deployVerticle('deploy/child.js', config, 1);
--- src/examples/javascript/websockets/ws_server.js
+++
@@ -1,8 +0,0 @@
-load('vertx.js')
-
-vertx.createHttpServer().websocketHandler(function(ws) {
-  ws.dataHandler( function(buffer) { ws.writeTextFrame(buffer.toString()) });
-}).requestHandler(function(req) {
-  if (req.uri == "/") req.response.sendFile("websockets/ws.html")
-}).listen(8080)
-
