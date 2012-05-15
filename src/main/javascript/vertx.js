d3dc4d725d8b1e6d2c59c4f77eaacbde31ff53fc
nelson.silva <nelson.silva@inevo.pt>
2012-05-15 11:46:09 +0100

    Added commonjs modules support

--- src/main/javascript/vertx.js
+++ src/main/javascript/vertx.js
@@ -14,16 +14,18 @@
  * limitations under the License.
  */
 
-load('core/buffer.js');
-load('core/event_bus.js');
-load('core/net.js');
-load('core/http.js');
-load('core/streams.js');
-load('core/timers.js');
-load('core/utils.js');
-load('core/sockjs.js');
-load('core/parse_tools.js');
-load('core/shared_data.js');
-load('core/filesystem.js');
-load('core/deploy.js');
-load('core/logger.js');
+(function(){
+    load('core/buffer.js');
+    load('core/event_bus.js');
+    load('core/net.js');
+    load('core/http.js');
+    load('core/streams.js');
+    load('core/timers.js');
+    load('core/utils.js');
+    load('core/sockjs.js');
+    load('core/parse_tools.js');
+    load('core/shared_data.js');
+    load('core/filesystem.js');
+    load('core/deploy.js');
+    load('core/logger.js');
+}).call((this.module && module.exports)? module.exports : this);
