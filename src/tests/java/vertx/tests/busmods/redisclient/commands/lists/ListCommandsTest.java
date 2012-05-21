e00dcf9b4a4a86968364506035a3882122588b01
thmarx <thmarx@gmx.net>
2012-05-21 10:46:55 +0200

    add tests for hashcommands

---
+++ src/tests/java/vertx/tests/busmods/redisclient/commands/lists/ListCommandsTest.java
@@ -0,0 +1,41 @@
+package vertx.tests.busmods.redisclient.commands.lists;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.json.JsonArray;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.mods.redis.CommandContext;
+import org.vertx.mods.redis.commands.CommandException;
+import org.vertx.mods.redis.commands.lists.*;
+
+import redis.clients.jedis.Jedis;
+import vertx.tests.busmods.redisclient.commands.CommandTest;
+
+public class ListCommandsTest extends CommandTest {
+
+	static CommandContext context;
+	
+	@BeforeClass
+	public static void before () {
+		final Jedis jedis = new Jedis("localhost");
+		context = new CommandContext() {
+			
+			@Override
+			public Jedis getClient() {
+				return jedis;
+			}
+		};
+	}
+	@AfterClass
+	public static void after () {
+		context.getClient().quit();
+	}
+	
+	
+	
+}
