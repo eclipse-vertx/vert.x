66e282b9f94902f999fdf84e34e21ffbed3ba4d0
Stephen Sugden <glurgle@gmail.com>
2012-05-24 22:22:04 -0700

    update install_rhino.sh script for ant 1.8.4 release

--- src/scripts/install_rhino.sh
+++ src/scripts/install_rhino.sh
@@ -4,13 +4,13 @@
 # This script is only necessary for a short period, until Rhino1.7.R4 is released
 mkdir inst_tmp
 cd inst_tmp
-curl http://www.apache.org/dist/ant/binaries/apache-ant-1.8.3-bin.tar.gz > ant.tar.gz
+curl http://www.apache.org/dist/ant/binaries/apache-ant-1.8.4-bin.tar.gz > ant.tar.gz
 tar -zxf ant.tar.gz
 curl https://nodeload.github.com/mozilla/rhino/tarball/master > rhino.tar.gz
 tar -zxf rhino.tar.gz
 mv mozilla-rhino-* mozilla-rhino
 cd mozilla-rhino
-../apache-ant-1.8.3/bin/ant jar
+../apache-ant-1.8.4/bin/ant jar
 cp build/rhino1_7R4*/js.jar ../../lib/jars
 cd ../..
 rm -rf inst_tmp
