63dc6ac49c487f5605d762bb3ebefc5512abfd2e
Scott Horn <scott@hornmicro.com>
2012-06-16 20:00:44 +1000

    Added script loading test

---
+++ src/tests/python/core/scriptloading/test_client.py
@@ -0,0 +1,32 @@
+# Copyright 2011-2012 the original author or authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import vertx
+from test_utils import TestUtils
+
+tu = TestUtils()
+
+execfile("src/tests/python/core/scriptloading/script1.py")
+
+class ScriptingLoadingTest(object):
+    def test_scriptloading(self):
+        tu.azzert(Foo.func1(tu) == "foo")
+        tu.test_complete()
+
+def vertx_stop():
+    tu.unregister_all()
+    tu.app_stopped()
+
+tu.register_all(ScriptingLoadingTest())
+tu.app_ready()
