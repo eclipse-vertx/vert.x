c71743c8e74e60ce6599794c83cb14836f911b13
Scott Horn <scott@hornmicro.com>
2012-06-20 22:16:43 +1000

    Further work on pydocx to override pydoc to support java classes and not create broken links.

--- pydocx.py
+++ pydocx.py
@@ -2,6 +2,8 @@ import pydoc
 import os
 import re
 import shutil
+import sys
+import __builtin__
 
 class CustomHTMLDoc(pydoc.HTMLDoc):
     def docmodule(self, object, name=None, mod=None, *ignored):
@@ -26,6 +28,19 @@ class CustomHTMLDoc(pydoc.HTMLDoc):
         file.close()
         print 'wrote', APIDOC_DIR+'index.html'
 
+    def classlink(self, object, modname):
+        """Make a link for a class."""
+        name, module = object.__name__, sys.modules.get(object.__module__)
+        from java.lang import Object
+        if (object is Object or 
+            Object in object.__bases__ or
+            object is __builtin__.object):
+            return pydoc.classname(object, modname)
+        elif hasattr(module, name) and getattr(module, name) is object:
+            return '<a href="%s.html#%s">%s</a>' % (
+                module.__name__, name, pydoc.classname(object, modname))
+        return pydoc.classname(object, modname)
+
 def custom_writedoc(thing, forceload=0):
     """Write HTML documentation to specific directory"""
     try:
@@ -38,12 +53,93 @@ def custom_writedoc(thing, forceload=0):
     except (ImportError, pydoc.ErrorDuringImport), value:
         print value
 
+
+def custom_classify_class_attrs(cls):
+    """Override inspect.classify_class_attrs to catch errors on java objects"""
+    mro = pydoc.inspect.getmro(cls)
+    names = dir(cls)
+    result = []
+    for name in names:
+        try:
+            # Get the object associated with the name.
+            # Getting an obj from the __dict__ sometimes reveals more than
+            # using getattr.  Static and class methods are dramatic examples.
+            if name in cls.__dict__:
+                obj = cls.__dict__[name]
+            else:
+                obj = getattr(cls, name)
+
+            # Figure out where it was defined.
+            homecls = getattr(obj, "__objclass__", None)
+            if homecls is None:
+                # search the dicts.
+                for base in mro:
+                    if name in base.__dict__:
+                        homecls = base
+                        break
+
+            # Get the object again, in order to get it from the defining
+            # __dict__ instead of via getattr (if possible).
+            if homecls is not None and name in homecls.__dict__:
+                obj = homecls.__dict__[name]
+
+            # Also get the object via getattr.
+            obj_via_getattr = getattr(cls, name)
+
+            # Classify the object.
+            if isinstance(obj, staticmethod):
+                kind = "static method"
+            elif isinstance(obj, classmethod):
+                kind = "class method"
+            elif isinstance(obj, property):
+                kind = "property"
+            elif (pydoc.inspect.ismethod(obj_via_getattr) or
+                  pydoc.inspect.ismethoddescriptor(obj_via_getattr)):
+                kind = "method"
+            else:
+                kind = "data"
+
+            result.append((name, kind, homecls, obj))
+        except:
+            pass
+    return result
+
+def custom_getclasstree(classes, unique=0):
+    children = {}
+    roots = []
+    for c in classes:        
+        if c.__bases__:
+            for parent in c.__bases__:
+                if not parent in children:
+                    children[parent] = []
+                children[parent].append(c)
+                if unique and parent in classes: break
+        elif c not in roots:
+            roots.append(c)
+    extrachildren = {}
+    for parent in children:
+        if hasattr(parent, "name") and parent.name.startswith("org.python.proxies"):
+            base = parent.__bases__[0]
+            if base not in roots:
+                roots.append(base)
+            if not base in extrachildren:
+                extrachildren[base] = []    
+            extrachildren[base].append(parent)
+        elif parent not in classes : 
+            roots.append(parent)
+    children.update(extrachildren)
+    return pydoc.inspect.walktree(roots, children, None)
+
+# Remove and recreate the output docs directory
 APIDOC_DIR = os.getcwd()+"/target/docs/python/api/"
 shutil.rmtree("target/docs/python")
 os.makedirs(APIDOC_DIR)
-
 os.chdir("src/main/python")
+
+# Replace some of the pydoc methods to all them to work with java inheritance
 pydoc.html = CustomHTMLDoc()
+pydoc.inspect.classify_class_attrs = custom_classify_class_attrs
+pydoc.inspect.getclasstree = custom_getclasstree
 pydoc.writedoc = custom_writedoc
 pydoc.writedocs("./")
 pydoc.html.write_index()
