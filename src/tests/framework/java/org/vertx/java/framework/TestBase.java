7a5ff32e6c8327afd5f1123bb206ea1c80483c5e
Scott Horn <scott@hornmicro.com>
2012-06-24 22:30:39 +1000

    turned up test timeouts

--- src/tests/framework/java/org/vertx/java/framework/TestBase.java
+++ src/tests/framework/java/org/vertx/java/framework/TestBase.java
@@ -200,7 +200,7 @@ public class TestBase extends TestCase {
 
     startedApps.add(deploymentName);
 
-    if (!doneLatch.await(10, TimeUnit.SECONDS)) {
+    if (!doneLatch.await(30, TimeUnit.SECONDS)) {
       throw new IllegalStateException("Timedout waiting for apps to start");
     }
 
@@ -221,7 +221,7 @@ public class TestBase extends TestCase {
         latch.countDown();
       }
     });
-    if (!latch.await(10, TimeUnit.SECONDS)) {
+    if (!latch.await(30, TimeUnit.SECONDS)) {
         throw new IllegalStateException("Timedout waiting for app to stop");
     }
     for (int i = 0; i < instances; i++) {
