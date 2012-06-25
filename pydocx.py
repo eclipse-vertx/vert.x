892655319816c17971f1a089c979d471d8b78a5b
Scott Horn <scott@hornmicro.com>
2012-06-25 23:31:57 +1000

    Added some comments for pydocx

--- pydocx.py
+++ pydocx.py
@@ -1,3 +1,26 @@
+# Copyright 2011 the original author or authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+""" 
+This is a simple script to patch the jython pydoc to generate documentation
+when inheriting form java object. It also includes the generation of and index 
+and remove missing links to build-ins. 
+
+pydoc is the work of Ka-Ping Yee (ping@lfw.org) and I have done my best to keep all references to
+his work intact.
+"""
+
 import pydoc
 import os
 import re
