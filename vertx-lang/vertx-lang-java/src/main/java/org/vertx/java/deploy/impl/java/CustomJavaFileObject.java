a69913396f424a99efa0da946e6f28a0a34680fb
Janne Hietamaki <janne.hietamaki@gmail.com>
2012-07-18 08:20:06 +0300

    fixed loading from jar file, URI.getPath() returns null for jar urls so SimpleJavaFileObject can't be used.

--- vertx-lang/vertx-lang-java/src/main/java/org/vertx/java/deploy/impl/java/CustomJavaFileObject.java
+++ vertx-lang/vertx-lang-java/src/main/java/org/vertx/java/deploy/impl/java/CustomJavaFileObject.java
@@ -16,19 +16,29 @@
 
 package org.vertx.java.deploy.impl.java;
 
-import javax.tools.SimpleJavaFileObject;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
 import java.net.URI;
+
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.NestingKind;
+import javax.tools.JavaFileObject;
 
 /**
  * @author Janne Hietam&auml;ki
  */
-public class CustomJavaFileObject extends SimpleJavaFileObject {
+public class CustomJavaFileObject implements JavaFileObject {
   private final String binaryName;
+  private Kind kind;
+  private URI uri;
 
   protected CustomJavaFileObject(URI uri, Kind kind, String binaryName) {
-    super(uri, kind);
+    this.uri = uri;
+    this.kind = kind;
     this.binaryName = binaryName;
   }
 
@@ -39,5 +49,60 @@ public class CustomJavaFileObject extends SimpleJavaFileObject {
   @Override
   public InputStream openInputStream() throws IOException {
     return uri.toURL().openStream();
+  }
+
+  public Kind getKind() {
+    return kind;
+  }
+
+  public NestingKind getNestingKind() {
+    return null;
+  }
+
+  @Override
+  public URI toUri() {
+    return uri;
+  }
+
+  public String getName() {
+    return toUri().getPath();
+  }
+
+  public OutputStream openOutputStream() throws IOException {
+    throw new UnsupportedOperationException();
+  }
+
+  public Reader openReader(boolean ignoreEncodingErrors) throws IOException {
+    throw new UnsupportedOperationException();
+  }
+
+  public CharSequence getCharContent(boolean ignoreEncodingErrors) throws IOException {
+    throw new UnsupportedOperationException();
+  }
+
+  public Writer openWriter() throws IOException {
+    return new OutputStreamWriter(openOutputStream());
+  }
+
+  public long getLastModified() {
+    return 0L;
+  }
+
+  public boolean delete() {
+    return false;
+  }
+
+  public boolean isNameCompatible(String simpleName, Kind kind) {
+    String name = simpleName + kind.extension;
+    return (name.equals(toUri().getPath()) || toUri().getPath().endsWith("/" + name)) && kind.equals(getKind());
+  }
+
+  public Modifier getAccessLevel() {
+    return null;
+  }
+
+  @Override
+  public String toString() {
+    return getClass().getName() + "[" + toUri() + "]";
   }
 }
