29e5688cd9fa09668f95cada8a8d36618970bd6d
Alex Tkachman <alex.tkachman@gmail.com>
2012-07-23 08:24:07 +0300

    copyright updated

--- vertx-examples/src/main/kotlin/echo/EchoSslServer.kt
+++ vertx-examples/src/main/kotlin/echo/EchoSslServer.kt
@@ -1,11 +1,11 @@
 /*
- * Copyright 2012 JetBrains s.r.o.
+ * Copyright 2011-2012 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.vertx.kotlin.examples.echo
 
 import org.vertx.java.deploy.Verticle
@@ -24,7 +25,7 @@ public class EchoSslServer() : Verticle() {
     public override fun start() {
         createNetServer {
             setSSL(true)
-            setKeyStorePath("./server-keystore.jks")
+            setKeyStorePath("../java/server-keystore.jks")
             setKeyStorePassword("wibble")
 
             connectHandler{ socket ->
