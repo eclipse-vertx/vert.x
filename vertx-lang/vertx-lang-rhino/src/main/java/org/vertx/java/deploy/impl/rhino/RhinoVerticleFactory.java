7c6d8fa2b4964faf834a3717e6630452c7a2df2f
Jock Murphy <Jock@jockmurphy.com>
2012-09-08 16:41:56 -0700

    Initial checkin, enhances existing classes to support rhino debugging

--- vertx-lang/vertx-lang-rhino/src/main/java/org/vertx/java/deploy/impl/rhino/RhinoVerticleFactory.java
+++ vertx-lang/vertx-lang-rhino/src/main/java/org/vertx/java/deploy/impl/rhino/RhinoVerticleFactory.java
@@ -18,6 +18,10 @@ package org.vertx.java.deploy.impl.rhino;
 
 import org.mozilla.javascript.ContextFactory;
 import org.mozilla.javascript.RhinoException;
+
+import org.mozilla.javascript.tools.debugger.Main;
+import org.mozilla.javascript.tools.debugger.ScopeProvider;
+
 import org.vertx.java.core.logging.Logger;
 import org.vertx.java.deploy.Verticle;
 import org.vertx.java.deploy.VerticleFactory;
@@ -29,13 +33,51 @@ import org.vertx.java.deploy.impl.VerticleManager;
  */
 public class RhinoVerticleFactory implements VerticleFactory {
 
-  static {
-    ContextFactory.initGlobal(new RhinoContextFactory());
-  }
+  static boolean initializeContextFactory = false;
 
   private VerticleManager mgr;
 
+
+  private static ContextFactory getRhinoDebugContextFactory() {
+    // Certification of origin:  This function in based on
+    // code found in the Mozilla Rhino mailing list:
+    //   https://groups.google.com/forum/?fromgroups=#!msg/mozilla.dev.tech.js-engine.rhino/pNJlUd_2ueg/GBc2std9czEJ
+
+	  ContextFactory contextFactory = new RhinoDebugContextFactory();
+
+    Main main = new Main("JS Debugger");
+    main.setExitAction(new Runnable() {
+      public void run() {
+        System.exit(0);
+      }
+    });
+
+    main.attachTo(contextFactory);
+    main.setScopeProvider(new ScopeProvider() {
+      public org.mozilla.javascript.Scriptable getScope() {
+        throw new UnsupportedOperationException();
+      }
+    });
+
+    main.pack();
+    main.setSize(600, 460);
+    main.setVisible(true);
+
+    return contextFactory;
+  }
+
+
   public RhinoVerticleFactory() {
+    if(!RhinoVerticleFactory.initializeContextFactory) {
+      boolean rhinoDebuggingEnabled = "true".equals(System.getProperty("vertx.debugRhino", "false"));
+      if(rhinoDebuggingEnabled) {
+        ContextFactory.initGlobal(RhinoVerticleFactory.getRhinoDebugContextFactory());
+      } else {
+        ContextFactory.initGlobal(new RhinoContextFactory());
+      }
+
+      RhinoVerticleFactory.initializeContextFactory = true;
+    }
   }
 
   @Override
