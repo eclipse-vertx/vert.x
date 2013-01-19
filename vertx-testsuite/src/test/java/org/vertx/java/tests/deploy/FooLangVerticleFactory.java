ab22523d33ee0584cb8e57d0411169fdc15ac003
Pid ster <pid@pidster.com>
2013-01-19 22:03:48 +0000

    add some license headers

--- vertx-testsuite/src/test/java/org/vertx/java/tests/deploy/FooLangVerticleFactory.java
+++ vertx-testsuite/src/test/java/org/vertx/java/tests/deploy/FooLangVerticleFactory.java
@@ -1,4 +1,18 @@
-package org.vertx.java.tests.deploy;
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */package org.vertx.java.tests.deploy;
 
 import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.deploy.Verticle;
@@ -6,6 +20,10 @@ import org.vertx.java.deploy.impl.ModuleClassLoader;
 import org.vertx.java.deploy.impl.VerticleFactory;
 import org.vertx.java.deploy.impl.VerticleManager;
 
+/**
+ * @author swilliams
+ *
+ */
 public class FooLangVerticleFactory implements VerticleFactory {
 
   private VerticleManager manager;
