462b8f4c5ef270b7b41b3d7beec0fea68d614397
purplefox <timvolpe@gmail.com>
2013-02-11 08:46:26 +0000

    increase timer leeway - needed for CI

--- vertx-core/src/test/java/vertx/tests/core/timer/TestClient.java
+++ vertx-core/src/test/java/vertx/tests/core/timer/TestClient.java
@@ -94,8 +94,8 @@ public class TestClient extends TestClientBase {
         tu.checkThread();
         long dur = System.currentTimeMillis() - start;
         tu.azzert(dur >= delay);
-        long maxDelay = (long)(delay * 1.5);
-        tu.azzert(dur < maxDelay, "Timer accuracy: " + dur + " vs " + maxDelay); // 50% margin of error
+        long maxDelay = delay * 2;
+        tu.azzert(dur < maxDelay, "Timer accuracy: " + dur + " vs " + maxDelay); // 100% margin of error (needed for CI)
         vertx.cancelTimer(timerID);
         tu.testComplete();
       }
