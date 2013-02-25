bf12c537e84b0316c4ebd6736574d364e22f3245
purplefox <timvolpe@gmail.com>
2013-02-25 17:05:06 +0000

    maven local support

---
+++ vertx-platform/src/main/java/org/vertx/java/platform/impl/resolver/MavenIdentifier.java
@@ -0,0 +1,45 @@
+package org.vertx.java.platform.impl.resolver;
+
+/*
+ * Copyright 2013 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+public class MavenIdentifier {
+
+  public MavenIdentifier(String moduleName) {
+    String[] parts = moduleName.split(":");
+    if (parts.length != 3) {
+      throw new IllegalArgumentException(moduleName + " must be of the form <group_id>:<artifact_id>:<version>");
+    }
+
+    groupID = parts[0];
+    artifactID = parts[1];
+    version = parts[2];
+
+    StringBuilder uri = new StringBuilder('/');
+    String[] groupParts = groupID.split("\\.");
+    for (String groupPart: groupParts) {
+      uri.append(groupPart).append('/');
+    }
+    uri.append(artifactID).append('/').append(version).append('/');
+    uriRoot = uri.toString();
+  }
+
+  public String groupID;
+  public String artifactID;
+  public String version;
+  public String uriRoot;
+}
