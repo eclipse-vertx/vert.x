b427f0f827f781aaf9e636c142e427153aede1e8
John Meyer <johnpmeyer@gmail.com>
2013-06-21 18:03:48 -0400

    Changed Maven Resolution to use classifier of mod Updated tests to reflect new names in maven repos.

--- vertx-platform/src/test/mod-test/io.vertx~mod-proxy-maven-server~1.0/Server.java
+++ vertx-platform/src/test/mod-test/io.vertx~mod-proxy-maven-server~1.0/Server.java
@@ -16,7 +16,7 @@ public class Server extends Verticle {
     vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
       public void handle(HttpServerRequest req) {
         // It's being proxied so should be absolute url
-        tu.azzert(req.uri().equals("http://localhost:9192/maven2/io/vertx/mod-maven-test/1.0.0/mod-maven-test-1.0.0.zip"));
+        tu.azzert(req.uri().equals("http://localhost:9192/maven2/io/vertx/mod-maven-test/1.0.0/mod-maven-test-1.0.0-mod.zip"));
         if (req.path().indexOf("..") != -1) {
           req.response().setStatusCode(403);
           req.response().end();
