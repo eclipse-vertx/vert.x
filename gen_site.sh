0db220e9c88018da39f32b5e6fbd19307f50d03c
purplefox <timvolpe@gmail.com>
2013-06-27 14:55:12 +0100

    moved markdown to own directory

--- gen_site.sh
+++ gen_site.sh
@@ -1,38 +1,14 @@
 #!/bin/sh
 
-./gen_section.sh manual.md manual_templ.html manual.html
-./gen_section.sh mods_manual.md mods_manual_templ.html mods_manual.html
-./gen_section.sh core_manual_js.md core_manual_js_templ.html core_manual_js.html
-./gen_section.sh core_manual_ruby.md core_manual_ruby_templ.html core_manual_ruby.html
-./gen_section.sh core_manual_python.md core_manual_python_templ.html core_manual_python.html
-./gen_section.sh core_manual_java.md core_manual_java_templ.html core_manual_java.html
-./gen_section.sh core_manual_groovy.md core_manual_groovy_templ.html core_manual_groovy.html
-./gen_section.sh install.md install_manual_templ.html install.html
-./gen_section.sh js_web_tutorial.md js_web_tutorial_templ.html js_web_tutorial.html
-./gen_section.sh ruby_web_tutorial.md ruby_web_tutorial_templ.html ruby_web_tutorial.html
-./gen_section.sh python_web_tutorial.md python_web_tutorial_templ.html python_web_tutorial.html
-./gen_section.sh groovy_web_tutorial.md groovy_web_tutorial_templ.html groovy_web_tutorial.html
-
-
-
-./gen_section.sh vertx20_docs/manual.md vertx20_docs/manual_templ.html vertx20_docs/manual.html
-./gen_section.sh vertx20_docs/mods_manual.md vertx20_docs/mods_manual_templ.html vertx20_docs/mods_manual.html
-./gen_section.sh vertx20_docs/core_manual_js.md vertx20_docs/core_manual_js_templ.html vertx20_docs/core_manual_js.html
-./gen_section.sh vertx20_docs/core_manual_ruby.md vertx20_docs/core_manual_ruby_templ.html vertx20_docs/core_manual_ruby.html
-./gen_section.sh vertx20_docs/core_manual_python.md vertx20_docs/core_manual_python_templ.html vertx20_docs/core_manual_python.html
-./gen_section.sh vertx20_docs/core_manual_java.md vertx20_docs/core_manual_java_templ.html vertx20_docs/core_manual_java.html
-./gen_section.sh vertx20_docs/core_manual_groovy.md vertx20_docs/core_manual_groovy_templ.html vertx20_docs/core_manual_groovy.html
-./gen_section.sh vertx20_docs/install.md vertx20_docs/install_manual_templ.html vertx20_docs/install.html
-./gen_section.sh vertx20_docs/js_web_tutorial.md vertx20_docs/js_web_tutorial_templ.html vertx20_docs/js_web_tutorial.html
-./gen_section.sh vertx20_docs/ruby_web_tutorial.md vertx20_docs/ruby_web_tutorial_templ.html vertx20_docs/ruby_web_tutorial.html
-./gen_section.sh vertx20_docs/python_web_tutorial.md vertx20_docs/python_web_tutorial_templ.html vertx20_docs/python_web_tutorial.html
-./gen_section.sh vertx20_docs/groovy_web_tutorial.md vertx20_docs/groovy_web_tutorial_templ.html vertx20_docs/groovy_web_tutorial.html
-./gen_section.sh vertx20_docs/dev_guide.md vertx20_docs/dev_guide_templ.html vertx20_docs/dev_guide.html
-./gen_section.sh vertx20_docs/gradle_dev.md vertx20_docs/gradle_dev_templ.html vertx20_docs/gradle_dev.html
-./gen_section.sh vertx20_docs/maven_dev.md vertx20_docs/maven_dev_templ.html vertx20_docs/maven_dev.html
-./gen_section.sh vertx20_docs/example_project.md vertx20_docs/example_project_templ.html vertx20_docs/example_project.html
-./gen_section.sh vertx20_docs/embedding_manual.md vertx20_docs/embedding_manual_templ.html vertx20_docs/embedding_manual.html
-./gen_section.sh vertx20_docs/language_support.md vertx20_docs/language_support_templ.html vertx20_docs/language_support.html
-
-
-
+./gen_section.sh docs_md/manual.md manual_templ.html manual.html
+./gen_section.sh docs_md/mods_manual.md mods_manual_templ.html mods_manual.html
+./gen_section.sh docs_md/core_manual_js.md core_manual_js_templ.html core_manual_js.html
+./gen_section.sh docs_md/core_manual_ruby.md core_manual_ruby_templ.html core_manual_ruby.html
+./gen_section.sh docs_md/core_manual_python.md core_manual_python_templ.html core_manual_python.html
+./gen_section.sh docs_md/core_manual_java.md core_manual_java_templ.html core_manual_java.html
+./gen_section.sh docs_md/core_manual_groovy.md core_manual_groovy_templ.html core_manual_groovy.html
+./gen_section.sh docs_md/install.md install_manual_templ.html install.html
+./gen_section.sh docs_md/js_web_tutorial.md js_web_tutorial_templ.html js_web_tutorial.html
+./gen_section.sh docs_md/ruby_web_tutorial.md ruby_web_tutorial_templ.html ruby_web_tutorial.html
+./gen_section.sh docs_md/python_web_tutorial.md python_web_tutorial_templ.html python_web_tutorial.html
+./gen_section.sh docs_md/groovy_web_tutorial.md groovy_web_tutorial_templ.html groovy_web_tutorial.html
