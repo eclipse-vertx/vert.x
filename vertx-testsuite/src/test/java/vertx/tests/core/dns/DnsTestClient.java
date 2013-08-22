b8572f8e61a97c7aa21c8f99a16090c388ec954b
Norman Maurer <nmaurer@redhat.com>
2013-08-22 12:17:00 +0200

    Fix up a test

--- vertx-testsuite/src/test/java/vertx/tests/core/dns/DnsTestClient.java
+++ vertx-testsuite/src/test/java/vertx/tests/core/dns/DnsTestClient.java
@@ -342,9 +342,9 @@ public class DnsTestClient extends TestClientBase {
       }
     });
 
-    dns.lookup("vertx.io", new Handler<AsyncResult<InetAddress>>() {
+    dns.lookup4("vertx.io", new Handler<AsyncResult<Inet4Address>>() {
       @Override
-      public void handle(AsyncResult<InetAddress> event) {
+      public void handle(AsyncResult<Inet4Address> event) {
         InetAddress result = event.result();
         tu.azzert(result != null);
         tu.azzert("10.0.0.1".equals(result.getHostAddress()));
