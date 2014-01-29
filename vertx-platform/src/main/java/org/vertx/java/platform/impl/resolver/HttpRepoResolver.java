c247e34abf0748594e5f345cadff72b9532b3572
purplefox <timvolpe@gmail.com>
2014-01-29 11:12:00 +0000

    Updated licence headers on Red Hat and Netty files after RedHat and Netty agreed to relicence them under dual Apache License v2.0 / Eclipse Public License 1.0

--- vertx-platform/src/main/java/org/vertx/java/platform/impl/resolver/HttpRepoResolver.java
+++ vertx-platform/src/main/java/org/vertx/java/platform/impl/resolver/HttpRepoResolver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2013 The original author or authors
+ * Copyright (c) 2011-2013 Red Hat Inc.
  * ------------------------------------------------------
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -14,24 +14,7 @@
  * You may elect to redistribute this code under either of these licenses.
  */
 
-package org.vertx.java.platform.impl.resolver;/*
- * Copyright 2013 Red Hat, Inc.
- *
- * Red Hat licenses this file to you under the Apache License, version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License.  You may obtain a copy of the License at:
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
- *
- * @author <a href="http://tfox.org">Tim Fox</a>
- */
-
+package org.vertx.java.platform.impl.resolver;
 import org.vertx.java.core.Vertx;
 import org.vertx.java.platform.impl.ModuleIdentifier;
 
--- vertx-testsuite/src/test/java/org/vertx/java/tests/core/http/JavaWorkerHttpTest.java
+++ vertx-testsuite/src/test/java/org/vertx/java/tests/core/http/JavaWorkerHttpTest.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2011-2013 The original author or authors
- * ------------------------------------------------------
+ * Copyright (c) 2011-2013 Red Hat Inc.
+ * ------------------------------------
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * and Apache License v2.0 which accompanies this distribution.
@@ -14,23 +14,7 @@
  * You may elect to redistribute this code under either of these licenses.
  */
 
-package org.vertx.java.tests.core.http;/*
- * Copyright 2013 Red Hat, Inc.
- *
- * Red Hat licenses this file to you under the Apache License, version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License.  You may obtain a copy of the License at:
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
- *
- * @author <a href="http://tfox.org">Tim Fox</a>
- */
+package org.vertx.java.tests.core.http;
 
 import org.junit.Test;
 import org.vertx.java.testframework.TestBase;
