a2f536a8bdb46e57901db898934e2d1ad51367ee
purplefox <timvolpe@gmail.com>
2014-02-17 14:54:17 +0000

    channel exceptions should be reported on exception handler

--- vertx-core/src/main/java/org/vertx/java/core/net/impl/ConnectionBase.java
+++ vertx-core/src/main/java/org/vertx/java/core/net/impl/ConnectionBase.java
@@ -156,7 +156,7 @@ public abstract class ConnectionBase {
               }
             });
           } else if (!channelFuture.isSuccess()) {
-            vertx.reportException(channelFuture.cause());
+            handleException(channelFuture.cause());
           }
         }
       });
