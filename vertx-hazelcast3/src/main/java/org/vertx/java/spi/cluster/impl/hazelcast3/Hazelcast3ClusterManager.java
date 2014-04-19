f6da231e34b9c5bf00eca15f152359f48751d682
javaboon <richard.m.hightower@gmail.com>
2014-04-18 17:40:43 -0700

    Hazelcast3 support including dist files and integration tests
    Hazelcast3 support including dist files and integration tests.
    
    Hazelcast 3.2 has some critical bug fixes in it.
    
    To use Hazelcast 3.2 with vertx use the distribution file called:
    
    vert.x.hazel3cluster-${version}.zip or
    vert.x.hazel3cluster-${version}.tar.gz.
    
    A new gradle jar project was created called 'vertx-hazelcast3'. Vertx
    uses a service location pattern to load the in a pluggable cluster
    manager. This fix employs the pluggable nature of vertx cluster manager
    to plugin Hazelcast 3 instead of Hazelcast 2.
    
    This produces a new jar file that has a new cluster manager factory
    which can be loaded with the service locator.
    
    It has been tested.
    
    Find more details at:
    
    https://github.com/RichardHightower/vert.x/wiki/Using-Hazelcast-3.2

---
+++ vertx-hazelcast3/src/main/java/org/vertx/java/spi/cluster/impl/hazelcast3/Hazelcast3ClusterManager.java
@@ -0,0 +1,207 @@
+/*
+ * Copyright (c) 2011-2013 The original author or authors
+ * ------------------------------------------------------
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ *
+ *     The Eclipse Public License is available at
+ *     http://www.eclipse.org/legal/epl-v10.html
+ *
+ *     The Apache License v2.0 is available at
+ *     http://www.opensource.org/licenses/apache2.0.php
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ */
+
+package org.vertx.java.spi.cluster.impl.hazelcast3;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.XmlConfigBuilder;
+import com.hazelcast.core.*;
+import org.vertx.java.core.logging.Logger;
+import org.vertx.java.core.logging.impl.LoggerFactory;
+import org.vertx.java.core.spi.VertxSPI;
+import org.vertx.java.core.spi.cluster.AsyncMap;
+import org.vertx.java.core.spi.cluster.AsyncMultiMap;
+import org.vertx.java.core.spi.cluster.ClusterManager;
+import org.vertx.java.core.spi.cluster.NodeListener;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * A cluster manager that uses Hazelcast
+ * 
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+class Hazelcast3ClusterManager implements ClusterManager, MembershipListener {
+
+  private static final Logger log = LoggerFactory.getLogger(Hazelcast3ClusterManager.class);
+  // Hazelcast config file
+
+    private static final String DEFAULT_CONFIG_FILE = "default-cluster-hazelcast-3.xml";
+    private static final String CONFIG_FILE = "cluster3.xml";
+
+
+
+    private final VertxSPI vertx;
+
+  private HazelcastInstance hazelcast;
+  private String nodeID;
+  private NodeListener nodeListener;
+  private boolean active;
+
+  /**
+   * Constructor
+   */
+  public Hazelcast3ClusterManager(final VertxSPI vertx) {
+  	this.vertx = vertx;
+    // We have our own shutdown hook and need to ensure ours runs before Hazelcast is shutdown
+    System.setProperty("hazelcast3.shutdownhook.enabled", "false");
+  }
+
+  public synchronized void join() {
+    if (active) {
+      return;
+    }
+    Config cfg = getConfig();
+    if (cfg == null) {
+      log.warn("Cannot find cluster configuration on classpath. Using default hazelcast3 configuration");
+    }
+    hazelcast = Hazelcast.newHazelcastInstance(cfg);
+    nodeID = hazelcast.getCluster().getLocalMember().getUuid();
+    hazelcast.getCluster().addMembershipListener(this);
+
+    active = true;
+  }
+
+	/**
+	 * Every eventbus handler has an ID. SubsMap (subscriber map) is a MultiMap which 
+	 * maps handler-IDs with server-IDs and thus allows the eventbus to determine where 
+	 * to send messages.
+	 * 
+	 * @param name A unique name by which the the MultiMap can be identified within the cluster. 
+	 *     See the cluster config file (e.g. cluster.xml in case of Hazelcast3ClusterManager) for
+	 *     additional MultiMap config parameters.
+	 * @return subscription map
+	 */
+  public <K, V> AsyncMultiMap<K, V> getAsyncMultiMap(final String name) {
+    com.hazelcast.core.MultiMap map = hazelcast.getMultiMap(name);
+    return new HazelcastAsyncMultiMap(vertx, map);
+  }
+
+  @Override
+  public String getNodeID() {
+    return nodeID;
+  }
+
+  @Override
+  public List<String> getNodes() {
+    Set<Member> members = hazelcast.getCluster().getMembers();
+    List<String> lMembers = new ArrayList<>();
+    for (Member member: members) {
+      lMembers.add(member.getUuid());
+    }
+    return lMembers;
+  }
+
+  @Override
+  public void nodeListener(NodeListener listener) {
+    this.nodeListener = listener;
+  }
+
+  @Override
+  public <K, V> AsyncMap<K, V> getAsyncMap(String name) {
+    IMap<K, V> map = hazelcast.getMap(name);
+    return new HazelcastAsyncMap(vertx, map);
+  }
+
+  @Override
+  public <K, V> Map<K, V> getSyncMap(String name) {
+    IMap<K, V> map = hazelcast.getMap(name);
+    return map;
+  }
+
+  public synchronized void leave() {
+    if (!active) {
+      return;
+    }
+    hazelcast.getCluster().removeMembershipListener(nodeID);
+ 		hazelcast.getLifecycleService().shutdown();
+    active = false;
+  }
+
+  @Override
+  public synchronized void memberAdded(MembershipEvent membershipEvent) {
+    if (!active) {
+      return;
+    }
+    try {
+      if (nodeListener != null) {
+        Member member = membershipEvent.getMember();
+        nodeListener.nodeAdded(member.getUuid());
+      }
+    } catch (Throwable t) {
+      log.error("Failed to handle memberAdded", t);
+    }
+  }
+
+  @Override
+  public synchronized void memberRemoved(MembershipEvent membershipEvent) {
+    if (!active) {
+      return;
+    }
+    try {
+      if (nodeListener != null) {
+        Member member = membershipEvent.getMember();
+        nodeListener.nodeLeft(member.getUuid());
+      }
+    } catch (Throwable t) {
+      log.error("Failed to handle memberRemoved", t);
+    }
+  }
+
+    @Override
+    public void memberAttributeChanged(MemberAttributeEvent memberAttributeEvent) {
+
+    }
+
+    private InputStream getConfigStream() {
+    ClassLoader ctxClsLoader = Thread.currentThread().getContextClassLoader();
+    InputStream is = null;
+    if (ctxClsLoader != null) {
+      is = ctxClsLoader.getResourceAsStream(CONFIG_FILE);
+    }
+    if (is == null) {
+      is = getClass().getClassLoader().getResourceAsStream(CONFIG_FILE);
+      if (is == null) {
+        is = getClass().getClassLoader().getResourceAsStream(DEFAULT_CONFIG_FILE);
+      }
+    }
+    return is;
+  }
+
+  /**
+   * Get the Hazelcast config
+   * @return a config object
+   */
+  protected Config getConfig() {
+    Config cfg = null;
+    try (InputStream is = getConfigStream();
+         InputStream bis = new BufferedInputStream(is)) {
+      if (is != null) {
+        cfg = new XmlConfigBuilder(bis).build();
+      }
+    } catch (IOException ex) {
+      log.error("Failed to read config", ex);
+    }
+    return cfg;
+  }
+
+}
---
+++ vertx-hazelcast3/src/main/java/org/vertx/java/spi/cluster/impl/hazelcast3/HazelcastServerID.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2011-2013 The original author or authors
+ * ------------------------------------------------------
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ *
+ *     The Eclipse Public License is available at
+ *     http://www.eclipse.org/legal/epl-v10.html
+ *
+ *     The Apache License v2.0 is available at
+ *     http://www.opensource.org/licenses/apache2.0.php
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ */
+
+package org.vertx.java.spi.cluster.impl.hazelcast3;
+
+import com.hazelcast.nio.ObjectDataInput;
+import com.hazelcast.nio.ObjectDataOutput;
+import com.hazelcast.nio.serialization.DataSerializable;
+import org.vertx.java.core.net.impl.ServerID;
+
+import java.io.IOException;
+
+/**
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+class HazelcastServerID extends ServerID implements DataSerializable {
+
+  public HazelcastServerID() {
+  }
+
+  public HazelcastServerID(ServerID serverID) {
+    super(serverID.port, serverID.host);
+  }
+
+  @Override
+  public void writeData(ObjectDataOutput dataOutput) throws IOException {
+    dataOutput.writeInt(port);
+    dataOutput.writeUTF(host);
+  }
+
+  @Override
+  public void readData(ObjectDataInput dataInput) throws IOException {
+    port = dataInput.readInt();
+    host = dataInput.readUTF();
+  }
+
+  // We replace any ServerID instances with HazelcastServerID - this allows them to be serialized more optimally using
+  // DataSerializable
+  public static <V> V convertServerID(V val) {
+    if (val.getClass() == ServerID.class) {
+      ServerID sid = (ServerID)val;
+      HazelcastServerID hsid = new HazelcastServerID(sid);
+      return (V)hsid;
+    } else {
+      return val;
+    }
+  }
+
+}
