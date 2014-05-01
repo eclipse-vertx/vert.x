ac5ef5eb9379a5ef2173c22e530c0002d1047810
Geoffrey Arnold <geoffrey@geoffreyarnold.com>
2014-05-01 14:47:26 -0400

    Modifying module loading to scan for libraries recursively when using module links.
    Signed-off-by: Geoffrey Arnold <geoffrey@geoffreyarnold.com>

--- vertx-platform/src/main/java/org/vertx/java/platform/impl/DefaultPlatformManager.java
+++ vertx-platform/src/main/java/org/vertx/java/platform/impl/DefaultPlatformManager.java
@@ -268,10 +268,10 @@ public class DefaultPlatformManager implements PlatformManagerInternal, ModuleRe
         parents.add(entry.getKey());
       }
     }
-    
+
     final CountingCompletionHandler<Void> count = new CountingCompletionHandler<>(vertx, parents.size());
     count.setHandler(doneHandler);
-    
+
     for (String name: parents) {
       undeploy(name, new Handler<AsyncResult<Void>>() {
         public void handle(AsyncResult<Void> res) {
@@ -1183,15 +1183,7 @@ public class DefaultPlatformManager implements PlatformManagerInternal, ModuleRe
                 }
                 URL url = fentry.toURI().toURL();
                 cpList.add(url);
-                if (fentry.exists() && fentry.isDirectory()) {
-                  File[] files = fentry.listFiles();
-                  for (File file : files) {
-                    String fPath = file.getCanonicalPath();
-                    if (fPath.endsWith(".jar") || fPath.endsWith(".zip")) {
-                      cpList.add(file.getCanonicalFile().toURI().toURL());
-                    }
-                  }
-                }
+                addLibrariesRecursively(cpList, fentry);
               }
             }
           } catch (Exception e) {
@@ -1223,6 +1215,21 @@ public class DefaultPlatformManager implements PlatformManagerInternal, ModuleRe
     return new ModuleInfo(modJSON, cpList, modDir);
   }
 
+  private void addLibrariesRecursively(List<URL> cpList, File fentry) throws IOException {
+    if (fentry.exists() && fentry.isDirectory()) {
+      File[] files = fentry.listFiles();
+      for (File file : files) {
+        if (file.isDirectory()) {
+          addLibrariesRecursively(cpList, file);
+        } else {
+          String fPath = file.getCanonicalPath();
+          if (fPath.endsWith(".jar") || fPath.endsWith(".zip")) {
+            cpList.add(file.getCanonicalFile().toURI().toURL());
+          }
+        }
+      }
+    }
+  }
 
   private void deployModuleFromFileSystem(File modRoot, String depName, ModuleIdentifier modID,
                                           JsonObject config,
