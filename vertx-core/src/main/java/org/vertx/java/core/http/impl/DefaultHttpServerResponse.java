eclipse-vertx/vert.x

commit 4a0c08d3b37c07b0f246315f7c45c46b4ee99212

Make sure the statusMessage is not dropped. Fixes BZ 425931

vertx-core/src/main/java/org/vertx/java/core/http/impl/DefaultHttpServerResponse.java
@@ -96,7 +96,7 @@ public class DefaultHttpServerResponse implements HttpServerResponse {
 
   @Override
   public HttpServerResponse setStatusCode(int statusCode) {
-    this.response.setStatus(HttpResponseStatus.valueOf(statusCode));
+    this.response.setStatus(new HttpResponseStatus(statusCode, response.getStatus().reasonPhrase()));
     return this;
   }
 

commit fbe8da75cbe1672bf5d3d68fce2a832b3f7b262f

Fix430827 setting http status code updates message
Line up with the documentation, which guarantees that the default
status message should be lined up with the status code.
Broken since 4a0c08d3.

vertx-core/src/main/java/org/vertx/java/core/http/impl/DefaultHttpServerResponse.java
@@ -57,6 +57,7 @@ public class DefaultHttpServerResponse implements HttpServerResponse {
   private MultiMap headers;
   private LastHttpContent trailing;
   private MultiMap trailers;
+  private boolean statusMessageSet;
 
   DefaultHttpServerResponse(final VertxInternal vertx, ServerConnection conn, HttpRequest request) {
   	this.vertx = vertx;
@@ -93,7 +94,13 @@ public class DefaultHttpServerResponse implements HttpServerResponse {
 
   @Override
   public HttpServerResponse setStatusCode(int statusCode) {
-    this.response.setStatus(new HttpResponseStatus(statusCode, response.getStatus().reasonPhrase()));
+    HttpResponseStatus status;
+    if(statusMessageSet) {
+      status = new HttpResponseStatus(statusCode, response.getStatus().reasonPhrase());
+    } else {
+      status = HttpResponseStatus.valueOf(statusCode);
+    }
+    this.response.setStatus(status);
     return this;
   }
 
@@ -105,6 +112,7 @@ public class DefaultHttpServerResponse implements HttpServerResponse {
   @Override
   public HttpServerResponse setStatusMessage(String statusMessage) {
     this.response.setStatus(new HttpResponseStatus(response.getStatus().code(), statusMessage));
+    statusMessageSet = true;
     return this;
   }
 