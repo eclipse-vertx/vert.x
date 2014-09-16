8f9da255a3a66d248de6d212138a8db90be38e9b
purplefox <timvolpe@gmail.com>
2014-09-16 16:39:51 +0100

    correct spelling

--- vertx-core/src/main/java/io/vertx/core/Starter.java
+++ vertx-core/src/main/java/io/vertx/core/Starter.java
@@ -338,7 +338,7 @@ public class Starter {
         "                               a cluster with any other vert.x instances on    \n" +
         "                               the network.                                    \n" +
         "        -cluster-port          port to use for cluster communication.          \n" +
-        "                               Default is 0 which means chose a spare          \n" +
+        "                               Default is 0 which means choose a spare          \n" +
         "                               random port.                                    \n" +
         "        -cluster-host          host to bind to for cluster communication.      \n" +
         "                               If this is not specified vert.x will attempt    \n" +
