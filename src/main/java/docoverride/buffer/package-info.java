958fcde104db069e0c455da5885952ded922a826
Julien Viet <julien@julienviet.com>
2015-01-29 14:46:58 +0100

    Creating buffer from bytes is exclusive to the java api

---
+++ src/main/java/docoverride/buffer/package-info.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2011-2013 The original author or authors
+ *  ------------------------------------------------------
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  and Apache License v2.0 which accompanies this distribution.
+ *
+ *      The Eclipse Public License is available at
+ *      http://www.eclipse.org/legal/epl-v10.html
+ *
+ *      The Apache License v2.0 is available at
+ *      http://www.opensource.org/licenses/apache2.0.php
+ *
+ *  You may elect to redistribute this code under either of these licenses.
+ */
+
+/**
+ * Create a buffer from a byte[]
+ *
+ * [source,java]
+ * ----
+ * {@link docoverride.buffer.Examples#example4}
+ * ----
+ */
+@Document(fileName = "override/buffer_from_bytes.adoc")
+package docoverride.buffer;
+
+import io.vertx.docgen.Document;
+
