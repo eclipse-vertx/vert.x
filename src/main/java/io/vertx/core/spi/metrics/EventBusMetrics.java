3626f6ccf0b0250a4679f4a9479a56bc1b4077eb
nscavell <nick.scavelli@gmail.com>
2015-01-30 18:46:11 -0500

    Add some metrics documentation

--- src/main/java/io/vertx/core/spi/metrics/EventBusMetrics.java
+++ src/main/java/io/vertx/core/spi/metrics/EventBusMetrics.java
@@ -19,17 +19,46 @@ package io.vertx.core.spi.metrics;
 import io.vertx.core.eventbus.ReplyFailure;
 
 /**
+ * The event bus metrics SPI which Vert.x will use to call when each event occurs.
+ *
  * @author <a href="mailto:nscavell@redhat.com">Nick Scavelli</a>
  */
 public interface EventBusMetrics extends BaseMetrics {
 
+  /**
+   * Called when a handler is registered on the event bus.
+   *
+   * @param address the address used to register the handler
+   */
   void handlerRegistered(String address);
 
+  /**
+   * Called when a handler has been unregistered from the event bus.
+   *
+   * @param address the address that was used to register the handler
+   */
   void handlerUnregistered(String address);
 
+  /**
+   * Called when a message has been sent or published.
+   *
+   * @param address the address
+   * @param publish true if it was a publish
+   */
   void messageSent(String address, boolean publish);
 
+  /**
+   * Called when a message is received
+   *
+   * @param address the address
+   */
   void messageReceived(String address);
 
+  /**
+   * Called whenever there is a reply failure on the event bus
+   *
+   * @param address the address
+   * @param failure the {@link io.vertx.core.eventbus.ReplyFailure}
+   */
   void replyFailure(String address, ReplyFailure failure);
 }
