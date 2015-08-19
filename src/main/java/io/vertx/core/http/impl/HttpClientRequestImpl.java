06c18a8844cfeff36f108f6d19b34e5219066c8d
Ingo Weiss <iweiss@redhat.com>
2015-08-19 11:29:46 +0100

    Tidying up the code
    Signed-off-by: Ingo Weiss <ingo@redhat.com>

--- src/main/java/io/vertx/core/http/impl/HttpClientRequestImpl.java
+++ src/main/java/io/vertx/core/http/impl/HttpClientRequestImpl.java
@@ -396,7 +396,7 @@ public class HttpClientRequestImpl implements HttpClientRequest {
 
           @Override
           public String getHeader(CharSequence headerName) {
-            return resp.getHeader(headerName.toString());
+            return resp.getHeader(headerName);
           }
 
           @Override
