8fe2e170b4ad8eb82735620ee8a530c174504364
Manish Kumar <manishk@vmware.com>
2015-08-24 19:11:10 -0400

    Revert "Implemented parameterized logging in Vertx core api."
    This reverts commit 22b9570bab4e116f5e88ed317dbb4db96383a7da.

--- src/main/java/io/vertx/core/logging/impl/SLF4JLogDelegate.java
+++ src/main/java/io/vertx/core/logging/impl/SLF4JLogDelegate.java
@@ -59,88 +59,40 @@ public class SLF4JLogDelegate implements LogDelegate{
     log(ERROR_INT, message);
   }
 
-  @Override
-  public void error(Object message, Object... params) {
-    log(ERROR_INT, message, null, params);
-  }
-
   public void error(final Object message, final Throwable t) {
     log(ERROR_INT, message, t);
-  }
-
-  @Override
-  public void error(Object message, Throwable t, Object... params) {
-    log(ERROR_INT, message, t, params);
   }
 
   public void warn(final Object message) {
     log(WARN_INT, message);
   }
 
-  @Override
-  public void warn(Object message, Object... params) {
-    log(WARN_INT, message, null, params);
-  }
-
   public void warn(final Object message, final Throwable t) {
     log(WARN_INT, message, t);
-  }
-
-  @Override
-  public void warn(Object message, Throwable t, Object... params) {
-    log(WARN_INT, message, t, params);
   }
 
   public void info(final Object message) {
     log(INFO_INT, message);
   }
 
-  @Override
-  public void info(Object message, Object... params) {
-    log(INFO_INT, message, null, params);
-  }
-
   public void info(final Object message, final Throwable t) {
     log(INFO_INT, message, t);
-  }
-
-  @Override
-  public void info(Object message, Throwable t, Object... params) {
-    log(INFO_INT, message, t, params);
   }
 
   public void debug(final Object message) {
     log(DEBUG_INT, message);
   }
 
-  public void debug(final Object message, final Object... params) {
-    log(DEBUG_INT, message, null, params);
-  }
-
   public void debug(final Object message, final Throwable t) {
     log(DEBUG_INT, message, t);
-  }
-
-  public void debug(final Object message, final Throwable t, final Object... params) {
-    log(DEBUG_INT, message, t, params);
   }
 
   public void trace(final Object message) {
     log(TRACE_INT, message);
   }
 
-  @Override
-  public void trace(Object message, Object... params) {
-    log(TRACE_INT, message, null, params);
-  }
-
   public void trace(final Object message, final Throwable t) {
     log(TRACE_INT, message, t);
-  }
-
-  @Override
-  public void trace(Object message, Throwable t, Object... params) {
-    log(TRACE_INT, message, t, params);
   }
 
   private void log(int level, Object message) {
@@ -148,10 +100,6 @@ public class SLF4JLogDelegate implements LogDelegate{
   }
 
   private void log(int level, Object message, Throwable t) {
-      log(level, message, t, null);
-  }
-
-  private void log(int level, Object message, Throwable t, final Object... params) {
     String msg = (message == null) ? "NULL" : message.toString();
 
     if (logger instanceof LocationAwareLogger) {
@@ -160,19 +108,19 @@ public class SLF4JLogDelegate implements LogDelegate{
     } else {
       switch (level) {
         case TRACE_INT:
-          logger.trace(msg, t, params);
+          logger.trace(msg, t);
           break;
         case DEBUG_INT:
-          logger.debug(msg, t, params);
+          logger.debug(msg, t);
           break;
         case INFO_INT:
-          logger.info(msg, t, params);
+          logger.info(msg, t);
           break;
         case WARN_INT:
-          logger.warn(msg, t, params);
+          logger.warn(msg, t);
           break;
         case ERROR_INT:
-          logger.error(msg, t, params);
+          logger.error(msg, t);
           break;
         default:
           throw new IllegalArgumentException("Unknown log level " + level);
