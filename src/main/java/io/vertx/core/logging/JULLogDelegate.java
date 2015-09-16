5bdcc740b919e1385bea0b95ea5e77181e64f2bd
Manish Kumar <manishk@vmware.com>
2015-09-16 16:13:47 -0400

    Revert "Implemented parameterized logging in Vertx core api"
    This reverts commit 0598be3753b75bac100c22268275df6edfc2fad3.
    
    Signed-off-by: Manish Kumar <manishk@vmware.com>

--- src/main/java/io/vertx/core/logging/JULLogDelegate.java
+++ src/main/java/io/vertx/core/logging/JULLogDelegate.java
@@ -57,97 +57,47 @@ public class JULLogDelegate implements LogDelegate {
     log(Level.SEVERE, message);
   }
 
-    @Override
-    public void error(Object message, Object... params) {
-        log(Level.SEVERE, message, null, params);
-    }
-
-    public void error(final Object message, final Throwable t) {
+  public void error(final Object message, final Throwable t) {
     log(Level.SEVERE, message, t);
   }
 
-    @Override
-    public void error(Object message, Throwable t, Object... params) {
-        log(Level.SEVERE, message, t, params);
-    }
-
-    public void warn(final Object message) {
+  public void warn(final Object message) {
     log(Level.WARNING, message);
   }
 
-    @Override
-    public void warn(Object message, Object... params) {
-        log(Level.WARNING, message, null, params);
-    }
-
-    public void warn(final Object message, final Throwable t) {
+  public void warn(final Object message, final Throwable t) {
     log(Level.WARNING, message, t);
   }
 
-    @Override
-    public void warn(Object message, Throwable t, Object... params) {
-        log(Level.WARNING, message, t, params);
-    }
-
-    public void info(final Object message) {
+  public void info(final Object message) {
     log(Level.INFO, message);
   }
 
-    @Override
-    public void info(Object message, Object... params) {
-        log(Level.INFO, message, null, params);
-    }
-
-    public void info(final Object message, final Throwable t) {
+  public void info(final Object message, final Throwable t) {
     log(Level.INFO, message, t);
   }
 
-    @Override
-    public void info(Object message, Throwable t, Object... params) {
-        log(Level.INFO, message, t, params);
-    }
-
-    public void debug(final Object message) {
+  public void debug(final Object message) {
     log(Level.FINE, message);
   }
 
-    @Override
-    public void debug(Object message, Object... params) {
-        log(Level.FINE, message, null, params);
-    }
-
-    public void debug(final Object message, final Throwable t) {
+  public void debug(final Object message, final Throwable t) {
     log(Level.FINE, message, t);
   }
 
-    @Override
-    public void debug(Object message, Throwable t, Object... params) {
-        log(Level.FINE, message, t, params);
-    }
-
-    public void trace(final Object message) {
+  public void trace(final Object message) {
     log(Level.FINEST, message);
   }
 
-    @Override
-    public void trace(Object message, Object... params) {
-        log(Level.FINEST, message, null, params);
-    }
-
-    public void trace(final Object message, final Throwable t) {
+  public void trace(final Object message, final Throwable t) {
     log(Level.FINEST, message, t);
   }
 
-    @Override
-    public void trace(Object message, Throwable t, Object... params) {
-        log(Level.FINEST, message, t, params);
-    }
-
-    private void log(Level level, Object message) {
+  private void log(Level level, Object message) {
     log(level, message, null);
   }
 
-  private void log(Level level, Object message, Throwable t, Object... params) {
+  private void log(Level level, Object message, Throwable t) {
     if (!logger.isLoggable(level)) {
       return;
     }
@@ -158,12 +108,7 @@ public class JULLogDelegate implements LogDelegate {
     // This will disable stack trace lookup inside JUL. If someone wants location info, they can use their own formatter
     // or use a different logging framework like sl4j, or log4j
     record.setSourceClassName(null);
-    record.setParameters(params);
 
     logger.log(record);
   }
-
-    private void log(Level level, Object message, Throwable t) {
-        log(level, message, t, null);
-    }
 }
