33ebed901dd4cca2b9c120afb0f9f990819918e9
Julien Viet <julien@julienviet.com>
2016-06-09 15:27:42 +0200

    Make the delivery options headers compile with codegen map entry header support in data object

--- src/main/java/io/vertx/core/eventbus/DeliveryOptions.java
+++ src/main/java/io/vertx/core/eventbus/DeliveryOptions.java
@@ -17,6 +17,7 @@
 package io.vertx.core.eventbus;
 
 import io.vertx.codegen.annotations.DataObject;
+import io.vertx.codegen.annotations.GenIgnore;
 import io.vertx.core.MultiMap;
 import io.vertx.core.http.CaseInsensitiveHeaders;
 import io.vertx.core.impl.Arguments;
@@ -153,6 +154,7 @@ public class DeliveryOptions {
    * @param headers  the headers
    * @return  a reference to this, so the API can be used fluently
    */
+  @GenIgnore
   public DeliveryOptions setHeaders(MultiMap headers) {
     this.headers = headers;
     return this;
@@ -163,6 +165,7 @@ public class DeliveryOptions {
    *
    * @return  the headers
    */
+  @GenIgnore
   public MultiMap getHeaders() {
     return headers;
   }
