c6e24a9edf70ba65352e1c41005c1abad738c7c4
Clement Escoffier <clement.escoffier@gmail.com>
2016-07-22 16:09:37 +0200

    Fix https://github.com/vert-x3/issues/issues/156
    Add support for absolute path pattern in the redeploy commands
    Fix a race condition in the modification test
    
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

---
+++ src/test/java/io/vertx/core/impl/launcher/commands/WatcherAbsolutePathTest.java
@@ -0,0 +1,59 @@
+/*
+ *  Copyright (c) 2011-2015 The original author or authors
+ *  ------------------------------------------------------
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  and Apache License v2.0 which accompanies this distribution.
+ *
+ *       The Eclipse Public License is available at
+ *       http://www.eclipse.org/legal/epl-v10.html
+ *
+ *       The Apache License v2.0 is available at
+ *       http://www.opensource.org/licenses/apache2.0.php
+ *
+ *  You may elect to redistribute this code under either of these licenses.
+ */
+
+package io.vertx.core.impl.launcher.commands;
+
+import org.junit.Before;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Test the watching service behavior when using absolute path in the include list.
+ *
+ * @author <a href="http://escoffier.me">Clement Escoffier</a>
+ */
+public class WatcherAbsolutePathTest extends WatcherTest {
+
+  @Before
+  public void prepare() {
+    root = new File("target/junk/watcher");
+    deleteRecursive(root);
+    root.mkdirs();
+
+    deploy = new AtomicInteger();
+    undeploy = new AtomicInteger();
+
+    watcher = new Watcher(root, Collections.unmodifiableList(
+        Arrays.asList(
+            root.getAbsolutePath() + File.separator + "**" + File.separator + "*.txt",
+            root.getAbsolutePath() + File.separator + "windows\\*.win",
+            root.getAbsolutePath() + File.separator + "unix/*.nix",
+            "FOO.bar")), next -> {
+      deploy.incrementAndGet();
+      if (next != null) {
+        next.handle(null);
+      }
+    }, next -> {
+      undeploy.incrementAndGet();
+      if (next != null) {
+        next.handle(null);
+      }
+    }, null, 10, 10);
+  }
+}
