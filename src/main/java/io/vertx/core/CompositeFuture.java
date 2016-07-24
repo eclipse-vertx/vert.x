7dc5f3c1578e97d423f6fed5d2993e82709a6c34
Arnold Schrijver <arnold_schrijver@yahoo.com>
2016-07-24 17:21:10 +0200

    Added CompositeFuture.all() method overloads with collectResults parameter

--- src/main/java/io/vertx/core/CompositeFuture.java
+++ src/main/java/io/vertx/core/CompositeFuture.java
@@ -43,35 +43,35 @@ public interface CompositeFuture extends Future<CompositeFuture> {
    * @return the composite future
    */
   static <T1, T2> CompositeFuture all(Future<T1> f1, Future<T2> f2) {
-    return CompositeFutureImpl.all(f1, f2);
+    return CompositeFutureImpl.all(false, f1, f2);
   }
 
   /**
    * Like {@link #all(Future, Future)} but with 3 futures.
    */
   static <T1, T2, T3> CompositeFuture all(Future<T1> f1, Future<T2> f2, Future<T3> f3) {
-    return CompositeFutureImpl.all(f1, f2, f3);
+    return CompositeFutureImpl.all(false, f1, f2, f3);
   }
 
   /**
    * Like {@link #all(Future, Future)} but with 4 futures.
    */
   static <T1, T2, T3, T4> CompositeFuture all(Future<T1> f1, Future<T2> f2, Future<T3> f3, Future<T4> f4) {
-    return CompositeFutureImpl.all(f1, f2, f3, f4);
+    return CompositeFutureImpl.all(false, f1, f2, f3, f4);
   }
 
   /**
    * Like {@link #all(Future, Future)} but with 5 futures.
    */
   static <T1, T2, T3, T4, T5> CompositeFuture all(Future<T1> f1, Future<T2> f2, Future<T3> f3, Future<T4> f4, Future<T5> f5) {
-    return CompositeFutureImpl.all(f1, f2, f3, f4, f5);
+    return CompositeFutureImpl.all(false, f1, f2, f3, f4, f5);
   }
 
   /**
    * Like {@link #all(Future, Future)} but with 6 futures.
    */
   static <T1, T2, T3, T4, T5, T6> CompositeFuture all(Future<T1> f1, Future<T2> f2, Future<T3> f3, Future<T4> f4, Future<T5> f5, Future<T6> f6) {
-    return CompositeFutureImpl.all(f1, f2, f3, f4, f5, f6);
+    return CompositeFutureImpl.all(false, f1, f2, f3, f4, f5, f6);
   }
 
   /**
@@ -80,7 +80,64 @@ public interface CompositeFuture extends Future<CompositeFuture> {
    * When the list is empty, the returned future will be already completed.
    */
   static CompositeFuture all(List<Future> futures) {
-    return CompositeFutureImpl.all(futures.toArray(new Future[futures.size()]));
+    return CompositeFutureImpl.all(false, futures.toArray(new Future[futures.size()]));
+  }
+
+  /**
+   * Return a composite future, succeeded when all futures are succeeded, failed when any future is failed.
+   * <p/>
+   * The returned future fails if either {@code f1} or {@code f2} fails. The {@code collectResults} flag determines how
+   * the composite future completes.
+   * <p/>
+   * If {@code collectResults = false} the composite future fails as soon as either of its futures fails. This is the
+   * default and is equivalent to {@link #all(Future, Future)} without the parameter. On the other hand if
+   * {@code collectResults = true} the composite feature completes only when both {@code f1} and {@code f2} have
+   * completed, thereby allowing inspection of all failures, not just the first.
+   *
+   * @param collectResults whether to collect results or not
+   * @param f1 future
+   * @param f2 future
+   * @return the composite future
+   */
+  static <T1, T2> CompositeFuture all(boolean collectResults, Future<T1> f1, Future<T2> f2) {
+    return CompositeFutureImpl.all(collectResults, f1, f2);
+  }
+
+  /**
+   * Like {@link #all(boolean, Future, Future, Future)} but with 3 futures.
+   */
+  static <T1, T2, T3> CompositeFuture all(boolean collectResults, Future<T1> f1, Future<T2> f2, Future<T3> f3) {
+    return CompositeFutureImpl.all(collectResults, f1, f2, f3);
+  }
+
+  /**
+   * Like {@link #all(boolean, Future, Future, Future)} but with 4 futures.
+   */
+  static <T1, T2, T3, T4> CompositeFuture all(boolean collectResults, Future<T1> f1, Future<T2> f2, Future<T3> f3, Future<T4> f4) {
+    return CompositeFutureImpl.all(collectResults, f1, f2, f3, f4);
+  }
+
+  /**
+   * Like {@link #all(boolean, Future, Future, Future)} but with 5 futures.
+   */
+  static <T1, T2, T3, T4, T5> CompositeFuture all(boolean collectResults, Future<T1> f1, Future<T2> f2, Future<T3> f3, Future<T4> f4, Future<T5> f5) {
+    return CompositeFutureImpl.all(collectResults, f1, f2, f3, f4, f5);
+  }
+
+  /**
+   * Like {@link #all(boolean, Future, Future, Future)} but with 6 futures.
+   */
+  static <T1, T2, T3, T4, T5, T6> CompositeFuture all(boolean collectResults, Future<T1> f1, Future<T2> f2, Future<T3> f3, Future<T4> f4, Future<T5> f5, Future<T6> f6) {
+    return CompositeFutureImpl.all(collectResults, f1, f2, f3, f4, f5, f6);
+  }
+
+  /**
+   * Like {@link #all(boolean, Future, Future, Future)} but with a list of futures.<p>
+   *
+   * When the list is empty, the returned future will be already completed.
+   */
+  static CompositeFuture all(boolean collectResults, List<Future> futures) {
+    return CompositeFutureImpl.all(collectResults, futures.toArray(new Future[futures.size()]));
   }
 
   /**
@@ -93,35 +150,35 @@ public interface CompositeFuture extends Future<CompositeFuture> {
    * @return the composite future
    */
   static <T1, T2> CompositeFuture any(Future<T1> f1, Future<T2> f2) {
-    return CompositeFutureImpl.any(f1, f2);
+    return CompositeFutureImpl.any(false, f1, f2);
   }
 
   /**
    * Like {@link #any(Future, Future)} but with 3 futures.
    */
   static <T1, T2, T3> CompositeFuture any(Future<T1> f1, Future<T2> f2, Future<T3> f3) {
-    return CompositeFutureImpl.any(f1, f2, f3);
+    return CompositeFutureImpl.any(false, f1, f2, f3);
   }
 
   /**
    * Like {@link #any(Future, Future)} but with 4 futures.
    */
   static <T1, T2, T3, T4> CompositeFuture any(Future<T1> f1, Future<T2> f2, Future<T3> f3, Future<T4> f4) {
-    return CompositeFutureImpl.any(f1, f2, f3, f4);
+    return CompositeFutureImpl.any(false, f1, f2, f3, f4);
   }
 
   /**
    * Like {@link #any(Future, Future)} but with 5 futures.
    */
   static <T1, T2, T3, T4, T5> CompositeFuture any(Future<T1> f1, Future<T2> f2, Future<T3> f3, Future<T4> f4, Future<T5> f5) {
-    return CompositeFutureImpl.any(f1, f2, f3, f4, f5);
+    return CompositeFutureImpl.any(false, f1, f2, f3, f4, f5);
   }
 
   /**
    * Like {@link #any(Future, Future)} but with 6 futures.
    */
   static <T1, T2, T3, T4, T5, T6> CompositeFuture any(Future<T1> f1, Future<T2> f2, Future<T3> f3, Future<T4> f4, Future<T5> f5, Future<T6> f6) {
-    return CompositeFutureImpl.any(f1, f2, f3, f4, f5, f6);
+    return CompositeFutureImpl.any(false, f1, f2, f3, f4, f5, f6);
   }
 
   /**
@@ -130,7 +187,7 @@ public interface CompositeFuture extends Future<CompositeFuture> {
    * When the list is empty, the returned future will be already completed.
    */
   static CompositeFuture any(List<Future> futures) {
-    return CompositeFutureImpl.any(futures.toArray(new Future[futures.size()]));
+    return CompositeFutureImpl.any(false, futures.toArray(new Future[futures.size()]));
   }
 
   @Override
