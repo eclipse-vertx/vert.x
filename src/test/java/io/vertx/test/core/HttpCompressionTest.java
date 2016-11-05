23c6832e5d605aa85ab3fda19ff94a2dfb9a074e
Stefano Gargiulo <info@creativeprorgramming.it>
2016-11-05 17:06:29 +0100

    http compression level tests Signed-off-by:Stefano Gargiulo <info@creativeprorgramming.it>

--- src/test/java/io/vertx/test/core/HttpCompressionTest.java
+++ src/test/java/io/vertx/test/core/HttpCompressionTest.java
@@ -33,52 +33,25 @@ import org.junit.Test;
  */
 public class HttpCompressionTest extends HttpTestBase {
 
-    private static final String LARGE_HTML_STRING = "<!--?xml version=\"1.0\" encoding=\"ISO-8859-1\"?-->\n"
-            + "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" "
-            + "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n"
-            + "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\"><head>\n"
-            + "    <title>Apache Tomcat</title>\n"
-            + "</head>\n"
-            + '\n'
-            + "<body>\n"
-            + "<h1>It works !</h1>\n"
-            + '\n'
-            + "<p>If you're seeing this page via a web browser, it means you've setup Tomcat successfully."
-            + " Congratulations!</p>\n"
-            + " \n"
-            + "<p>This is the default Tomcat home page."
-            + " It can be found on the local filesystem at: <code>/var/lib/tomcat7/webapps/ROOT/index.html</code></p>\n"
-            + '\n'
-            + "<p>Tomcat7 veterans might be pleased to learn that this system instance of Tomcat is installed with"
-            + " <code>CATALINA_HOME</code> in <code>/usr/share/tomcat7</code> and <code>CATALINA_BASE</code> in"
-            + " <code>/var/lib/tomcat7</code>, following the rules from"
-            + " <code>/usr/share/doc/tomcat7-common/RUNNING.txt.gz</code>.</p>\n"
-            + '\n'
-            + "<p>You might consider installing the following packages, if you haven't already done so:</p>\n"
-            + '\n'
-            + "<p><b>tomcat7-docs</b>: This package installs a web application that allows to browse the Tomcat 7"
-            + " documentation locally. Once installed, you can access it by clicking <a href=\"docs/\">here</a>.</p>\n"
-            + '\n'
-            + "<p><b>tomcat7-examples</b>: This package installs a web application that allows to access the Tomcat"
-            + " 7 Servlet and JSP examples. Once installed, you can access it by clicking"
-            + " <a href=\"examples/\">here</a>.</p>\n"
-            + '\n'
-            + "<p><b>tomcat7-admin</b>: This package installs two web applications that can help managing this Tomcat"
-            + " instance. Once installed, you can access the <a href=\"manager/html\">manager webapp</a> and"
-            + " the <a href=\"host-manager/html\">host-manager webapp</a>.</p><p>\n"
-            + '\n'
-            + "</p><p>NOTE: For security reasons, using the manager webapp is restricted"
-            + " to users with role \"manager\"."
-            + " The host-manager webapp is restricted to users with role \"admin\". Users are "
-            + "defined in <code>/etc/tomcat7/tomcat-users.xml</code>.</p>\n"
-            + '\n'
-            + '\n'
-            + '\n'
-            + "</body></html>";
+    private static final String COMPRESS_TEST_STRING = "/*\n" +
+" * Copyright (c) 2011-2014 The original author or authors\n" +
+" * ------------------------------------------------------\n" +
+" * All rights reserved. This program and the accompanying materials\n" +
+" * are made available under the terms of the Eclipse Public License v1.0\n" +
+" * and Apache License v2.0 which accompanies this distribution.\n" +
+" *\n" +
+" *     The Eclipse Public License is available at\n" +
+" *     http://www.eclipse.org/legal/epl-v10.html\n" +
+" *\n" +
+" *     The Apache License v2.0 is available at\n" +
+" *     http://www.opensource.org/licenses/apache2.0.php\n" +
+" *\n" +
+" * You may elect to redistribute this code under either of these licenses.\n" +
+" */";
 
     HttpServer serverWithMinCompressionLevel, serverWithMaxCompressionLevel = null;
 
-    HttpClient client2, clientraw, clientraw2 = null;
+    HttpClient clientraw = null;
 
     public void setUp() throws Exception {
         super.setUp();
@@ -99,7 +72,7 @@ public class HttpCompressionTest extends HttpTestBase {
             assertEquals(2, req.headers().size());
             //  assertEquals("localhost:" + DEFAULT_HTTP_PORT, req.headers().get("host"));
             assertNotNull(req.headers().get("Accept-Encoding"));
-            req.response().end(Buffer.buffer(LARGE_HTML_STRING).toString(CharsetUtil.UTF_8));
+            req.response().end(Buffer.buffer(COMPRESS_TEST_STRING).toString(CharsetUtil.UTF_8));
         };
 
         serverWithMinCompressionLevel.requestHandler(requestHandler);
@@ -125,7 +98,7 @@ public class HttpCompressionTest extends HttpTestBase {
                 resp -> {
                     resp.bodyHandler(responseBuffer -> {
                         String responseBody = responseBuffer.toString(CharsetUtil.UTF_8);
-                        assertEquals(LARGE_HTML_STRING, responseBody);
+                        assertEquals(COMPRESS_TEST_STRING, responseBody);
                         minCompressionTestPassed = true;
                         terminateTestWhenAllPassed();
                     });
@@ -139,7 +112,7 @@ public class HttpCompressionTest extends HttpTestBase {
                 resp -> {
                     resp.bodyHandler(responseBuffer -> {
                         String responseBody = responseBuffer.toString(CharsetUtil.UTF_8);
-                        assertEquals(LARGE_HTML_STRING, responseBody);
+                        assertEquals(COMPRESS_TEST_STRING, responseBody);
                         maxCompressionTestPassed = true;
                         terminateTestWhenAllPassed();
                     });
