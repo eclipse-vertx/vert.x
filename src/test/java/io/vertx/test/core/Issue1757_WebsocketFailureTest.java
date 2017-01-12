6cb2b950c0f746e37d0e8033f9125f4c5810a1fa
Jonas Berlin <xkr47@outerspace.dyndns.org>
2017-01-12 21:48:02 +0200

    Remove System.out from test code
    Signed-off-by: Jonas Berlin <xkr47@outerspace.dyndns.org>

--- src/test/java/io/vertx/test/core/Issue1757_WebsocketFailureTest.java
+++ src/test/java/io/vertx/test/core/Issue1757_WebsocketFailureTest.java
@@ -12,7 +12,6 @@ import java.io.UnsupportedEncodingException;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
-import java.util.regex.Pattern;
 
 public class Issue1757_WebsocketFailureTest {
     final BlockingQueue<Throwable> resultQueue = new ArrayBlockingQueue<Throwable>(10);
@@ -46,7 +45,6 @@ public class Issue1757_WebsocketFailureTest {
                     sock.handler(b -> {
                         fullReq.appendBuffer(b);
                         String reqPart = b.toString();
-                        System.out.println(Pattern.compile("^", Pattern.MULTILINE).matcher(reqPart.replace("\r", "\\r").replace("\n", "\\n\n")).replaceAll("REQ: "));
                         if (fullReq.toString().contains("\r\n\r\n")) {
                             try {
                                 String content = "0123456789";
@@ -57,7 +55,6 @@ public class Issue1757_WebsocketFailureTest {
                                     resp += "Connection: close\r\n";
                                 }
                                 resp += "Content-Length: 100\r\n\r\n" + content;
-                                System.out.println(Pattern.compile("^", Pattern.MULTILINE).matcher(resp.replace("\r", "\\r").replace("\n", "\\n\n")).replaceAll("RSP: "));
                                 sock.write(Buffer.buffer(resp.getBytes("ASCII")));
                             } catch (UnsupportedEncodingException e) {
                                 addResult(e);
@@ -65,7 +62,6 @@ public class Issue1757_WebsocketFailureTest {
                         }
                     });
                     sock.closeHandler(v -> {
-                        System.out.println("Server got close");
                         addResult(serverGotCloseException);
                     });
                 }).listen(ar -> {
@@ -76,20 +72,17 @@ public class Issue1757_WebsocketFailureTest {
                     NetServer server = ar.result();
                     int port = server.actualPort();
 
-                    System.out.println("client keepalive = " + keepAliveInOptions);
                     HttpClientOptions opts = new HttpClientOptions().setKeepAlive(keepAliveInOptions);
                     vertx.createHttpClient(opts).websocket(port, "localhost", "/", ws -> {
                         addResult(new AssertionError("Websocket unexpectedly connected"));
                         ws.close();
                     }, t -> {
-                        System.out.println("Client got exception");
                         addResult(t);
                     });
                 });
             }
         });
         try {
-            System.out.println("Test thread waiting for resultQueue");
             boolean serverGotClose = false;
             boolean clientGotCorrectException = false;
             while (!serverGotClose || !clientGotCorrectException) {
@@ -97,17 +90,14 @@ public class Issue1757_WebsocketFailureTest {
                 if (result == null) {
                     throw new AssertionError("Timed out waiting for expected state, current: serverGotClose = " + serverGotClose + ", clientGotCorrectException = " + clientGotCorrectException);
                 } else if (result == serverGotCloseException) {
-                    System.out.println("ACK Server got close");
                     serverGotClose = true;
                 } else if (result instanceof WebSocketHandshakeException
                         && result.getMessage().equals("Websocket connection attempt returned HTTP status code 200")) {
                     clientGotCorrectException = true;
-                    System.out.println("ACK Client got correct exception");
                 } else {
                     throw result;
                 }
             }
-            System.out.println("Success!");
         } finally {
             vertx.close();
         }
