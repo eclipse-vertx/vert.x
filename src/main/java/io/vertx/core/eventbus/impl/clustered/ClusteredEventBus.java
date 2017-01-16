dcee9a41ea9704c30fb3fb4f18c25a7cf327402a
Thomas Segismont <tsegismont@gmail.com>
2017-01-16 18:39:07 +0100

    Minor, comment update
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- src/main/java/io/vertx/core/eventbus/impl/clustered/ClusteredEventBus.java
+++ src/main/java/io/vertx/core/eventbus/impl/clustered/ClusteredEventBus.java
@@ -245,6 +245,7 @@ public class ClusteredEventBus extends EventBusImpl {
     Context context = Vertx.currentContext();
     if (context == null || !context.isEventLoopContext()) {
       // Guarantees the order when there is no current context
+      // Or, when there is a worker context, makes sure message can be sent instantly
       sendNoContext.runOnContext(v -> {
         subs.get(address, resultHandler);
       });
