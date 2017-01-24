e852dde34029f705d53a594282682216801329e3
stecolet@gmail.com <stefano.coletta@hubparking.com>
2017-01-24 17:14:31 +0100

    Added unmasked frame supporting for websocket, client and server both. See https://tools.ietf.org/html/rfc6455#section-5.2
    Signed-off-by: stecolet@gmail.com <stefano.coletta@hubparking.com>

--- src/main/java/io/vertx/core/http/HttpServerOptions.java
+++ src/main/java/io/vertx/core/http/HttpServerOptions.java
@@ -106,6 +106,7 @@ public class HttpServerOptions extends NetServerOptions {
   private List<HttpVersion> alpnVersions;
   private int http2ConnectionWindowSize;
   private boolean decompressionSupported;
+  private boolean unmaskedFrame;
 
   /**
    * Default constructor
@@ -135,6 +136,7 @@ public class HttpServerOptions extends NetServerOptions {
     this.alpnVersions = other.alpnVersions != null ? new ArrayList<>(other.alpnVersions) : null;
     this.http2ConnectionWindowSize = other.http2ConnectionWindowSize;
     this.decompressionSupported = other.isDecompressionSupported();
+    this.unmaskedFrame = other.isUnmaskedFrame();
   }
 
   /**
@@ -405,6 +407,25 @@ public class HttpServerOptions extends NetServerOptions {
    */
   public int getMaxWebsocketFrameSize() {
     return maxWebsocketFrameSize;
+  }
+
+  /**
+   * Is Unmasking frame enabled. It's false as default
+   * @return
+   */
+  public boolean isUnmaskedFrame() {
+    return unmaskedFrame;
+  }
+
+  /**
+   * Set whether unmasking frame is enabled
+   *
+   * @param unmaskedFrame  true if enabled
+   * @return a reference to this, so the API can be used fluently
+   */
+  public HttpServerOptions setUnmaskedFrame(boolean unmaskedFrame) {
+    this.unmaskedFrame = unmaskedFrame;
+    return this;
   }
 
   /**
