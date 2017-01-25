fd9a2f569bc3247fb5c4a8ac3e09451ad3b988be
stecolet@gmail.com <stecolet@gmail.com>
2017-01-25 08:51:38 +0100

    Revert "edit user.email git property"
    This reverts commit 08467fdd8ec4513a416248d4870eb7d18477b732.

--- src/test/java/io/vertx/test/core/WebsocketTest.java
+++ src/test/java/io/vertx/test/core/WebsocketTest.java
@@ -1247,6 +1247,7 @@ public class WebsocketTest extends VertxTestBase {
         fail("Cannot decode unmasked message because I require masked frame as configured");
       });
     });
+
     server.listen(onSuccess(server -> {
       client.websocket(HttpTestBase.DEFAULT_HTTP_PORT, HttpTestBase.DEFAULT_HTTP_HOST, "/", ws -> {
         ws.writeFinalTextFrame("first unmasked frame");
