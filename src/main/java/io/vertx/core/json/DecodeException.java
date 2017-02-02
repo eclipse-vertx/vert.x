1fe004b8b64c4771bd82e250cefb68fc7496ce75
Søren Buron <sorenburon@gmail.com>
2017-02-02 10:56:01 +0100

    Add cause to DecodeException
    Signed-off-by: Søren Buron <sorenburon@gmail.com>

--- src/main/java/io/vertx/core/json/DecodeException.java
+++ src/main/java/io/vertx/core/json/DecodeException.java
@@ -29,4 +29,8 @@ public class DecodeException extends RuntimeException {
   public DecodeException(String message) {
     super(message);
   }
+
+  public DecodeException(String message, Throwable cause) {
+    super(message, cause);
+  }
 }
