d50a1906ca910a1b81d53ddd4c887aa0481406ff
Julien Viet <julien@julienviet.com>
2017-02-07 09:41:50 +0100

    Add JMH empty benchmark

---
+++ src/test/benchmarks/io/vertx/benchmarks/EmptyBenchmark.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2011-2013 The original author or authors
+ * ------------------------------------------------------
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ *
+ *     The Eclipse Public License is available at
+ *     http://www.eclipse.org/legal/epl-v10.html
+ *
+ *     The Apache License v2.0 is available at
+ *     http://www.opensource.org/licenses/apache2.0.php
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ */
+package io.vertx.benchmarks;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+
+/**
+ * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
+ */
+@State(Scope.Thread)
+public class EmptyBenchmark {
+
+  @Benchmark
+  public void baseline(Blackhole blackhole) {
+    blackhole.consume("whatever");
+  }
+}
