ae18df334c2fec35b0e28e07e805c387ac6146ab
Clement Escoffier <clement.escoffier@gmail.com>
2017-05-08 13:31:25 +0200

    Fix #1972
    Add:
    * beforeStoppingVertx - called during the close method of vert.x (CloseHook)
    * afterStoppingVertx - called once vert.x has been closed
    
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

--- src/main/java/io/vertx/core/impl/launcher/VertxLifecycleHooks.java
+++ src/main/java/io/vertx/core/impl/launcher/VertxLifecycleHooks.java
@@ -28,32 +28,49 @@ import io.vertx.core.json.JsonObject;
 public interface VertxLifecycleHooks {
 
   /**
-   * Hook for sub-classes of the starter class before the vertx instance is started. Options can still be updated.
+   * Hook for sub-classes of the {@link io.vertx.core.Launcher} class before the vertx instance is started. Options
+   * can still be updated.
    *
    * @param config the json config file passed via -conf on the command line, an empty json object is not set.
    */
   void afterConfigParsed(JsonObject config);
 
   /**
-   * Hook for sub-classes of the starter class before the vertx instance is started. Options can still be updated.
+   * Hook for sub-classes of the {@link io.vertx.core.Launcher} class before the vertx instance is started. Options
+   * can still be updated.
    *
    * @param options the vert.x options
    */
   void beforeStartingVertx(VertxOptions options);
 
   /**
-   * Hook for sub-classes of the starter class after the vertx instance is started.
+   * Hook for sub-classes of the {@link io.vertx.core.Launcher} class after the vertx instance is started.
    *
    * @param vertx the vert.x instance
    */
   void afterStartingVertx(Vertx vertx);
 
   /**
-   * Hook for sub classes of the starter class before the verticle is deployed. Deployment options can still be updated.
+   * Hook for sub classes of the {@link io.vertx.core.Launcher} class before the verticle is deployed. Deployment
+   * options can still be updated.
    *
    * @param deploymentOptions the deployment options
    */
   void beforeDeployingVerticle(DeploymentOptions deploymentOptions);
+
+  /**
+   * Hook for sub classes of the {@link io.vertx.core.Launcher} class called before the {@link Vertx} instance is
+   * terminated. The hook is called during the {@link Vertx#close()} method.
+   *
+   * @param vertx the {@link Vertx} instance, cannot be {@code null}
+   */
+  void beforeStoppingVertx(Vertx vertx);
+
+  /**
+   * Hook for sub classes of the {@link io.vertx.core.Launcher} class called after the {@link Vertx} instance has been
+   * terminated. The hook is called after the {@link Vertx#close()} method.
+   */
+  void afterStoppingVertx();
 
   /**
    * A deployment failure has been encountered. You can override this method to customize the behavior.
