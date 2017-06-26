7e3a01450f3e1593990e61936c9aece74411e6ff
Julien Viet <julien@julienviet.com>
2017-06-26 09:32:25 +0200

    Clarify the metrics SPI returned objects

--- src/main/java/io/vertx/core/spi/metrics/VertxMetrics.java
+++ src/main/java/io/vertx/core/spi/metrics/VertxMetrics.java
@@ -101,7 +101,7 @@ public interface VertxMetrics extends Metrics, Measured {
    * @param server       the Vert.x http server
    * @param localAddress localAddress the local address the net socket is listening on
    * @param options      the options used to create the {@link io.vertx.core.http.HttpServer}
-   * @return the http server metrics SPI
+   * @return the http server metrics SPI or null when metrics are disabled
    */
   HttpServerMetrics<?, ?, ?> createMetrics(HttpServer server, SocketAddress localAddress, HttpServerOptions options);
 
@@ -112,7 +112,7 @@ public interface VertxMetrics extends Metrics, Measured {
    *
    * @param client  the Vert.x http client
    * @param options the options used to create the {@link io.vertx.core.http.HttpClient}
-   * @return the http client metrics SPI
+   * @return the http client metrics SPI or null when metrics are disabled
    */
   HttpClientMetrics<?, ?, ?, ?, ?> createMetrics(HttpClient client, HttpClientOptions options);
 
@@ -128,7 +128,7 @@ public interface VertxMetrics extends Metrics, Measured {
    *
    * @param localAddress localAddress the local address the net socket is listening on
    * @param options      the options used to create the {@link NetServer}
-   * @return the net server metrics SPI
+   * @return the net server metrics SPI or null when metrics are disabled
    */
   TCPMetrics<?> createMetrics(SocketAddress localAddress, NetServerOptions options);
 
@@ -138,7 +138,7 @@ public interface VertxMetrics extends Metrics, Measured {
    * No specific thread and context can be expected when this method is called.
    *
    * @param options the options used to create the {@link NetClient}
-   * @return the net client metrics SPI
+   * @return the net client metrics SPI or null when metrics are disabled
    */
   TCPMetrics<?> createMetrics(NetClientOptions options);
 
@@ -170,7 +170,8 @@ public interface VertxMetrics extends Metrics, Measured {
    * @param pool the pool of resource, it can be used by the metrics implementation to gather extra statistics
    * @param poolType the type of the pool e.g worker, datasource, etc..
    * @param poolName the name of the pool
-   * @param maxPoolSize the pool max size, or -1 if the number cannot be determined   @return the thread pool metrics SPI
+   * @param maxPoolSize the pool max size, or -1 if the number cannot be determined
+   * @return the thread pool metrics SPI  or null when metrics are disabled
    */
   <P> PoolMetrics<?> createMetrics(P pool, String poolType, String poolName, int maxPoolSize);
 }
