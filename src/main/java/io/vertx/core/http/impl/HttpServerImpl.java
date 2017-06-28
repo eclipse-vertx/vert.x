fa761c983bade24ff4b79fa69d37f66f20ef12c5
Julien Viet <julien@julienviet.com>
2017-06-28 21:06:19 +0200

    Better handle incorrect websocket invalid HTTP request failures

--- src/main/java/io/vertx/core/http/impl/HttpServerImpl.java
+++ src/main/java/io/vertx/core/http/impl/HttpServerImpl.java
@@ -586,6 +586,8 @@ public class HttpServerImpl implements HttpServer, Closeable, MetricsProvider {
 
     private boolean closeFrameSent;
     private FullHttpRequest wsRequest;
+    private HttpResponseStatus handshakeErrorStatus;
+    private String handshakeErrorMsg;
 
     public ServerHandlerWithWebSockets(SSLHelper sslHelper, HttpServerOptions options, String serverOrigin, HandlerHolder<HttpHandlers> holder, HttpServerMetrics metrics) {
       super(sslHelper, options, serverOrigin, holder, metrics);
@@ -600,16 +602,19 @@ public class HttpServerImpl implements HttpServer, Closeable, MetricsProvider {
         if (log.isTraceEnabled()) log.trace("Server received request: " + request.getUri());
 
         if (request.headers().contains(io.vertx.core.http.HttpHeaders.UPGRADE, io.vertx.core.http.HttpHeaders.WEBSOCKET, true)) {
+
           // As a fun part, Firefox 6.0.2 supports Websockets protocol '7'. But,
           // it doesn't send a normal 'Connection: Upgrade' header. Instead it
           // sends: 'Connection: keep-alive, Upgrade'. Brilliant.
           String connectionHeader = request.headers().get(io.vertx.core.http.HttpHeaders.CONNECTION);
           if (connectionHeader == null || !connectionHeader.toLowerCase().contains("upgrade")) {
-            sendError("\"Connection\" must be \"Upgrade\".", BAD_REQUEST, ch);
+            handshakeErrorStatus = BAD_REQUEST;
+            handshakeErrorMsg = "\"Connection\" must be \"Upgrade\".";
             return;
           }
 
           if (request.getMethod() != HttpMethod.GET) {
+            handshakeErrorStatus = METHOD_NOT_ALLOWED;
             sendError(null, METHOD_NOT_ALLOWED, ch);
             return;
           }
@@ -662,6 +667,13 @@ public class HttpServerImpl implements HttpServer, Closeable, MetricsProvider {
             handshake(conn, req, ch, chctx);
             return;
           }
+        } else if (handshakeErrorStatus != null) {
+          if (msg instanceof LastHttpContent) {
+            sendError(handshakeErrorMsg, handshakeErrorStatus, ch);
+            handshakeErrorMsg = null;
+            handshakeErrorMsg = null;
+          }
+          return;
         }
         conn.handleMessage(msg);
       } else {
