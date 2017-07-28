9d6fbd4c4057213ca89d68b2703fcec4c240af72
Thomas Segismont <tsegismont@gmail.com>
2017-07-28 16:03:22 +0200

    Fixes #2079 JSON message codecs creates unnecessary intermediate String
    With this change, codecs encode to and decode from Buffer directly.
    
    The benchmark results are the following:
    
    Benchmark                            Mode  Cnt    Score    Error   Units
    JsonDecodeBenchmark.directLarge     thrpt   10   99,408 ±  0,592  ops/ms
    JsonDecodeBenchmark.directSmall     thrpt   10  570,768 ±  3,802  ops/ms
    JsonDecodeBenchmark.viaStringLarge  thrpt   10   79,384 ±  0,715  ops/ms
    JsonDecodeBenchmark.viaStringSmall  thrpt   10  430,398 ± 13,331  ops/ms
    JsonEncodeBenchmark.directLarge     thrpt   10  147,995 ±  1,050  ops/ms
    JsonEncodeBenchmark.directSmall     thrpt   10  817,803 ±  5,216  ops/ms
    JsonEncodeBenchmark.viaStringLarge  thrpt   10  111,845 ±  1,348  ops/ms
    JsonEncodeBenchmark.viaStringSmall  thrpt   10  623,426 ±  4,568  ops/ms
    
    Benefits range from +25% to 32%, depending on whether you are encoding/decoding a small/large payload.
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- src/main/java/io/vertx/core/eventbus/impl/codecs/JsonObjectMessageCodec.java
+++ src/main/java/io/vertx/core/eventbus/impl/codecs/JsonObjectMessageCodec.java
@@ -16,7 +16,6 @@
 
 package io.vertx.core.eventbus.impl.codecs;
 
-import io.netty.util.CharsetUtil;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.eventbus.MessageCodec;
 import io.vertx.core.json.JsonObject;
@@ -28,20 +27,16 @@ public class JsonObjectMessageCodec implements MessageCodec<JsonObject, JsonObje
 
   @Override
   public void encodeToWire(Buffer buffer, JsonObject jsonObject) {
-    String strJson = jsonObject.encode();
-    byte[] encoded = strJson.getBytes(CharsetUtil.UTF_8);
-    buffer.appendInt(encoded.length);
-    Buffer buff = Buffer.buffer(encoded);
-    buffer.appendBuffer(buff);
+    Buffer encoded = jsonObject.toBuffer();
+    buffer.appendInt(encoded.length());
+    buffer.appendBuffer(encoded);
   }
 
   @Override
   public JsonObject decodeFromWire(int pos, Buffer buffer) {
     int length = buffer.getInt(pos);
     pos += 4;
-    byte[] encoded = buffer.getBytes(pos, pos + length);
-    String str = new String(encoded, CharsetUtil.UTF_8);
-    return new JsonObject(str);
+    return new JsonObject(buffer.slice(pos, pos + length));
   }
 
   @Override
