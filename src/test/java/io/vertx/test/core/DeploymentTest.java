84069362db3128c2de89046d36ab562699566895
Julien Viet <julien@julienviet.com>
2017-08-04 09:34:07 +0200

    Revert "Merge pull request #2082 from tsegismont/easy-java-verticle"
    This reverts commit b731ad8edda5a56e3679989c6aee3a28190be122, reversing
    changes made to c41d4c85f14393d96edbd3dea5d37418e982b6e0.

--- src/test/java/io/vertx/test/core/DeploymentTest.java
+++ src/test/java/io/vertx/test/core/DeploymentTest.java
@@ -41,8 +41,6 @@ import java.net.URLClassLoader;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -54,7 +52,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
-import java.util.function.Supplier;
 
 /**
  *
@@ -439,13 +436,6 @@ public class DeploymentTest extends VertxTestBase {
     MyVerticle verticle = new MyVerticle();
     try {
       vertx.deployVerticle(verticle, new DeploymentOptions().setWorker(false).setMultiThreaded(true), ar -> {
-      });
-      fail("Should throw exception");
-    } catch (IllegalArgumentException e) {
-      // OK
-    }
-    try {
-      vertx.deployVerticle(MyVerticle.class.getName(), new DeploymentOptions().setWorker(false).setMultiThreaded(true), ar -> {
       });
       fail("Should throw exception");
     } catch (IllegalArgumentException e) {
@@ -1267,7 +1257,7 @@ public class DeploymentTest extends VertxTestBase {
     }));
     await();
   }
-
+  
   @Test
   public void testUndeployWhenUndeployIsInProgress() throws Exception {
     int numIts = 10;
@@ -1287,80 +1277,6 @@ public class DeploymentTest extends VertxTestBase {
       });
     }
     awaitLatch(latch);
-  }
-
-  @Test
-  public void testDeploySupplier() throws Exception {
-    JsonObject config = generateJSONObject();
-    Set<MyVerticle> myVerticles = Collections.synchronizedSet(new HashSet<>());
-    vertx.deployVerticle(() -> {
-      MyVerticle myVerticle = new MyVerticle();
-      myVerticles.add(myVerticle);
-      return myVerticle;
-    }, new DeploymentOptions().setInstances(4).setConfig(config), onSuccess(deploymentId -> {
-      myVerticles.forEach(myVerticle -> {
-        assertEquals(deploymentId, myVerticle.deploymentID);
-        assertEquals(config, myVerticle.config);
-        assertTrue(myVerticle.startCalled);
-      });
-      testComplete();
-    }));
-    await();
-  }
-
-  @Test
-  public void testDeploySupplierNull() throws Exception {
-    vertx.deployVerticle(() -> null, new DeploymentOptions(), onFailure(t -> {
-      testComplete();
-    }));
-    await();
-  }
-
-  @Test
-  public void testDeploySupplierDuplicate() throws Exception {
-    MyVerticle myVerticle = new MyVerticle();
-    vertx.deployVerticle(() -> myVerticle, new DeploymentOptions().setInstances(2), onFailure(t -> {
-      assertFalse(myVerticle.startCalled);
-      testComplete();
-    }));
-    await();
-  }
-
-  @Test
-  public void testDeploySupplierDifferentClasses() throws Exception {
-    AtomicInteger idx = new AtomicInteger();
-    MyVerticle myVerticle = new MyVerticle();
-    MyAsyncVerticle myAsyncVerticle = new MyAsyncVerticle(null, null);
-    Supplier<Verticle> supplier = () -> idx.getAndIncrement() % 2 == 0 ? myAsyncVerticle : myAsyncVerticle;
-    vertx.deployVerticle(supplier, new DeploymentOptions().setInstances(2), onFailure(t -> {
-      assertFalse(myVerticle.startCalled);
-      testComplete();
-    }));
-    await();
-  }
-
-  @Test
-  public void testDeployClass() throws Exception {
-    JsonObject config = generateJSONObject();
-    vertx.deployVerticle(ReferenceSavingMyVerticle.class, new DeploymentOptions().setInstances(4).setConfig(config), onSuccess(deploymentId -> {
-      ReferenceSavingMyVerticle.myVerticles.forEach(myVerticle -> {
-        assertEquals(deploymentId, myVerticle.deploymentID);
-        assertEquals(config, myVerticle.config);
-        assertTrue(myVerticle.startCalled);
-      });
-      testComplete();
-    }));
-    await();
-  }
-
-  @Test
-  public void testDeployClassNoDefaultPublicConstructor() throws Exception {
-    class NoDefaultPublicConstructorVerticle extends AbstractVerticle {
-    }
-    vertx.deployVerticle(NoDefaultPublicConstructorVerticle.class, new DeploymentOptions(), onFailure(t -> {
-      testComplete();
-    }));
-    await();
   }
 
   // TODO
@@ -1420,7 +1336,7 @@ public class DeploymentTest extends VertxTestBase {
       .put("obj", new JsonObject().put("quux", "flip"));
   }
 
-  public static class MyVerticle extends AbstractVerticle {
+  public class MyVerticle extends AbstractVerticle {
 
     static final int NOOP = 0, THROW_EXCEPTION = 1, THROW_ERROR = 2;
 
@@ -1471,15 +1387,6 @@ public class DeploymentTest extends VertxTestBase {
     }
   }
 
-  public static class ReferenceSavingMyVerticle extends MyVerticle {
-    static Set<MyVerticle> myVerticles = Collections.synchronizedSet(new HashSet<>());
-
-    public ReferenceSavingMyVerticle() {
-      super();
-      myVerticles.add(this);
-    }
-  }
-
   public class MyAsyncVerticle extends AbstractVerticle {
 
     private final Consumer<Future<Void>> startConsumer;
@@ -1504,5 +1411,7 @@ public class DeploymentTest extends VertxTestBase {
       }
     }
   }
+
+
 }
 
