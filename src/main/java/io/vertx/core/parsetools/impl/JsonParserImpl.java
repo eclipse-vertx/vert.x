52bdf0f710c977bb57c4cc4be85d2d88c73d3051
Julien Viet <julien@julienviet.com>
2017-08-29 16:45:21 +0200

    Update headers date

--- src/main/java/io/vertx/core/parsetools/impl/JsonParserImpl.java
+++ src/main/java/io/vertx/core/parsetools/impl/JsonParserImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2013 The original author or authors
+ * Copyright (c) 2011-2017 The original author or authors
  * ------------------------------------------------------
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
--- src/test/java/io/vertx/test/core/JsonParserTest.java
+++ src/test/java/io/vertx/test/core/JsonParserTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Red Hat, Inc.
+ * Copyright 2017 Red Hat, Inc.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
