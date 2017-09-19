01897a4b9bccb3579ed45cda5b9a929901e04b7d
Julien Ponge <julien.ponge@gmail.com>
2017-09-19 10:10:28 +0200

    File header update based on the Eclipse Foundation guidance

--- src/main/java/io/vertx/core/http/impl/headers/VertxHttpHeaders.java
+++ src/main/java/io/vertx/core/http/impl/headers/VertxHttpHeaders.java
@@ -1,17 +1,15 @@
 /*
  * Copyright (c) 2011-2017 The original author or authors
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v2.0
- * and Apache License v2.0 which accompanies this distribution.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 and the Apache License, Version 2.0
+ * which accompanies this distribution. The Eclipse Public License 2.0 is
+ * available at http://www.eclipse.org/legal/epl-2.0.html, and the Apache
+ * License, Version 2.0 is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
- *     The Eclipse Public License is available at
- *     https://www.eclipse.org/legal/epl-2.0/
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
  *
- *     The Apache License v2.0 is available at
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * You may elect to redistribute this code under either of these licenses.
  */
 package io.vertx.core.http.impl.headers;
 
