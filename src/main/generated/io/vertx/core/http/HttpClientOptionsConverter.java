20f6f8b47542c08b78f6f40d89784bf5d524cf63
Julien Ponge <julien.ponge@gmail.com>
2017-09-21 10:53:57 +0200

    Rebasing + applying license headers
    Notice how codegen applies another license header :-)

--- src/main/generated/io/vertx/core/http/HttpClientOptionsConverter.java
+++ src/main/generated/io/vertx/core/http/HttpClientOptionsConverter.java
@@ -1,14 +1,17 @@
 /*
- * Copyright (c) 2011-2017 The original author or authors
+ * Copyright 2014 Red Hat, Inc.
  *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 and the Apache License, Version 2.0
- * which accompanies this distribution. The Eclipse Public License 2.0 is
- * available at http://www.eclipse.org/legal/epl-2.0.html, and the Apache
- * License, Version 2.0 is available at
- * https://www.apache.org/licenses/LICENSE-2.0
+ * Red Hat licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
  *
- * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  */
 
 package io.vertx.core.http;
