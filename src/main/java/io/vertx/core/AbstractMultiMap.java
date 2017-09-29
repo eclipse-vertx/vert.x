14ef87970c77ba4bde8f8b32e78cb54e08af5e34
Jess Sightler <jesse.sightler@gmail.com>
2017-09-28 21:45:07 -0400

    Issue 2096 - Fixed copyright line

--- src/main/java/io/vertx/core/AbstractMultiMap.java
+++ src/main/java/io/vertx/core/AbstractMultiMap.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 jsight.
+ * Copyright 2017 Red Hat, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
