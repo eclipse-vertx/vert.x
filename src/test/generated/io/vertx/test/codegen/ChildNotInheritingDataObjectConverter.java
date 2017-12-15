a5790d57e00d68ba62ba7fe6524fdb967cc388c3
Julien Ponge <julien.ponge@gmail.com>
2017-12-15 17:03:40 +0100

    Adding the missing (c) symbol

--- src/test/generated/io/vertx/test/codegen/ChildNotInheritingDataObjectConverter.java
+++ src/test/generated/io/vertx/test/codegen/ChildNotInheritingDataObjectConverter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Red Hat, Inc.
+ * Copyright (c) 2014 Red Hat, Inc. and others
  *
  * Red Hat licenses this file to you under the Apache License, version 2.0
  * (the "License"); you may not use this file except in compliance with the
