996e1e0496055df1ea3d0f3553224ccb8b6889eb
Sammers21 <titantins@gmail.com>
2018-01-06 14:46:49 +0300

    remove VertxSniHandler#replaceHandler
    Signed-off-by: Sammers21 <titantins@gmail.com>

--- src/main/java/io/vertx/core/net/impl/VertxSniHandler.java
+++ src/main/java/io/vertx/core/net/impl/VertxSniHandler.java
@@ -20,18 +20,13 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.ssl.SniCompletionEvent;
 import io.netty.handler.ssl.SniHandler;
-import io.netty.handler.ssl.SslContext;
-import io.netty.handler.ssl.SslHandler;
 import io.netty.util.Attribute;
 import io.netty.util.AttributeKey;
-import io.netty.util.ReferenceCountUtil;
 import io.netty.util.concurrent.DefaultPromise;
 import io.netty.util.concurrent.EventExecutor;
 import io.netty.util.concurrent.Future;
 import io.netty.util.concurrent.Promise;
 import io.vertx.core.impl.VertxInternal;
-
-import javax.net.ssl.SSLEngine;
 
 /**
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
@@ -85,33 +80,5 @@ public class VertxSniHandler extends SniHandler {
   @Override
   public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
     context = ctx;
-  }
-
-  @Override
-  protected void replaceHandler(ChannelHandlerContext ctx, String hostname, SslContext sslContext) throws Exception {
-    SslHandler sslHandler = null;
-    try {
-      SSLEngine engine = helper.createEngine(sslContext);
-      sslHandler = new SslHandler(engine);
-      ctx.pipeline().replace(this, "ssl", sslHandler);
-      Future<Channel> fut = sslHandler.handshakeFuture();
-      fut.addListener(future -> {
-        if (future.isSuccess()) {
-          Attribute<String> val = ctx.channel().attr(SERVER_NAME_ATTR);
-          val.set(hostname);
-          handshakeFuture.setSuccess(ctx.channel());
-        } else {
-          handshakeFuture.setFailure(future.cause());
-        }
-      });
-      sslHandler = null;
-    } finally {
-      // Since the SslHandler was not inserted into the pipeline the ownership of the SSLEngine was not
-      // transferred to the SslHandler.
-      // See https://github.com/netty/netty/issues/5678
-      if (sslHandler != null) {
-        ReferenceCountUtil.safeRelease(sslHandler.engine());
-      }
-    }
   }
 }
