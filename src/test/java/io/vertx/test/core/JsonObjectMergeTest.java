de80afb1bc19738a491a379b08cafae26c889af7
Roman Novikov <mystdem@gmail.com>
2018-07-04 11:40:37 +0300

    MergeIn

---
+++ src/test/java/io/vertx/test/core/JsonObjectMergeTest.java
@@ -0,0 +1,44 @@
+package io.vertx.test.core;
+
+import io.vertx.core.json.JsonObject;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ *
+ * @author <a href="https://github.com/mystdeim">Roman Novikov</a>
+ */
+public class JsonObjectMergeTest {
+
+  @Test
+  public void testSuccessful() {
+    Sample sample = new Sample();
+    JsonObject json = new JsonObject().put("id", "1").put("name", "Sam");
+
+    json.mergeIn(sample);
+    assertEquals("1", sample.id);
+    assertEquals("Sam", sample.name);
+  }
+
+  @Test(expected = RuntimeException.class)
+  public void testFailed() {
+    Sample sample = new Sample();
+    JsonObject json = new JsonObject().put("id", "1").put("person", "Sam");
+
+    json.mergeIn(sample);
+  }
+
+  class Sample {
+    private String id;
+    private String name;
+    public Sample() {
+    }
+    public void setId(String id) {
+      this.id = id;
+    }
+    public void setName(String name) {
+      this.name = name;
+    }
+  }
+}
