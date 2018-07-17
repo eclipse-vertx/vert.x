e8d7a6a007eab17fa3baa9e8ce3773e87f8d84fd
Julien Viet <julien@julienviet.com>
2018-07-17 11:09:51 +0200

    Regen code

--- src/main/generated/io/vertx/core/eventbus/EventBusOptionsConverter.java
+++ src/main/generated/io/vertx/core/eventbus/EventBusOptionsConverter.java
@@ -2,6 +2,8 @@ package io.vertx.core.eventbus;
 
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.json.JsonArray;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Converter for {@link io.vertx.core.eventbus.EventBusOptions}.
--- src/main/generated/io/vertx/core/net/PfxOptionsConverter.java
+++ src/main/generated/io/vertx/core/net/PfxOptionsConverter.java
@@ -2,6 +2,8 @@ package io.vertx.core.net;
 
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.json.JsonArray;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Converter for {@link io.vertx.core.net.PfxOptions}.
