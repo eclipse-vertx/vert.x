49790adbb605d80c063de7706e9f7befd6c31c68
Julien Viet <julien@julienviet.com>
2018-09-25 18:17:37 +0200

    Attempt to run a deploy script in a stage after all jobs have succeeded and only for master or x.y branch

---
+++ .travis.deploy.artifacts.sh
@@ -0,0 +1,7 @@
+# PROJECT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version -B | grep -v '\[')
+PROJECT_VERSION="3.6.0-SNAPSHOT"
+if [[ "$PROJECT_VERSION" =~ .*SNAPSHOT ]] && [[ "${TRAVIS_BRANCH}" =~ ^master$|^[0-9]+\.[0-9]+$ ]] && [[ "${TRAVIS_PULL_REQUEST}" = "false" ]];
+then
+  # mvn deploy -DskipTests -B;
+  echo "deploy"
+fi
