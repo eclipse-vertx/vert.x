05c3b151a8cbd2e8423de4dd405b9f1aafbfb257
Julien Viet <julien@julienviet.com>
2018-09-26 10:15:39 +0200

    Now really deploy artifacts

--- .travis.deploy.artifacts.sh
+++ .travis.deploy.artifacts.sh
@@ -2,6 +2,5 @@
 PROJECT_VERSION="3.6.0-SNAPSHOT"
 if [[ "$PROJECT_VERSION" =~ .*SNAPSHOT ]] && [[ "${TRAVIS_BRANCH}" =~ ^master$|^[0-9]+\.[0-9]+$ ]] && [[ "${TRAVIS_PULL_REQUEST}" = "false" ]];
 then
-  # mvn deploy -DskipTests -B;
-  echo "deploy"
+  mvn deploy -DskipTests -B;
 fi
