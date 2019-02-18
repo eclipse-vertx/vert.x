111b9aa03a2fb6cda449f4a6c07fbec548a0593f
Julien Viet <julien@julienviet.com>
2019-02-18 19:18:25 +0100

    Minor: covariant override of copy() in JdkSSLEngineOptions

--- src/main/java/io/vertx/core/net/JdkSSLEngineOptions.java
+++ src/main/java/io/vertx/core/net/JdkSSLEngineOptions.java
@@ -79,7 +79,7 @@ public class JdkSSLEngineOptions extends SSLEngineOptions {
   }
 
   @Override
-  public SSLEngineOptions copy() {
+  public JdkSSLEngineOptions copy() {
     return new JdkSSLEngineOptions();
   }
 }
