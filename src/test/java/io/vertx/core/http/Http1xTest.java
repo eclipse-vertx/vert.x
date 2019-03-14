21d375ee87f7d3a2b5df152becfc276840327eb6
Julien Viet <julien@julienviet.com>
2019-03-14 12:09:14 +0100

    Fix test failing incorrectly

--- src/test/java/io/vertx/core/http/Http1xTest.java
+++ src/test/java/io/vertx/core/http/Http1xTest.java
@@ -3138,9 +3138,9 @@ public class Http1xTest extends HttpTest {
       awaitLatch(latch);
       client.close();
       client = vertx.createHttpClient(new HttpClientOptions().setMaxPoolSize(1).setKeepAlive(keepAlive).setPipelining(pipelined));
-      HttpClientRequest post = client.post(DEFAULT_HTTP_PORT, DEFAULT_HTTP_HOST, DEFAULT_TEST_URI, resp -> {
+      HttpClientRequest post = client.post(DEFAULT_HTTP_PORT, DEFAULT_HTTP_HOST, DEFAULT_TEST_URI, onSuccess(resp -> {
         fail();
-      });
+      }));
       post.setChunked(true).write(TestUtils.randomBuffer(1024));
       assertTrue(post.reset());
       client.getNow(DEFAULT_HTTP_PORT, DEFAULT_HTTP_HOST, DEFAULT_TEST_URI, resp -> {
