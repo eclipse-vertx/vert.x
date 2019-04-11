680d286f4b440e9f82bd2c51775717e6c06075db
slinkydeveloper <francescoguard@gmail.com>
2019-04-11 18:53:09 +0200

    Made JsonCodecLoader as singleton
    Signed-off-by: slinkydeveloper <francescoguard@gmail.com>

--- src/test/java/io/vertx/core/json/JsonCodecLoaderTest.java
+++ src/test/java/io/vertx/core/json/JsonCodecLoaderTest.java
@@ -9,89 +9,82 @@ import static org.junit.Assert.assertEquals;
 
 public class JsonCodecLoaderTest {
 
-  JsonCodecLoader jsonCodecLoader;
-
-  @Before
-  public void setUp() throws Exception {
-    this.jsonCodecLoader = JsonCodecLoader.loadCodecsFromSPI();
-  }
-
   @Test
   public void booleanCodecTest() {
     MyBooleanPojo pojo = new MyBooleanPojo();
     pojo.setValue(true);
-    assertEquals(encodeToBuffer(true), jsonCodecLoader.encodeBuffer(pojo));
-    assertEquals(pojo, jsonCodecLoader.decodeBuffer(encodeToBuffer(true), MyBooleanPojo.class));
+    assertEquals(encodeToBuffer(true), JsonCodecLoader.INSTANCE.encodeBuffer(pojo));
+    assertEquals(pojo, JsonCodecLoader.INSTANCE.decodeBuffer(encodeToBuffer(true), MyBooleanPojo.class));
   }
 
   @Test(expected = ClassCastException.class)
   public void booleanCodecWrongTypeTest() {
-    jsonCodecLoader.decodeBuffer(encodeToBuffer("aaa"), MyBooleanPojo.class);
+    JsonCodecLoader.INSTANCE.decodeBuffer(encodeToBuffer("aaa"), MyBooleanPojo.class);
   }
 
   @Test
   public void doubleCodecTest() {
     MyDoublePojo pojo = new MyDoublePojo();
     pojo.setValue(1.2d);
-    assertEquals(encodeToBuffer(1.2d), jsonCodecLoader.encodeBuffer(pojo));
-    assertEquals(pojo, jsonCodecLoader.decodeBuffer(encodeToBuffer(1.2d), MyDoublePojo.class));
+    assertEquals(encodeToBuffer(1.2d), JsonCodecLoader.INSTANCE.encodeBuffer(pojo));
+    assertEquals(pojo, JsonCodecLoader.INSTANCE.decodeBuffer(encodeToBuffer(1.2d), MyDoublePojo.class));
   }
 
   @Test(expected = ClassCastException.class)
   public void doubleCodecWrongTypeTest() {
-    jsonCodecLoader.decodeBuffer(encodeToBuffer(""), MyDoublePojo.class);
+    JsonCodecLoader.INSTANCE.decodeBuffer(encodeToBuffer(""), MyDoublePojo.class);
   }
 
   @Test
   public void floatCodecTest() {
     MyFloatPojo pojo = new MyFloatPojo();
     pojo.setValue(1.2f);
-    assertEquals(encodeToBuffer(1.2f), jsonCodecLoader.encodeBuffer(pojo));
-    assertEquals(pojo, jsonCodecLoader.decodeBuffer(encodeToBuffer(1.2f), MyFloatPojo.class));
+    assertEquals(encodeToBuffer(1.2f), JsonCodecLoader.INSTANCE.encodeBuffer(pojo));
+    assertEquals(pojo, JsonCodecLoader.INSTANCE.decodeBuffer(encodeToBuffer(1.2f), MyFloatPojo.class));
   }
 
   @Test(expected = ClassCastException.class)
   public void floatCodecWrongTypeTest() {
-    jsonCodecLoader.decodeBuffer(encodeToBuffer(""), MyFloatPojo.class);
+    JsonCodecLoader.INSTANCE.decodeBuffer(encodeToBuffer(""), MyFloatPojo.class);
   }
 
   @Test
   public void intCodecTest() {
     MyIntegerPojo pojo = new MyIntegerPojo();
     pojo.setValue(1);
-    assertEquals(encodeToBuffer((int)1), jsonCodecLoader.encodeBuffer(pojo));
-    assertEquals(pojo, jsonCodecLoader.decodeBuffer(encodeToBuffer((int)1), MyIntegerPojo.class));
+    assertEquals(encodeToBuffer((int)1), JsonCodecLoader.INSTANCE.encodeBuffer(pojo));
+    assertEquals(pojo, JsonCodecLoader.INSTANCE.decodeBuffer(encodeToBuffer((int)1), MyIntegerPojo.class));
   }
 
   @Test(expected = ClassCastException.class)
   public void intCodecWrongTypeTest() {
-    jsonCodecLoader.decodeBuffer(encodeToBuffer(""), MyIntegerPojo.class);
+    JsonCodecLoader.INSTANCE.decodeBuffer(encodeToBuffer(""), MyIntegerPojo.class);
   }
 
   @Test
   public void longCodecTest() {
     MyLongPojo pojo = new MyLongPojo();
     pojo.setValue(1L);
-    assertEquals(encodeToBuffer(1L), jsonCodecLoader.encodeBuffer(pojo));
-    assertEquals(pojo, jsonCodecLoader.decodeBuffer(encodeToBuffer(1L), MyLongPojo.class));
+    assertEquals(encodeToBuffer(1L), JsonCodecLoader.INSTANCE.encodeBuffer(pojo));
+    assertEquals(pojo, JsonCodecLoader.INSTANCE.decodeBuffer(encodeToBuffer(1L), MyLongPojo.class));
   }
 
   @Test(expected = ClassCastException.class)
   public void longCodecWrongTypeTest() {
-    jsonCodecLoader.decodeBuffer(encodeToBuffer(""), MyLongPojo.class);
+    JsonCodecLoader.INSTANCE.decodeBuffer(encodeToBuffer(""), MyLongPojo.class);
   }
 
   @Test
   public void shortCodecTest() {
     MyShortPojo pojo = new MyShortPojo();
     pojo.setValue((short)1);
-    assertEquals(encodeToBuffer((short)1), jsonCodecLoader.encodeBuffer(pojo));
-    assertEquals(pojo, jsonCodecLoader.decodeBuffer(encodeToBuffer((short)1), MyShortPojo.class));
+    assertEquals(encodeToBuffer((short)1), JsonCodecLoader.INSTANCE.encodeBuffer(pojo));
+    assertEquals(pojo, JsonCodecLoader.INSTANCE.decodeBuffer(encodeToBuffer((short)1), MyShortPojo.class));
   }
 
   @Test(expected = ClassCastException.class)
   public void shortCodecWrongTypeTest() {
-    jsonCodecLoader.decodeBuffer(encodeToBuffer(""), MyShortPojo.class);
+    JsonCodecLoader.INSTANCE.decodeBuffer(encodeToBuffer(""), MyShortPojo.class);
   }
 
   @Test
@@ -99,13 +92,13 @@ public class JsonCodecLoaderTest {
     MyJsonArrayPojo pojo = new MyJsonArrayPojo();
     JsonArray array = new JsonArray().add(1).add(2).add(3);
     pojo.setValue(array);
-    assertEquals(array.toBuffer(), jsonCodecLoader.encodeBuffer(pojo));
-    assertEquals(pojo, jsonCodecLoader.decodeBuffer(array.toBuffer(), MyJsonArrayPojo.class));
+    assertEquals(array.toBuffer(), JsonCodecLoader.INSTANCE.encodeBuffer(pojo));
+    assertEquals(pojo, JsonCodecLoader.INSTANCE.decodeBuffer(array.toBuffer(), MyJsonArrayPojo.class));
   }
 
   @Test(expected = ClassCastException.class)
   public void jsonArrayCodecWrongTypeTest() {
-    jsonCodecLoader.decodeBuffer(encodeToBuffer(2), MyJsonArrayPojo.class);
+    JsonCodecLoader.INSTANCE.decodeBuffer(encodeToBuffer(2), MyJsonArrayPojo.class);
   }
 
   @Test
@@ -113,13 +106,13 @@ public class JsonCodecLoaderTest {
     MyJsonObjectPojo pojo = new MyJsonObjectPojo();
     JsonObject obj = new JsonObject().put("a", 1).put("b", "c");
     pojo.setValue(obj);
-    assertEquals(obj.toBuffer(), jsonCodecLoader.encodeBuffer(pojo));
-    assertEquals(pojo, jsonCodecLoader.decodeBuffer(obj.toBuffer(), MyJsonObjectPojo.class));
+    assertEquals(obj.toBuffer(), JsonCodecLoader.INSTANCE.encodeBuffer(pojo));
+    assertEquals(pojo, JsonCodecLoader.INSTANCE.decodeBuffer(obj.toBuffer(), MyJsonObjectPojo.class));
   }
 
   @Test(expected = ClassCastException.class)
   public void jsonObjectCodecWrongTypeTest() {
-    jsonCodecLoader.decodeBuffer(encodeToBuffer(2), MyJsonObjectPojo.class);
+    JsonCodecLoader.INSTANCE.decodeBuffer(encodeToBuffer(2), MyJsonObjectPojo.class);
   }
   
 }
