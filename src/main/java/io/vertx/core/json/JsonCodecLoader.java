d2a22162afcb7f0625d6c436bc131b2c031dead5
slinkydeveloper <francescoguard@gmail.com>
2019-04-11 18:53:09 +0200

    Made JsonCodecLoader as singleton
    Signed-off-by: slinkydeveloper <francescoguard@gmail.com>

--- src/main/java/io/vertx/core/json/JsonCodecLoader.java
+++ src/main/java/io/vertx/core/json/JsonCodecLoader.java
@@ -9,19 +9,10 @@ import java.util.ServiceLoader;
 @SuppressWarnings("unchecked")
 public class JsonCodecLoader {
 
-  Map<Class, JsonCodec> jsonCodecMap;
+  private Map<Class, JsonCodec> jsonCodecMap;
 
   private JsonCodecLoader(Map<Class, JsonCodec> jsonCodecMap) {
     this.jsonCodecMap = jsonCodecMap;
-  }
-
-  public static JsonCodecLoader loadCodecsFromSPI() {
-    Map<Class, JsonCodec> codecs = new HashMap<>();
-    ServiceLoader<JsonCodec> codecServiceLoader = ServiceLoader.load(JsonCodec.class);
-    for (JsonCodec j : codecServiceLoader) {
-      codecs.put(j.getTargetClass(), j);
-    }
-    return new JsonCodecLoader(codecs);
   }
 
   private <T> JsonCodec retrieveCodec(Class<T> c) {
@@ -30,8 +21,12 @@ public class JsonCodecLoader {
     return codec;
   }
 
+  public boolean hasCodecFor(Class c) {
+    return jsonCodecMap.containsKey(c);
+  }
+
   public <T> T decode(Object json, Class<T> c) {
-    return (T) retrieveCodec(c).decode(json);
+    return (T) ((json != null) ? retrieveCodec(c).decode(json) : null);
   }
 
   public <T> T decodeBuffer(Buffer value, Class<T> c) {
@@ -39,10 +34,21 @@ public class JsonCodecLoader {
   }
 
   public Object encode(Object value) {
-    return retrieveCodec(value.getClass()).encode(value);
+    return (value != null) ? retrieveCodec(value.getClass()).encode(value) : null;
   }
 
   public Buffer encodeBuffer(Object value) {
     return Json.encodeToBuffer(encode(value));
   }
+
+  private static JsonCodecLoader loadCodecsFromSPI() {
+    Map<Class, JsonCodec> codecs = new HashMap<>();
+    ServiceLoader<JsonCodec> codecServiceLoader = ServiceLoader.load(JsonCodec.class);
+    for (JsonCodec j : codecServiceLoader) {
+      codecs.put(j.getTargetClass(), j);
+    }
+    return new JsonCodecLoader(codecs);
+  }
+
+  public static final JsonCodecLoader INSTANCE = JsonCodecLoader.loadCodecsFromSPI();
 }
