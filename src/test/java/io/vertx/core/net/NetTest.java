8b8e4c56eb836d3838c7129a5a5338533dc70b29
Julien Viet <julien@julienviet.com>
2019-05-02 18:38:15 +0200

    In NetTest#testIdleTimeoutSendChunkedFile remaining bytes should only be set in client end handler

--- src/test/java/io/vertx/core/net/NetTest.java
+++ src/test/java/io/vertx/core/net/NetTest.java
@@ -3320,7 +3320,6 @@ public class NetTest extends VertxTestBase {
     };
     Consumer<NetSocket> receiver = so -> {
       now.set(System.currentTimeMillis());
-      remaining.set(expected);
       int[] len = { 0 };
       so.handler(buff -> {
         len[0] += buff.length();
