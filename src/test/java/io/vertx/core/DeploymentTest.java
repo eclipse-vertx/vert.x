14effa3691778f717866c09e75d06be699957c9a
Julien Viet <julien@julienviet.com>
2019-08-19 12:16:37 +0200

    NPE when undeploying several instances of the same verticle that share the same isolationGroup - closes #3074

--- src/test/java/io/vertx/core/DeploymentTest.java
+++ src/test/java/io/vertx/core/DeploymentTest.java
@@ -1161,12 +1161,24 @@ public class DeploymentTest extends VertxTestBase {
 
   @Test
   public void testCloseIsolationGroup() throws Exception {
+    testCloseIsolationGroup(1);
+  }
+
+  @Test
+  public void testCloseIsolationGroupMultiInstances() throws Exception {
+    testCloseIsolationGroup(3);
+  }
+
+  private void testCloseIsolationGroup(int instances) throws Exception {
     boolean expectedSuccess = Thread.currentThread().getContextClassLoader() instanceof URLClassLoader;
     String dir = createClassOutsideClasspath("MyVerticle");
-    List<String> extraClasspath = Arrays.asList(dir);
+    List<String> extraClasspath = Collections.singletonList(dir);
     Vertx vertx = Vertx.vertx();
     try {
-      DeploymentOptions options = new DeploymentOptions().setIsolationGroup("somegroup").setExtraClasspath(extraClasspath);
+      DeploymentOptions options = new DeploymentOptions()
+        .setInstances(instances)
+        .setIsolationGroup("somegroup")
+        .setExtraClasspath(extraClasspath);
       vertx.deployVerticle("java:" + ExtraCPVerticleNotInParentLoader.class.getCanonicalName(), options, onSuccess(id -> {
         vertx.close(onSuccess(v -> {
           assertTrue(ExtraCPVerticleNotInParentLoader.cl.isClosed());
@@ -1181,6 +1193,7 @@ public class DeploymentTest extends VertxTestBase {
       ExtraCPVerticleNotInParentLoader.cl = null;
     }
   }
+
   public static class ParentVerticle extends AbstractVerticle {
 
     @Override
