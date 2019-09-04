5148ef762f186213c0b3ad4c2901ae6f2e66a60e
Julien Viet <julien@julienviet.com>
2019-09-04 16:10:19 +0200

    Move relevant tests to JsonMapperTest

--- src/test/java/io/vertx/core/json/JsonMapperTest.java
+++ src/test/java/io/vertx/core/json/JsonMapperTest.java
@@ -10,21 +10,40 @@
  */
 package io.vertx.core.json;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.impl.Utils;
 import io.vertx.test.core.TestUtils;
 import org.junit.Test;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 import static java.time.format.DateTimeFormatter.ISO_INSTANT;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 public class JsonMapperTest {
+
+  private static final TypeReference<Integer> INTEGER_TYPE_REF = new TypeReference<Integer>() {};
+  private static final TypeReference<Long> LONG_TYPE_REF = new TypeReference<Long>() {};
+  private static final TypeReference<String> STRING_TYPE_REF = new TypeReference<String>() {};
+  private static final TypeReference<Float> FLOAT_TYPE_REF = new TypeReference<Float>() {};
+  private static final TypeReference<Double> DOUBLE_TYPE_REF = new TypeReference<Double>() {};
+  private static final TypeReference<Map<String, Object>> MAP_TYPE_REF = new TypeReference<Map<String, Object>>() {};
+  private static final TypeReference<List<Object>> LIST_TYPE_REF = new TypeReference<List<Object>>() {};
+  private static final TypeReference<Boolean> BOOLEAN_TYPE_REF = new TypeReference<Boolean>() {};
 
   @Test
   public void testEncodeJsonObject() {
@@ -299,6 +318,118 @@ public class JsonMapperTest {
         fail();
       } catch (DecodeException ignore) {
       }
+    }
+  }
+
+  @Test
+  public void encodeCustomTypeInstant() {
+    Instant now = Instant.now();
+    String json = Json.encode(now);
+    assertNotNull(json);
+    // the RFC is one way only
+    Instant decoded = Instant.from(ISO_INSTANT.parse(json.substring(1, json.length() - 1)));
+    assertEquals(now, decoded);
+
+  }
+
+  @Test
+  public void encodeCustomTypeInstantNull() {
+    Instant now = null;
+    String json = Json.encode(now);
+    assertNotNull(json);
+    assertEquals("null", json);
+  }
+
+  @Test
+  public void encodeCustomTypeBinary() {
+    byte[] data = new byte[] { 'h', 'e', 'l', 'l', 'o'};
+    String json = Json.encode(data);
+    assertNotNull(json);
+    // base64 encoded hello
+    assertEquals("\"aGVsbG8=\"", json);
+  }
+
+  @Test
+  public void encodeCustomTypeBinaryNull() {
+    byte[] data = null;
+    String json = Json.encode(data);
+    assertNotNull(json);
+    assertEquals("null", json);
+  }
+
+  @Test
+  public void encodeToBuffer() {
+    Buffer json = Json.encodeToBuffer("Hello World!");
+    assertNotNull(json);
+    // json strings are always UTF8
+    assertEquals("\"Hello World!\"", json.toString("UTF-8"));
+  }
+
+  @Test
+  public void testDecodeValue() {
+    assertDecodeValue(Buffer.buffer("42"), 42, INTEGER_TYPE_REF);
+    assertDecodeValue(Buffer.buffer("42"), 42L, LONG_TYPE_REF);
+    assertDecodeValue(Buffer.buffer("\"foobar\""), "foobar", STRING_TYPE_REF);
+    assertDecodeValue(Buffer.buffer("3.4"), 3.4f, FLOAT_TYPE_REF);
+    assertDecodeValue(Buffer.buffer("3.4"), 3.4d, DOUBLE_TYPE_REF);
+    assertDecodeValue(Buffer.buffer("{\"foo\":4}"), Collections.singletonMap("foo", 4), MAP_TYPE_REF);
+    assertDecodeValue(Buffer.buffer("[0,1,2]"), Arrays.asList(0, 1, 2), LIST_TYPE_REF);
+    assertDecodeValue(Buffer.buffer("true"), true, BOOLEAN_TYPE_REF);
+    assertDecodeValue(Buffer.buffer("false"), false, BOOLEAN_TYPE_REF);
+  }
+
+  private <T> void assertDecodeValue(Buffer buffer, T expected, TypeReference<T> ref) {
+    Type type = ref.getType();
+    Class<?> clazz = type instanceof Class ? (Class<?>) type : (Class<?>) ((ParameterizedType) type).getRawType();
+    assertEquals(expected, Json.decodeValue(buffer, clazz));
+    assertEquals(expected, Json.decodeValue(buffer, ref));
+    assertEquals(expected, Json.decodeValue(buffer.toString(StandardCharsets.UTF_8), clazz));
+    assertEquals(expected, Json.decodeValue(buffer.toString(StandardCharsets.UTF_8), ref));
+    Buffer nullValue = Buffer.buffer("null");
+    assertNull(Json.decodeValue(nullValue, clazz));
+    assertNull(Json.decodeValue(nullValue, ref));
+    assertNull(Json.decodeValue(nullValue.toString(StandardCharsets.UTF_8), clazz));
+    assertNull(Json.decodeValue(nullValue.toString(StandardCharsets.UTF_8), ref));
+  }
+
+  @Test
+  public void testDecodeBufferUnknowContent() {
+    testDecodeUnknowContent(true);
+  }
+
+  @Test
+  public void testDecodeStringUnknowContent() {
+    testDecodeUnknowContent(false);
+  }
+
+  private void testDecodeUnknowContent(boolean asBuffer) {
+    String number = String.valueOf(1);
+    assertEquals(1, asBuffer ? Json.decodeValue(Buffer.buffer(number)) : Json.decodeValue(number));
+
+    String bool = Boolean.TRUE.toString();
+    assertEquals(true, asBuffer ? Json.decodeValue(Buffer.buffer(bool)) : Json.decodeValue(bool));
+
+    String text = "\"whatever\"";
+    assertEquals("whatever", asBuffer ? Json.decodeValue(Buffer.buffer(text)) : Json.decodeValue(text));
+
+    String nullText = "null";
+    assertNull(asBuffer ? Json.decodeValue(Buffer.buffer(nullText)) : Json.decodeValue(nullText));
+
+    JsonObject obj = new JsonObject().put("foo", "bar");
+    assertEquals(obj, asBuffer ? Json.decodeValue(obj.toBuffer()) : Json.decodeValue(obj.toString()));
+
+    JsonArray arr = new JsonArray().add(1).add(false).add("whatever").add(obj);
+    assertEquals(arr, asBuffer ? Json.decodeValue(arr.toBuffer()) : Json.decodeValue(arr.toString()));
+
+    String invalidText = "\"invalid";
+    try {
+      if (asBuffer) {
+        Json.decodeValue(Buffer.buffer(invalidText));
+      } else {
+        Json.decodeValue(invalidText);
+      }
+      fail();
+    } catch (DecodeException ignore) {
     }
   }
 }
