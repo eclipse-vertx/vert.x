200329ec5ecaf521500103eded17d57c17d470dc
Stéphane Épardaud <stef@inforealm.org>
2019-09-27 14:38:01 +0200

    Fixed docs of Context.isOnEventLoopThread() (#3118)
    Signed-off-by: Stephane Epardaud <stef@epardaud.fr>

--- src/main/java/io/vertx/core/Context.java
+++ src/main/java/io/vertx/core/Context.java
@@ -76,7 +76,7 @@ public interface Context {
    * NOTE! This is not always the same as calling {@link Context#isEventLoopContext}. If you are running blocking code
    * from an event loop context, then this will return false but {@link Context#isEventLoopContext} will return true.
    *
-   * @return true if current thread is a worker thread, false otherwise
+   * @return true if current thread is an event thread, false otherwise
    */
   static boolean isOnEventLoopThread() {
     Thread t = Thread.currentThread();
