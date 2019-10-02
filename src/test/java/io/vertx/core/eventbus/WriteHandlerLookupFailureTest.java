bca56c0f49a080ded0ad0b702e2f51b9c62987a5
Thomas Segismont <tsegismont@gmail.com>
2019-10-02 11:04:29 +0200

    Make WriteHandlerLookupFailureTest compile
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- src/test/java/io/vertx/core/eventbus/WriteHandlerLookupFailureTest.java
+++ src/test/java/io/vertx/core/eventbus/WriteHandlerLookupFailureTest.java
@@ -12,14 +12,12 @@
 package io.vertx.core.eventbus;
 
 import io.vertx.core.*;
-import io.vertx.core.spi.cluster.AsyncMultiMap;
-import io.vertx.core.spi.cluster.ChoosableIterable;
-import io.vertx.core.spi.cluster.ClusterManager;
+import io.vertx.core.spi.cluster.NodeInfo;
+import io.vertx.core.spi.cluster.impl.DefaultDeliveryStrategy;
 import io.vertx.test.core.VertxTestBase;
-import io.vertx.test.fakecluster.FakeClusterManager;
 import org.junit.Test;
 
-import java.util.function.Predicate;
+import java.util.List;
 
 /**
  * @author Julien Viet
@@ -29,47 +27,16 @@ public final class WriteHandlerLookupFailureTest extends VertxTestBase {
   @Test
   public void test() {
     Throwable cause = new Throwable();
-    ClusterManager cm = new FakeClusterManager() {
-      @Override
-      public <K, V> void getAsyncMultiMap(String name, Handler<AsyncResult<AsyncMultiMap<K, V>>> handler) {
-        if ("__vertx.subs".equals(name)) {
-          super.<K, V>getAsyncMultiMap(name, ar -> {
-            handler.handle(ar.map(map -> {
-              return new AsyncMultiMap<K, V>() {
-                @Override
-                public void add(K k, V v, Handler<AsyncResult<Void>> completionHandler) {
-                  map.add(k, v, completionHandler);
-                }
-
-                @Override
-                public void get(K k, Handler<AsyncResult<ChoosableIterable<V>>> completionHandler) {
-                  completionHandler.handle(Future.failedFuture(cause));
-                }
-
-                @Override
-                public void remove(K k, V v, Handler<AsyncResult<Boolean>> completionHandler) {
-                  map.remove(k, v, completionHandler);
-                }
-
-                @Override
-                public void removeAllForValue(V v, Handler<AsyncResult<Void>> completionHandler) {
-                  map.removeAllForValue(v, completionHandler);
-                }
-
-                @Override
-                public void removeAllMatching(Predicate<V> p, Handler<AsyncResult<Void>> completionHandler) {
-                  map.removeAllMatching(p, completionHandler);
-                }
-              };
-            }));
-          });
-        } else {
-          super.getAsyncMultiMap(name, handler);
+    VertxOptions options = new VertxOptions();
+    options.getEventBusOptions()
+      .setHost("localhost")
+      .setPort(0)
+      .setDeliveryStrategy(new DefaultDeliveryStrategy() {
+        @Override
+        public void chooseNodes(Message<?> message, Handler<AsyncResult<List<NodeInfo>>> handler) {
+          handler.handle(Future.failedFuture(cause));
         }
-      }
-    };
-    VertxOptions options = new VertxOptions().setClusterManager(cm);
-    options.getEventBusOptions().setHost("localhost").setPort(0);
+      });
     vertices = new Vertx[1];
     clusteredVertx(options, onSuccess(node -> {
       vertices[0] = node;
