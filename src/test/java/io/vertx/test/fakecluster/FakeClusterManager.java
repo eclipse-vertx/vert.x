15c0593b28062b6f0304d42b908809acd9e7f2cc
Thomas Segismont <tsegismont@gmail.com>
2019-10-02 10:24:33 +0200

    Make FakeClusterManager compile
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- src/test/java/io/vertx/test/fakecluster/FakeClusterManager.java
+++ src/test/java/io/vertx/test/fakecluster/FakeClusterManager.java
@@ -16,7 +16,6 @@ import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.impl.ContextInternal;
-import io.vertx.core.impl.TaskQueue;
 import io.vertx.core.impl.VertxInternal;
 import io.vertx.core.shareddata.AsyncMap;
 import io.vertx.core.shareddata.Counter;
@@ -24,30 +23,21 @@ import io.vertx.core.shareddata.Lock;
 import io.vertx.core.shareddata.impl.AsynchronousCounter;
 import io.vertx.core.shareddata.impl.LocalAsyncLocks;
 import io.vertx.core.shareddata.impl.LocalAsyncMapImpl;
-import io.vertx.core.spi.cluster.AsyncMultiMap;
-import io.vertx.core.spi.cluster.ChoosableIterable;
 import io.vertx.core.spi.cluster.ClusterManager;
 import io.vertx.core.spi.cluster.NodeListener;
+import io.vertx.core.spi.cluster.RegistrationInfo;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.function.Predicate;
 
 public class FakeClusterManager implements ClusterManager {
 
   private static Map<String, FakeClusterManager> nodes = Collections.synchronizedMap(new LinkedHashMap<>());
 
   private static ConcurrentMap<String, LocalAsyncMapImpl> asyncMaps = new ConcurrentHashMap<>();
-  private static ConcurrentMap<String, ConcurrentMap> asyncMultiMaps = new ConcurrentHashMap<>();
   private static ConcurrentMap<String, Map> syncMaps = new ConcurrentHashMap<>();
   private static LocalAsyncLocks localAsyncLocks = new LocalAsyncLocks();
   private static ConcurrentMap<String, AtomicLong> counters = new ConcurrentHashMap<>();
@@ -110,22 +100,7 @@ public class FakeClusterManager implements ClusterManager {
   }
 
   @Override
-  public <K, V> void getAsyncMultiMap(String name, Handler<AsyncResult<AsyncMultiMap<K, V>>> resultHandler) {
-    ConcurrentMap map = asyncMultiMaps.get(name);
-    if (map == null) {
-      map = new ConcurrentHashMap<>();
-      ConcurrentMap prevMap = asyncMultiMaps.putIfAbsent(name, map);
-      if (prevMap != null) {
-        map = prevMap;
-      }
-    }
-    @SuppressWarnings("unchecked")
-    ConcurrentMap<K, ChoosableSet<V>> theMap = map;
-    vertx.runOnContext(v -> resultHandler.handle(Future.succeededFuture(new FakeAsyncMultiMap<>(theMap))));
-  }
-
   @SuppressWarnings("unchecked")
-  @Override
   public <K, V> Future<AsyncMap<K, V>> getAsyncMap(String name) {
     LocalAsyncMapImpl<K, V> asyncMap = asyncMaps.computeIfAbsent(name, n -> new LocalAsyncMapImpl(vertx));
     ContextInternal context = vertx.getOrCreateContext();
@@ -215,99 +190,21 @@ public class FakeClusterManager implements ClusterManager {
     return nodeID != null;
   }
 
+  @Override
+  public void register(RegistrationInfo registrationInfo, Handler<AsyncResult<Void>> completionHandler) {
+    throw new UnsupportedOperationException("Not implemented yet");
+  }
+
+  @Override
+  public void unregister(RegistrationInfo registrationInfo, Handler<AsyncResult<Void>> completionHandler) {
+    throw new UnsupportedOperationException("Not implemented yet");
+  }
+
   public static void reset() {
     nodes.clear();
     asyncMaps.clear();
-    asyncMultiMaps.clear();
     localAsyncLocks = new LocalAsyncLocks();
     counters.clear();
     syncMaps.clear();
-  }
-
-  private class FakeAsyncMultiMap<K, V> implements AsyncMultiMap<K, V> {
-
-    private final ConcurrentMap<K, ChoosableSet<V>> map;
-    private final TaskQueue taskQueue;
-
-    public FakeAsyncMultiMap(ConcurrentMap<K, ChoosableSet<V>> map) {
-      taskQueue = new TaskQueue();
-      this.map = map;
-    }
-
-    @Override
-    public void add(final K k, final V v, Handler<AsyncResult<Void>> completionHandler) {
-      ContextInternal ctx = vertx.getOrCreateContext();
-      ctx.executeBlocking(fut -> {
-        ChoosableSet<V> vals = map.get(k);
-        if (vals == null) {
-          vals = new ChoosableSet<>(1);
-          ChoosableSet<V> prevVals = map.putIfAbsent(k, vals);
-          if (prevVals != null) {
-            vals = prevVals;
-          }
-        }
-        vals.add(v);
-        fut.complete();
-      }, taskQueue, completionHandler);
-    }
-
-    @Override
-    public void get(final K k, Handler<AsyncResult<ChoosableIterable<V>>> asyncResultHandler) {
-      ContextInternal ctx = vertx.getOrCreateContext();
-      ctx.executeBlocking(fut -> {
-        ChoosableIterable<V> it = map.get(k);
-        if (it == null) {
-          it = new ChoosableSet<>(0);
-        }
-        fut.complete(it);
-      }, taskQueue, asyncResultHandler);
-    }
-
-    @Override
-    public void remove(final K k, final V v, Handler<AsyncResult<Boolean>> completionHandler) {
-      ContextInternal ctx = vertx.getOrCreateContext();
-      ctx.executeBlocking(fut -> {
-        ChoosableSet<V> vals = map.get(k);
-        boolean found = false;
-        if (vals != null) {
-          boolean removed = vals.remove(v);
-          if (removed) {
-            if (vals.isEmpty()) {
-              map.remove(k);
-            }
-            found = true;
-          }
-        }
-        fut.complete(found);
-      }, taskQueue, completionHandler);
-    }
-
-    @Override
-    public void removeAllForValue(final V v, Handler<AsyncResult<Void>> completionHandler) {
-      removeAllMatching(v::equals, completionHandler);
-    }
-
-    @Override
-    public void removeAllMatching(Predicate<V> p, Handler<AsyncResult<Void>> completionHandler) {
-      ContextInternal ctx = vertx.getOrCreateContext();
-      ctx.executeBlocking(fut -> {
-        Iterator<Entry<K, ChoosableSet<V>>> mapIter = map.entrySet().iterator();
-        while (mapIter.hasNext()) {
-          Entry<K, ChoosableSet<V>> entry = mapIter.next();
-          ChoosableSet<V> vals = entry.getValue();
-          Iterator<V> iter = vals.iterator();
-          while (iter.hasNext()) {
-            V val = iter.next();
-            if (p.test(val)) {
-              iter.remove();
-            }
-          }
-          if (vals.isEmpty()) {
-            mapIter.remove();
-          }
-        }
-        fut.complete();
-      }, taskQueue, completionHandler);
-    }
   }
 }
