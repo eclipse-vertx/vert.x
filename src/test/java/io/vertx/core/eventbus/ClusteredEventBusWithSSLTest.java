a8300d58c49b23e0c45e1aefb70a61d0880ddbe2
Thomas Segismont <tsegismont@gmail.com>
2019-10-08 14:39:24 +0200

    Removed testSendWhileUnsubscribing
    With the new SPI design, there is no guarantee the DeliveryStrategy is updated on every node just after the removeRegistration method is invoked
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- src/test/java/io/vertx/core/eventbus/ClusteredEventBusWithSSLTest.java
+++ src/test/java/io/vertx/core/eventbus/ClusteredEventBusWithSSLTest.java
@@ -16,8 +16,6 @@ import io.vertx.core.http.ClientAuth;
 import io.vertx.core.http.Http1xTest;
 import io.vertx.test.tls.Cert;
 import io.vertx.test.tls.Trust;
-import org.junit.Ignore;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
@@ -85,12 +83,4 @@ public class ClusteredEventBusWithSSLTest extends ClusteredEventBusTestBase {
   protected void startNodes(int numNodes) {
     super.startNodes(numNodes, new VertxOptions().setEventBusOptions(options));
   }
-
-  @Test
-  @Ignore
-  @Override
-  public void testSendWhileUnsubscribing() throws Exception {
-    // This test can fail if CPU is busy, so avoid it with EventBus SSL
-  }
-
 }
