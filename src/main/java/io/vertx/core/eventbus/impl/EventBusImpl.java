165b0f4a7f61f2c9954be27014e86bd5eb74a2c9
Julien Viet <julien@julienviet.com>
2019-10-12 09:56:53 +0200

    EventBus consumer should register pending replies context close hooks when the context is associated with a deployment - closes #3138

--- src/main/java/io/vertx/core/eventbus/impl/EventBusImpl.java
+++ src/main/java/io/vertx/core/eventbus/impl/EventBusImpl.java
@@ -260,12 +260,7 @@ public class EventBusImpl implements EventBus, MetricsProvider {
                                                            boolean replyHandler, boolean localOnly) {
     Objects.requireNonNull(address, "address");
 
-    Context context = Vertx.currentContext();
-    boolean hasContext = context != null;
-    if (!hasContext) {
-      // Embedded
-      context = vertx.getOrCreateContext();
-    }
+    Context context = vertx.getOrCreateContext();
     registration.setHandlerContext(context);
 
     HandlerHolder<T> holder = new HandlerHolder<>(registration, replyHandler, localOnly, context);
@@ -276,7 +271,7 @@ public class EventBusImpl implements EventBus, MetricsProvider {
       handlers,
       (old, prev) -> old.add(prev.first()));
 
-    if (hasContext) {
+    if (context.deploymentID() != null) {
       HandlerEntry entry = new HandlerEntry<>(address, registration);
       context.addCloseHook(entry);
     }
@@ -304,7 +299,7 @@ public class EventBusImpl implements EventBus, MetricsProvider {
       ConcurrentCyclicSequence<HandlerHolder> next = val.remove(holder);
       return next.size() == 0 ? null : next;
     }) == null;
-    if (holder.setRemoved()) {
+    if (holder.setRemoved() && holder.getContext().deploymentID() != null) {
       holder.getContext().removeCloseHook(new HandlerEntry<>(address, holder.getHandler()));
     }
     return last;
