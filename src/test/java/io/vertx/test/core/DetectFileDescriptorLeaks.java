954987a08051c18f20dc7b246fe97b0682f4bf2f
Julien Viet <julien@julienviet.com>
2020-05-14 11:21:42 +0200

    Implement file descriptor leak tests (#3408)
    * Implement file descriptor leak tests
    
    Signed-off-by: Dimitris Zenios <dimitris.zenios@gmail.com>
    
    * Close the AsyncFile once we are finished with it
    
    Closes #3396
    
    Signed-off-by: Dimitris Zenios <dimitris.zenios@gmail.com>
    
    * Comments and refactorings
    
    Signed-off-by: Dimitris Zenios <dimitris.zenios@gmail.com>
    
    * Make sure that FileDescriptorLeakDetectorRule will skip when run in os that does not support getting the open file descriptors
    
    Signed-off-by: Dimitris Zenios <dimitris.zenios@gmail.com>
    
    * Use JMX to collect the open file descriptor count instead of the com.sun API + minor improvements
    
    Co-authored-by: Dimitris Zenios <dimitris.zenios@gmail.com>

---
+++ src/test/java/io/vertx/test/core/DetectFileDescriptorLeaks.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2011-2019 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
+
+package io.vertx.test.core;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({java.lang.annotation.ElementType.METHOD})
+public @interface DetectFileDescriptorLeaks {
+  long baseline() default 20;
+  long iterations() default 20;
+}
+
