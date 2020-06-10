1411f463d8ab22505361cc6b901c49735a13a684
Julien Viet <julien@julienviet.com>
2020-06-10 14:55:36 +0200

    Minor formatting

--- src/test/java/io/vertx/core/dns/DNSTest.java
+++ src/test/java/io/vertx/core/dns/DNSTest.java
@@ -124,13 +124,12 @@ public class DNSTest extends VertxTestBase {
 
   @Test
   public void testUnresolvedDnsServer() throws Exception {
-    final String ip = "10.0.0.1";
     try {
-    	DnsClient dns = vertx.createDnsClient(new DnsClientOptions().setHost("iamanunresolvablednsserver.com").setPort(53));
-    	fail();
+      DnsClient dns = vertx.createDnsClient(new DnsClientOptions().setHost("iamanunresolvablednsserver.com").setPort(53));
+      fail();
     } catch (Exception e) {
-    	assertTrue(e instanceof IllegalArgumentException);
-    	assertEquals("Cannot resolve the host to a valid ip address", e.getMessage());
+      assertTrue(e instanceof IllegalArgumentException);
+      assertEquals("Cannot resolve the host to a valid ip address", e.getMessage());
     }
   }
 
