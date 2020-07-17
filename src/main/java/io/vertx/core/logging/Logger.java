47600732b7844a57a57bd4f70b36931e4f928403
Julien Viet <julien@julienviet.com>
2020-07-17 14:56:02 +0200

    Log4j 1 removal - fixes #3502

--- src/main/java/io/vertx/core/logging/Logger.java
+++ src/main/java/io/vertx/core/logging/Logger.java
@@ -21,10 +21,10 @@ import io.vertx.core.spi.logging.LogDelegate;
  * used by JUL will taken from the default logging.properties in the JDK installation if no {@code  java.util.logging.config.file} system
  * property is set.
  * <p>
- * If you would prefer to use Log4J or SLF4J instead of JUL then you can set a system property called
+ * If you would prefer to use Log4J 2 or SLF4J instead of JUL then you can set a system property called
  * {@code vertx.logger-delegate-factory-class-name} to the class name of the delegate for your logging system.
- * For Log4J the value is {@code io.vertx.core.logging.Log4jLogDelegateFactory}, for SLF4J the value
- * is {@code io.vertx.core.logging.SLF4JLogDelegateFactory}. You will need to ensure whatever jar files
+ * For Log4J 2 the value is {@link io.vertx.core.logging.Log4j2LogDelegateFactory}, for SLF4J the value
+ * is {@link io.vertx.core.logging.SLF4JLogDelegateFactory}. You will need to ensure whatever jar files
  * required by your favourite log framework are on your classpath.
  * <p>
  * Keep in mind that logging backends use different formats to represent replaceable tokens in parameterized messages.
