a13bae4cad35a72095685b495ceb860d1c71dfd8
Julien Viet <julien@julienviet.com>
2020-09-04 08:54:12 +0200

    Vert.x heap buffer now use a ByteBuf implementation that is un-releasable, un-instrumented and un-pooled. As it is un-releasable, this avoids to wrap it with an un-releasable wrapper when used. - close #3560

---
+++ src/main/java/io/vertx/core/buffer/impl/VertxUnsafeHeapByteBuf.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2011-2020 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
+package io.vertx.core.buffer.impl;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.UnpooledUnsafeHeapByteBuf;
+
+/**
+ * An un-releasable, un-pooled, un-instrumented, un-safe heap {@code ByteBuf}.
+ */
+final class VertxUnsafeHeapByteBuf extends UnpooledUnsafeHeapByteBuf {
+
+  public VertxUnsafeHeapByteBuf(ByteBufAllocator alloc, int initialCapacity, int maxCapacity) {
+    super(alloc, initialCapacity, maxCapacity);
+  }
+
+  @Override
+  public ByteBuf retain(int increment) {
+    return this;
+  }
+
+  @Override
+  public ByteBuf retain() {
+    return this;
+  }
+
+  @Override
+  public ByteBuf touch() {
+    return this;
+  }
+
+  @Override
+  public ByteBuf touch(Object hint) {
+    return this;
+  }
+
+  @Override
+  public boolean release() {
+    return false;
+  }
+
+  @Override
+  public boolean release(int decrement) {
+    return false;
+  }
+}
