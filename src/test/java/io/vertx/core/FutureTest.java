c6c92b4b634007bba098b237a951fe375b56d7b8
hltj <jiaywe@gmail.com>
2020-10-10 15:48:57 +0800

    Add test cases for override methods in SucceededFuture & FailedFuture
    Signed-off-by: hltj <jiaywe@gmail.com>

--- src/test/java/io/vertx/core/FutureTest.java
+++ src/test/java/io/vertx/core/FutureTest.java
@@ -655,6 +655,25 @@ public class FutureTest extends VertxTestBase {
   }
 
   @Test
+  public void testMapValueSuccess() {
+    Promise<Integer> p = Promise.promise();
+    Future<Integer> f = p.future();
+    Future<String> mapped = f.map("5");
+    Checker<String> checker = new Checker<>(mapped);
+    checker.assertNotCompleted();
+    p.complete(3);
+    checker.assertSucceeded("5");
+  }
+
+  @Test
+  public void testMapValueAlreadySuccess() {
+    Future<Integer> f = Future.succeededFuture(3);
+    Future<String> mapped = f.map("5");
+    Checker<String> checker = new Checker<>(mapped);
+    checker.assertSucceeded("5");
+  }
+
+  @Test
   public void testMapFailure() {
     Throwable cause = new Throwable();
     Promise<String> p = Promise.promise();
@@ -663,6 +682,36 @@ public class FutureTest extends VertxTestBase {
     Checker<String> checker = new Checker<>(mapped);
     checker.assertNotCompleted();
     p.fail(cause);
+    checker.assertFailed(cause);
+  }
+
+  @Test
+  public void testMapAlreadyFailure() {
+    Throwable cause = new Throwable();
+    Future<String> f = Future.failedFuture(cause);
+    Future<String> mapped = f.map(Object::toString);
+    Checker<String> checker = new Checker<>(mapped);
+    checker.assertFailed(cause);
+  }
+
+  @Test
+  public void testMapValueFailure() {
+    Throwable cause = new Throwable();
+    Promise<String> p = Promise.promise();
+    Future<String> f = p.future();
+    Future<String> mapped = f.map("5");
+    Checker<String> checker = new Checker<>(mapped);
+    checker.assertNotCompleted();
+    p.fail(cause);
+    checker.assertFailed(cause);
+  }
+
+  @Test
+  public void testMapValueAlreadyFailure() {
+    Throwable cause = new Throwable();
+    Future<String> f = Future.failedFuture(cause);
+    Future<String> mapped = f.map("5");
+    Checker<String> checker = new Checker<>(mapped);
     checker.assertFailed(cause);
   }
 
@@ -789,6 +838,45 @@ public class FutureTest extends VertxTestBase {
   }
 
   @Test
+  public void testOtherwiseAlreadySuccessWithSuccess() {
+    AtomicBoolean called = new AtomicBoolean();
+    Future<String> f = Future.succeededFuture("yeah");
+    Future<String> r = f.otherwise(t -> {
+      called.set(true);
+      throw new AssertionError();
+    });
+    Checker<String> checker = new Checker<>(r);
+    assertTrue(r.succeeded());
+    checker.assertSucceeded("yeah");
+    assertFalse(called.get());
+  }
+
+  @Test
+  public void testOtherwiseValueSuccessWithSuccess() {
+    AtomicBoolean called = new AtomicBoolean();
+    Promise<String> p = Promise.promise();
+    Future<String> f = p.future();
+    Future<String> r = f.otherwise("other");
+    Checker<String> checker = new Checker<>(r);
+    checker.assertNotCompleted();
+    p.complete("yeah");
+    assertTrue(r.succeeded());
+    checker.assertSucceeded("yeah");
+    assertFalse(called.get());
+  }
+
+  @Test
+  public void testOtherwiseValueAlreadySuccessWithSuccess() {
+    AtomicBoolean called = new AtomicBoolean();
+    Future<String> f = Future.succeededFuture("yeah");
+    Future<String> r = f.otherwise("other");
+    Checker<String> checker = new Checker<>(r);
+    assertTrue(r.succeeded());
+    checker.assertSucceeded("yeah");
+    assertFalse(called.get());
+  }
+
+  @Test
   public void testOtherwiseFailureWithSuccess() {
     Promise<String> p = Promise.promise();
     Future<String> f = p.future();
@@ -797,6 +885,25 @@ public class FutureTest extends VertxTestBase {
     checker.assertNotCompleted();
     p.fail("recovered");
     checker.assertSucceeded("recovered");
+  }
+
+  @Test
+  public void testOtherwiseValueFailureWithSuccess() {
+    Promise<String> p = Promise.promise();
+    Future<String> f = p.future();
+    Future<String> r = f.otherwise("other");
+    Checker<String> checker = new Checker<>(r);
+    checker.assertNotCompleted();
+    p.fail("recovered");
+    checker.assertSucceeded("other");
+  }
+
+  @Test
+  public void testOtherwiseValueAlreadyFailureWithSuccess() {
+    Future<String> f = Future.failedFuture("recovered");
+    Future<String> r = f.otherwise("other");
+    Checker<String> checker = new Checker<>(r);
+    checker.assertSucceeded("other");
   }
 
   @Test
