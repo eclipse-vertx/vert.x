67dbeb0446861978fd914466bd65af0599ca1e35
Julian Ladisch <eclipse.org-rtn@ladisch.de>
2020-11-18 18:09:29 +0100

    Move os.name detection code into Utils
    Signed-off-by: Julian Ladisch <eclipse.org-rtn@ladisch.de>

--- src/main/java/io/vertx/core/impl/Utils.java
+++ src/main/java/io/vertx/core/impl/Utils.java
@@ -21,11 +21,20 @@ public class Utils {
 
   public static String LINE_SEPARATOR = System.getProperty("line.separator");
 
+  private static final boolean isLinux;
   private static final boolean isWindows;
 
   static {
-    String os = System.getProperty("os.name").toLowerCase();
+    String os = System.getProperty("os.name", "").toLowerCase();
+    isLinux = os.startsWith("linux");
     isWindows = os.contains("win");
+  }
+
+  /**
+   * @return true, if running on Linux
+   */
+  public static boolean isLinux() {
+    return isLinux;
   }
 
   /**
