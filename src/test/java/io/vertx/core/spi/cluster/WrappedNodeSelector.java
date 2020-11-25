f11a8b91bf5cff70027d8fd2c9436154cc50dbf8
Thomas Segismont <tsegismont@gmail.com>
2020-11-25 15:13:48 +0100

    Intermittent failure in testClusteredUnregistration
    The test failed intermittently with Hazelcast on CI.
    It failed consistently with Vert.x nodes configured as lite members of the Hazelcast cluster.
    
    When a subscription is removed, it can take some time before the cluster manager invokes our listeners.
    It is then possible that we still have in cache a registration that has been effectively removed.
    
    The fix consists in intercepting the registration update event to make sure it has been processed.
    Then we can try sending again and we should get the NO_HANDLERS error as expected.
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

---
+++ src/test/java/io/vertx/core/spi/cluster/WrappedNodeSelector.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2011-2020 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
+
+package io.vertx.core.spi.cluster;
+
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.Message;
+
+public class WrappedNodeSelector implements NodeSelector {
+
+  private final NodeSelector delegate;
+
+  public WrappedNodeSelector(NodeSelector delegate) {
+    this.delegate = delegate;
+  }
+
+  @Override
+  public void init(Vertx vertx, ClusterManager clusterManager) {
+    delegate.init(vertx, clusterManager);
+  }
+
+  @Override
+  public void eventBusStarted() {
+    delegate.eventBusStarted();
+  }
+
+  @Override
+  public void selectForSend(Message<?> message, Promise<String> promise) {
+    delegate.selectForSend(message, promise);
+  }
+
+  @Override
+  public void selectForPublish(Message<?> message, Promise<Iterable<String>> promise) {
+    delegate.selectForPublish(message, promise);
+  }
+
+  @Override
+  public void registrationsUpdated(RegistrationUpdateEvent event) {
+    delegate.registrationsUpdated(event);
+  }
+
+  @Override
+  public void registrationsLost() {
+    delegate.registrationsLost();
+  }
+}
