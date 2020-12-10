2c16bf443d904a73be0a335f03c2a7b6e2e39449
Thomas Segismont <tsegismont@gmail.com>
2020-12-10 18:42:14 +0100

    Redeploy mode ignores --options arg
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

---
+++ src/test/java/io/vertx/core/impl/launcher/commands/OptionsRecordingLauncher.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2011-2020 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
+
+package io.vertx.core.impl.launcher.commands;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Launcher;
+import io.vertx.core.VertxOptions;
+
+public class OptionsRecordingLauncher extends Launcher {
+
+  private VertxOptions options;
+
+  public static void main(String[] args) {
+    new OptionsRecordingLauncher().dispatch(args);
+  }
+
+  @Override
+  public void beforeStartingVertx(VertxOptions options) {
+    this.options = options;
+  }
+
+  @Override
+  public void beforeDeployingVerticle(DeploymentOptions deploymentOptions) {
+    deploymentOptions.getConfig().put(OptionsRecordingLauncher.class.getName(), options.toJson());
+  }
+}
