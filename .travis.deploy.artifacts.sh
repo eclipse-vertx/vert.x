b11050ce4c95e71d50c81d29c2814b28707a6393
Julien Viet <julien@julienviet.com>
2021-02-16 10:44:11 +0100

    Port CI to GitHub actions

--- .travis.deploy.artifacts.sh
+++
@@ -1,5 +0,0 @@
-PROJECT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version -B | grep -v '\[')
-if [[ "$PROJECT_VERSION" =~ .*SNAPSHOT ]] && [[ "${TRAVIS_BRANCH}" =~ ^master$|^[0-9]+\.[0-9]+$ ]] && [[ "${TRAVIS_PULL_REQUEST}" = "false" ]];
-then
-  mvn deploy -s .travis.maven.settings.xml -DskipTests -B;
-fi
