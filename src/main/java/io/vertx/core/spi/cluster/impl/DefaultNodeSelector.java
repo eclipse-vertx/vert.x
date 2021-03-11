c52d3384b6600a66e88ccfe8cd04758862cf4a66
Thomas Segismont <tsegismont@gmail.com>
2021-03-11 09:37:38 +0100

    Give ClusterManager a chance to know if NodeSelector wants updates for an address (#3846)
    This can be useful to reduce load/traffic.
    Indeed, cluster managers such as HZ/ISPN/Ignite must query the backend to get a fresh view of the data.
    Consequently, if the NodeSelector is not interested, this query can be avoided
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- src/main/java/io/vertx/core/spi/cluster/impl/DefaultNodeSelector.java
+++ src/main/java/io/vertx/core/spi/cluster/impl/DefaultNodeSelector.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2020 Contributors to the Eclipse Foundation
+ * Copyright (c) 2011-2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -60,5 +60,10 @@ public class DefaultNodeSelector implements NodeSelector {
   @Override
   public void registrationsLost() {
     selectors.dataLost();
+  }
+
+  @Override
+  public boolean wantsUpdatesFor(String address) {
+    return selectors.hasEntryFor(address);
   }
 }
