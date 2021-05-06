ea81216c4750122ab8c5d9333572c976a22d02cb
Yann Tavernier <yanntavernier@gmail.com>
2021-05-06 07:19:48 +0200

    fix: typo in OpenOptions setDsync javadoc

--- src/main/java/io/vertx/core/file/OpenOptions.java
+++ src/main/java/io/vertx/core/file/OpenOptions.java
@@ -274,7 +274,7 @@ public class OpenOptions {
   }
 
   /**
-   * Set whether every write to the file's content  ill be written synchronously to the underlying hardware.
+   * Set whether every write to the file's content will be written synchronously to the underlying hardware.
    * @param dsync  true if sync
    * @return a reference to this, so the API can be used fluently
    */
