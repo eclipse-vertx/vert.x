eclipse-vertx/vert.x

commit 2f690d7d6420c3601b0d62eeb574cae6dc6b3426

Make sure metrics and cluster manager can be customized in Launcher
Fixes #3804

Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

src/main/java/io/vertx/core/impl/launcher/commands/BareCommand.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2019 Contributors to the Eclipse Foundation
+ * Copyright (c) 2011-2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -11,7 +11,10 @@
 
 package io.vertx.core.impl.launcher.commands;
 
-import io.vertx.core.*;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxException;
+import io.vertx.core.VertxOptions;
 import io.vertx.core.cli.annotations.*;
 import io.vertx.core.eventbus.AddressHelper;
 import io.vertx.core.eventbus.EventBusOptions;
@@ -20,8 +23,6 @@ import io.vertx.core.impl.launcher.VertxLifecycleHooks;
 import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.json.DecodeException;
 import io.vertx.core.json.JsonObject;
-import io.vertx.core.metrics.MetricsOptions;
-import io.vertx.core.spi.VertxMetricsFactory;
 import io.vertx.core.spi.launcher.ExecutionContext;
 
 import java.io.File;
@@ -212,18 +213,19 @@ public class BareCommand extends ClasspathHandler {
     options = builder.options();
     options.setEventBusOptions(eventBusOptions);
 
+    beforeStartingVertx(options);
+    builder.init();
+
     configureFromSystemProperties.set(log);
     try {
       configureFromSystemProperties(options, VERTX_OPTIONS_PROP_PREFIX);
       if (options.getMetricsOptions() != null) {
         configureFromSystemProperties(options.getMetricsOptions(), METRICS_OPTIONS_PROP_PREFIX);
       }
-      builder.init();
     } finally {
       configureFromSystemProperties.set(null);
     }
 
-    beforeStartingVertx(options);
     Vertx instance;
     if (isClustered()) {
       log.info("Starting clustering...");

src/test/java/io/vertx/core/impl/launcher/LauncherExtensibilityTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2019 Contributors to the Eclipse Foundation
+ * Copyright (c) 2011-2021 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
@@ -16,21 +16,22 @@ import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.cli.CLIException;
 import io.vertx.core.cli.annotations.Name;
-import io.vertx.core.impl.launcher.commands.BareCommand;
+import io.vertx.core.impl.VertxInternal;
 import io.vertx.core.impl.launcher.commands.CommandTestBase;
 import io.vertx.core.impl.launcher.commands.HttpTestVerticle;
 import io.vertx.core.impl.launcher.commands.RunCommandTest;
-import io.vertx.core.impl.logging.LoggerFactory;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.spi.launcher.DefaultCommand;
+import io.vertx.test.fakecluster.FakeClusterManager;
+import io.vertx.test.fakemetrics.FakeMetricsFactory;
 import org.junit.After;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertSame;
 
 /**
  * @author <a href="http://escoffier.me">Clement Escoffier</a>
@@ -168,6 +169,71 @@ public class LauncherExtensibilityTest extends CommandTestBase {
     });
 
     assertThat(RunCommandTest.getContent().getJsonObject("conf").getLong("time")).isEqualTo(time);
+  }
+
+  @Test
+  public void testThatCustomLauncherCanCustomizeMetricsOption() throws Exception {
+    Launcher myLauncher = new Launcher() {
+      @Override
+      protected String getMainVerticle() {
+        return HttpTestVerticle.class.getName();
+      }
+
+      @Override
+      public void afterStartingVertx(Vertx vertx) {
+        LauncherExtensibilityTest.this.vertx = vertx;
+      }
+
+      @Override
+      public void beforeStartingVertx(VertxOptions options) {
+        options.getMetricsOptions().setEnabled(true).setFactory(new FakeMetricsFactory());
+      }
+    };
+
+    myLauncher.dispatch(new String[0]);
+    assertWaitUntil(() -> {
+      try {
+        return RunCommandTest.getHttpCode() == 200;
+      } catch (IOException e) {
+        return false;
+      }
+    });
+
+    assertThat(RunCommandTest.getContent().getBoolean("metrics")).isEqualTo(true);
+  }
+
+  @Test
+  public void testThatCustomLauncherCanCustomizeClusterManager() throws Exception {
+    FakeClusterManager clusterManager = new FakeClusterManager();
+
+    Launcher myLauncher = new Launcher() {
+      @Override
+      protected String getMainVerticle() {
+        return HttpTestVerticle.class.getName();
+      }
+
+      @Override
+      public void afterStartingVertx(Vertx vertx) {
+        LauncherExtensibilityTest.this.vertx = vertx;
+      }
+
+      @Override
+      public void beforeStartingVertx(VertxOptions options) {
+        options.setClusterManager(clusterManager);
+      }
+    };
+
+    myLauncher.dispatch(new String[]{"-cluster"});
+    assertWaitUntil(() -> {
+      try {
+        return RunCommandTest.getHttpCode() == 200;
+      } catch (IOException e) {
+        return false;
+      }
+    });
+
+    assertThat(RunCommandTest.getContent().getBoolean("clustered")).isEqualTo(true);
+    assertSame(clusterManager, ((VertxInternal) vertx).getClusterManager());
   }
 
   @Name("foo")

commit d183eba05d986e1e50b6fcc7fd2a042e9f5188af

Fix regression in CLI introduced by 2f690d7d6420c3601b0d62eeb574cae6dc6b3426 that moved the VertxBuilder before the metrics options are initialized from the system properties which prevents metrics to be initialized from system properties - backports #4008

src/main/java/io/vertx/core/impl/launcher/commands/BareCommand.java
@@ -214,7 +214,6 @@ public class BareCommand extends ClasspathHandler {
     options.setEventBusOptions(eventBusOptions);
 
     beforeStartingVertx(options);
-    builder.init();
 
     configureFromSystemProperties.set(log);
     try {
@@ -222,6 +221,7 @@ public class BareCommand extends ClasspathHandler {
       if (options.getMetricsOptions() != null) {
         configureFromSystemProperties(options.getMetricsOptions(), METRICS_OPTIONS_PROP_PREFIX);
       }
+      builder.init();
     } finally {
       configureFromSystemProperties.set(null);
     }

src/test/java/io/vertx/core/LauncherTest.java
@@ -513,6 +513,7 @@ public class LauncherTest extends VertxTestBase {
     VertxOptions opts = launcher.getVertxOptions();
     CustomMetricsOptions custom = (CustomMetricsOptions) opts.getMetricsOptions();
     assertEquals("customPropertyValue", custom.getCustomProperty());
+    assertTrue(launcher.getVertx().isMetricsEnabled());
   }
 
   @Test