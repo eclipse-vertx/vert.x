96c360b02f75df7ba6e491c3ac04953ea942c085
Igor Manushin <igor.manushin@gmail.com>
2021-09-17 13:42:35 +0100

    Add file paths to exception details (#3975)
    * Add file paths to exception details
    
    * Move methods into the more convenient place.
    
    * Argument naming fix
    
    * tests fix
    
    * tests fix

--- src/main/java/io/vertx/core/file/impl/FileCache.java
+++ src/main/java/io/vertx/core/file/impl/FileCache.java
@@ -56,7 +56,7 @@ class FileCache {
         Files.createDirectories(cacheDir.toPath(), PosixFilePermissions.asFileAttribute(perms));
       }
     } catch (IOException e) {
-      throw new IllegalStateException("Failed to create cache dir: " + cacheDirName, e);
+      throw new IllegalStateException(FileSystemImpl.getFolderAccessErrorMessage("create", fileCacheDir), e);
     }
     return cacheDir;
   }
