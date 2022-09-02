6f71b5f2adfb440b554ec9979ff3fdfd7830e985
Thomas Segismont <tsegismont@gmail.com>
2022-09-02 10:07:33 +0200

    Do not log before being LoggerFactory is fully initialized (#4459)
    * Do not log before being LoggerFactory is fully initialized
    
    Fixes reactiverse/reactiverse-contextual-logging#52
    
    Otherwise, a logger extension might use Vert.x classes before they are fully initialized.
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>
    
    * Add integration test for io.vertx.core.logging.LoggerFactory init fix
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- src/main/java/io/vertx/core/logging/Log4j2LogDelegateFactory.java
+++ src/main/java/io/vertx/core/logging/Log4j2LogDelegateFactory.java
@@ -13,25 +13,22 @@ package io.vertx.core.logging;
 
 import io.vertx.core.spi.logging.LogDelegate;
 import io.vertx.core.spi.logging.LogDelegateFactory;
+import org.apache.logging.log4j.LogManager;
 
 /**
  * A {@link LogDelegateFactory} which creates {@link Log4j2LogDelegate} instances.
  *
  * @author Clement Escoffier - clement@apache.org
- *
- *
  */
-public class Log4j2LogDelegateFactory implements LogDelegateFactory
-{
+public class Log4j2LogDelegateFactory implements LogDelegateFactory {
 
   @Override
   public boolean isAvailable() {
-    return true;
+    return LogManager.getLogger(Log4j2LogDelegateFactory.class) != null;
   }
 
-  public LogDelegate createDelegate(final String name)
-   {
-      return new Log4j2LogDelegate(name);
-   }
+  public LogDelegate createDelegate(final String name) {
+    return new Log4j2LogDelegate(name);
+  }
 
 }
