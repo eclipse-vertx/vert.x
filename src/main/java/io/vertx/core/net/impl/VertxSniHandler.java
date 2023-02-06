33626703be70fd2664d10f898ffe98c852c95c5a
Julien Viet <julien@julienviet.com>
2023-02-06 13:32:34 +0100

    Remove VertxSniHandler handshake timeout since it is supported in SniHandler

--- src/main/java/io/vertx/core/net/impl/VertxSniHandler.java
+++ src/main/java/io/vertx/core/net/impl/VertxSniHandler.java
@@ -11,56 +11,31 @@
 package io.vertx.core.net.impl;
 
 import io.netty.buffer.ByteBufAllocator;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.ssl.SniCompletionEvent;
 import io.netty.handler.ssl.SniHandler;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslHandler;
 import io.netty.util.AsyncMapping;
-import io.netty.util.Mapping;
-import io.netty.util.concurrent.ScheduledFuture;
 
-import javax.net.ssl.SSLException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Extend the {@code SniHandler} to support handshake timeout
+ * Extend the {@code SniHandler} to support delegated  task executor
  *
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
  */
 class VertxSniHandler extends SniHandler {
 
-  private final long handshakeTimeoutMillis;
-  private ScheduledFuture<?> timeoutFuture;
   private final Executor delegatedTaskExec;
 
   public VertxSniHandler(AsyncMapping<? super String, ? extends SslContext> mapping, long handshakeTimeoutMillis, Executor delegatedTaskExec) {
-    super(mapping);
+    super(mapping, handshakeTimeoutMillis);
 
-    this.handshakeTimeoutMillis = handshakeTimeoutMillis;
     this.delegatedTaskExec = delegatedTaskExec;
   }
 
   @Override
-  public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
-    if (handshakeTimeoutMillis > 0) {
-      // We assume to always be added in an active channel
-      assert(ctx.channel().isActive());
-      timeoutFuture = ctx.executor().schedule(() -> {
-        SSLException exception = new SSLException("handshake timed out after " + handshakeTimeoutMillis + "ms");
-        ctx.fireUserEventTriggered(new SniCompletionEvent(exception));
-        ctx.close();
-      }, handshakeTimeoutMillis, TimeUnit.MILLISECONDS);
-    }
-    super.handlerAdded(ctx);
-  }
-
-  @Override
   protected SslHandler newSslHandler(SslContext context, ByteBufAllocator allocator) {
-    if (timeoutFuture != null) {
-      timeoutFuture.cancel(false);
-    }
     SslHandler sslHandler = context.newHandler(allocator, delegatedTaskExec);
     sslHandler.setHandshakeTimeout(handshakeTimeoutMillis, TimeUnit.MILLISECONDS);
     return sslHandler;
