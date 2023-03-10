35bdd7380092803c6822c91a60ecb0e2968ce6ea
Julien Viet <julien@julienviet.com>
2023-03-10 17:08:35 +0100

    Remove callbacks from SharedCounterTest

--- src/test/java/io/vertx/core/shareddata/SharedCounterTest.java
+++ src/test/java/io/vertx/core/shareddata/SharedCounterTest.java
@@ -12,7 +12,6 @@
 package io.vertx.core.shareddata;
 
 import io.vertx.core.Vertx;
-import io.vertx.core.shareddata.Counter;
 import io.vertx.test.core.VertxTestBase;
 import org.junit.Test;
 
@@ -29,198 +28,140 @@ public class SharedCounterTest extends VertxTestBase {
 
   @Test
   public void testIllegalArguments() throws Exception {
-    assertNullPointerException(() -> getVertx().sharedData().getCounter(null, ar -> {}));
-    assertNullPointerException(() -> getVertx().sharedData().getCounter("foo", null));
-    getVertx().sharedData().getCounter("foo", ar -> {
-      Counter counter = ar.result();
-      assertNullPointerException(() -> counter.get(null));
-      assertNullPointerException(() -> counter.incrementAndGet(null));
-      assertNullPointerException(() -> counter.getAndIncrement(null));
-      assertNullPointerException(() -> counter.decrementAndGet(null));
-      assertNullPointerException(() -> counter.addAndGet(1, null));
-      assertNullPointerException(() -> counter.getAndAdd(1, null));
-      assertNullPointerException(() -> counter.compareAndSet(1, 1, null));
-      testComplete();
-    });
-    await();
+    assertNullPointerException(() -> getVertx().sharedData().getCounter(null));
   }
 
   @Test
   public void testGet() {
-    getVertx().sharedData().getCounter("foo", ar -> {
-      assertTrue(ar.succeeded());
-      Counter counter = ar.result();
-      counter.get(ar2 -> {
-        assertTrue(ar2.succeeded());
-        assertEquals(0l, ar2.result().longValue());
+    getVertx().sharedData().getCounter("foo").onComplete(onSuccess(counter -> {
+      counter.get().onComplete(onSuccess(res -> {
+        assertEquals(0L, res.longValue());
         testComplete();
-      });
-    });
+      }));
+    }));
     await();
   }
 
   @Test
   public void testIncrementAndGet() {
-    getVertx().sharedData().getCounter("foo", ar -> {
-      assertTrue(ar.succeeded());
-      Counter counter = ar.result();
-      counter.incrementAndGet(ar2 -> {
-        assertTrue(ar2.succeeded());
-        assertEquals(1l, ar2.result().longValue());
-        getVertx().sharedData().getCounter("foo", ar3 -> {
-          assertTrue(ar3.succeeded());
-          Counter counter2 = ar3.result();
-          counter2.incrementAndGet(ar4 -> {
-            assertTrue(ar4.succeeded());
-            assertEquals(2l, ar4.result().longValue());
+    getVertx().sharedData().getCounter("foo").onComplete(onSuccess(counter -> {
+      counter.incrementAndGet().onComplete(onSuccess(res1 -> {
+        assertEquals(1L, res1.longValue());
+        getVertx().sharedData().getCounter("foo").onComplete(onSuccess(counter2 -> {
+          counter2.incrementAndGet().onComplete(onSuccess(res2 -> {
+            assertEquals(2L, res2.longValue());
             testComplete();
-          });
-        });
-      });
-    });
+          }));
+        }));
+      }));
+    }));
     await();
   }
 
   @Test
   public void testGetAndIncrement() {
-    getVertx().sharedData().getCounter("foo", ar -> {
-      assertTrue(ar.succeeded());
-      Counter counter = ar.result();
-      counter.getAndIncrement(ar2 -> {
-        assertTrue(ar2.succeeded());
-        assertEquals(0l, ar2.result().longValue());
-        getVertx().sharedData().getCounter("foo", ar3 -> {
-          assertTrue(ar3.succeeded());
-          Counter counter2 = ar3.result();
-          counter2.getAndIncrement(ar4 -> {
-            assertTrue(ar4.succeeded());
-            assertEquals(1l, ar4.result().longValue());
-            counter2.get(ar5 -> {
-              assertTrue(ar5.succeeded());
-              assertEquals(2l, ar5.result().longValue());
+    getVertx().sharedData().getCounter("foo").onComplete(onSuccess(counter -> {
+      counter.getAndIncrement().onComplete(onSuccess(res -> {
+        assertEquals(0L, res.longValue());
+        getVertx().sharedData().getCounter("foo").onComplete(onSuccess(counter2 -> {
+          counter2.getAndIncrement().onComplete(onSuccess(res2 -> {
+            assertEquals(1L, res2.longValue());
+            counter2.get().onComplete(onSuccess(res3 -> {
+              assertEquals(2L, res3.longValue());
               testComplete();
-            });
-          });
-        });
-      });
-    });
+            }));
+          }));
+        }));
+      }));
+    }));
     await();
   }
 
   @Test
   public void testDecrementAndGet() {
-    getVertx().sharedData().getCounter("foo", ar -> {
-      assertTrue(ar.succeeded());
-      Counter counter = ar.result();
-      counter.decrementAndGet(ar2 -> {
-        assertTrue(ar2.succeeded());
-        assertEquals(-1l, ar2.result().longValue());
-        getVertx().sharedData().getCounter("foo", ar3 -> {
-          assertTrue(ar3.succeeded());
-          Counter counter2 = ar3.result();
-          counter2.decrementAndGet(ar4 -> {
-            assertTrue(ar4.succeeded());
-            assertEquals(-2l, ar4.result().longValue());
+    getVertx().sharedData().getCounter("foo").onComplete(onSuccess(counter -> {
+      counter.decrementAndGet().onComplete(onSuccess(res -> {
+        assertEquals(-1L, res.longValue());
+        getVertx().sharedData().getCounter("foo").onComplete(onSuccess(counter2 -> {
+          counter2.decrementAndGet().onComplete(onSuccess(res2 -> {
+            assertEquals(-2L, res2.longValue());
             testComplete();
-          });
-        });
-      });
-    });
+          }));
+        }));
+      }));
+    }));
     await();
   }
 
   @Test
   public void testAddAndGet() {
-    getVertx().sharedData().getCounter("foo", ar -> {
-      assertTrue(ar.succeeded());
-      Counter counter = ar.result();
-      counter.addAndGet(2, ar2 -> {
-        assertTrue(ar2.succeeded());
-        assertEquals(2l, ar2.result().longValue());
-        getVertx().sharedData().getCounter("foo", ar3 -> {
-          assertTrue(ar3.succeeded());
-          Counter counter2 = ar3.result();
-          counter2.addAndGet(2l, ar4 -> {
-            assertTrue(ar4.succeeded());
-            assertEquals(4l, ar4.result().longValue());
+    getVertx().sharedData().getCounter("foo").onComplete(onSuccess(counter -> {
+      counter.addAndGet(2).onComplete(onSuccess(res -> {
+        assertEquals(2L, res.longValue());
+        getVertx().sharedData().getCounter("foo").onComplete(onSuccess(counter2 -> {
+          counter2.addAndGet(2L).onComplete(onSuccess(res2 -> {
+            assertEquals(4L, res2.longValue());
             testComplete();
-          });
-        });
-      });
-    });
+          }));
+        }));
+      }));
+    }));
     await();
   }
 
   @Test
   public void getAndAdd() {
-    getVertx().sharedData().getCounter("foo", ar -> {
-      assertTrue(ar.succeeded());
-      Counter counter = ar.result();
-      counter.getAndAdd(2, ar2 -> {
-        assertTrue(ar2.succeeded());
-        assertEquals(0l, ar2.result().longValue());
-        getVertx().sharedData().getCounter("foo", ar3 -> {
-          assertTrue(ar3.succeeded());
-          Counter counter2 = ar3.result();
-          counter2.getAndAdd(2l, ar4 -> {
-            assertTrue(ar4.succeeded());
-            assertEquals(2l, ar4.result().longValue());
-            counter2.get(ar5 -> {
-              assertTrue(ar5.succeeded());
-              assertEquals(4l, ar5.result().longValue());
+    getVertx().sharedData().getCounter("foo").onComplete(onSuccess(counter -> {
+      counter.getAndAdd(2).onComplete(onSuccess(res -> {
+        assertEquals(0L, res.longValue());
+        getVertx().sharedData().getCounter("foo").onComplete(onSuccess(counter2 -> {
+          counter2.getAndAdd(2L).onComplete(onSuccess(res2 -> {
+            assertEquals(2L, res2.longValue());
+            counter2.get().onComplete(onSuccess(res3 -> {
+              assertEquals(4L, res3.longValue());
               testComplete();
-            });
-          });
-        });
-      });
-    });
+            }));
+          }));
+        }));
+      }));
+    }));
     await();
   }
 
   @Test
   public void testCompareAndSet() {
-    getVertx().sharedData().getCounter("foo", ar -> {
-      assertTrue(ar.succeeded());
-      Counter counter = ar.result();
-      counter.compareAndSet(0l, 2l, onSuccess(result -> {
-        getVertx().sharedData().getCounter("foo", ar3 -> {
-          assertTrue(ar3.succeeded());
-          Counter counter2 = ar3.result();
-          counter2.compareAndSet(2l, 4l, ar4 -> {
-            assertTrue(ar4.succeeded());
-            assertTrue(ar4.result());
-            counter2.compareAndSet(3l, 5l, ar5 -> {
-              assertTrue(ar5.succeeded());
-              assertFalse(ar5.result());
+    getVertx().sharedData().getCounter("foo").onComplete(onSuccess(counter -> {
+      counter.compareAndSet(0L, 2L).onComplete(onSuccess(result -> {
+        getVertx().sharedData().getCounter("foo").onComplete(onSuccess(counter2 -> {
+          counter2.compareAndSet(2L, 4L).onComplete(onSuccess(result2 -> {
+            assertTrue(result2);
+            counter2.compareAndSet(3L, 5L).onComplete(onSuccess(result3 -> {
+              assertFalse(result3);
               testComplete();
-            });
-          });
-        });
+            }));
+          }));
+        }));
       }));
-    });
+    }));
     await();
   }
 
   @Test
   public void testDifferentCounters() {
-    getVertx().sharedData().getCounter("foo", ar -> {
-      assertTrue(ar.succeeded());
-      Counter counter = ar.result();
-      counter.incrementAndGet(onSuccess(res -> {
-        assertEquals(1l, res.longValue());
-        getVertx().sharedData().getCounter("bar", ar3 -> {
-          assertTrue(ar3.succeeded());
-          Counter counter2 = ar3.result();
-          counter2.incrementAndGet(ar4 -> {
-            assertEquals(1l, ar4.result().longValue());
-            counter.incrementAndGet(ar5 -> {
-              assertEquals(2l, ar5.result().longValue());
+    getVertx().sharedData().getCounter("foo").onComplete(onSuccess(counter -> {
+      counter.incrementAndGet().onComplete(onSuccess(res -> {
+        assertEquals(1L, res.longValue());
+        getVertx().sharedData().getCounter("bar").onComplete(onSuccess(counter2 -> {
+          counter2.incrementAndGet().onComplete(onSuccess(res2 -> {
+            assertEquals(1L, res2.longValue());
+            counter.incrementAndGet().onComplete(onSuccess(res3 -> {
+              assertEquals(2L, res3.longValue());
               testComplete();
-            });
-          });
-        });
+            }));
+          }));
+        }));
       }));
-    });
+    }));
     await();
   }
 
