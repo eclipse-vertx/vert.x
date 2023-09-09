f95663110186022c0fadf8d204703cf251302524
A.Fink <aprpda@gmail.com>
2023-09-09 20:40:14 +0300

    MessageConsumerImpl: better tests

--- src/test/java/io/vertx/core/eventbus/impl/MessageConsumerImplTest.java
+++ src/test/java/io/vertx/core/eventbus/impl/MessageConsumerImplTest.java
@@ -1,21 +1,21 @@
 package io.vertx.core.eventbus.impl;
 
-import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.Handler;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.eventbus.MessageConsumer;
 import io.vertx.core.impl.VertxImpl;
 import io.vertx.test.core.VertxTestBase;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  @author Andrey Fink 2023-09-09
  @see io.vertx.core.eventbus.impl.MessageConsumerImpl */
-public class MessageConsumerImplTest extends VertxTestBase  {
-
-  protected EventBus eb;
+public class MessageConsumerImplTest extends VertxTestBase {
 
   @Test public void testBits (){
-    var mci = new MessageConsumerImpl<String>(( (VertxImpl) vertx ).getOrCreateContext(), (EventBusImpl) eb, "x.y", true);
+    var mci = new MessageConsumerImpl<String>(( (VertxImpl) vertx ).getOrCreateContext(), null, "x.y", true);
 
     assertTrue(mci.is(MessageConsumerImpl.OPT_LOCAL_ONLY));
     assertFalse(mci.is(MessageConsumerImpl.OPT_REGISTERED));
@@ -47,5 +47,46 @@ public class MessageConsumerImplTest extends VertxTestBase  {
     assertFalse(mci.is(MessageConsumerImpl.OPT_LOCAL_ONLY));
     assertFalse(mci.is(MessageConsumerImpl.OPT_REGISTERED));
     assertFalse(mci.is(MessageConsumerImpl.OPT_HANDLER));
+  }
+
+
+  @Test public void testSimple () throws InterruptedException{
+    var q = new LinkedBlockingQueue<Message<Integer>>();
+    var eb = vertx.eventBus();
+    Handler<Message<Integer>> add = q::add;
+
+    var consumer = (MessageConsumerImpl<Integer>) eb.consumer("x.y.z", add);
+    assertSame(add, consumer.getHandler());
+    assertTrue(consumer.is(MessageConsumerImpl.OPT_REGISTERED));
+    assertTrue(consumer.is(MessageConsumerImpl.OPT_HANDLER));
+
+    eb.send("x.y.z", 42);
+    var msg = q.take();
+    assertEquals(42, msg.body().intValue());
+
+    consumer.handler(null);
+    assertSame(add, consumer.getHandler());// not null! :-)
+    assertFalse(consumer.is(MessageConsumerImpl.OPT_REGISTERED));
+    assertFalse(consumer.is(MessageConsumerImpl.OPT_HANDLER));
+
+    // dev/null
+    eb.send("x.y.z", 45);
+    assertTrue(q.isEmpty());// but not used ^
+  }
+
+  @Test public void testNoHandler () throws InterruptedException{
+    var eb = vertx.eventBus();
+
+    var consumer = (MessageConsumerImpl<Integer>) eb.<Integer>consumer("x.y.z");
+    assertNull(consumer.getHandler());
+    assertFalse(consumer.is(MessageConsumerImpl.OPT_REGISTERED));
+    assertFalse(consumer.is(MessageConsumerImpl.OPT_HANDLER));
+
+    eb.send("x.y.z", 42);
+
+    consumer.handler(null);
+    assertNull(consumer.getHandler());
+    assertFalse(consumer.is(MessageConsumerImpl.OPT_REGISTERED));
+    assertFalse(consumer.is(MessageConsumerImpl.OPT_HANDLER));
   }
 }
