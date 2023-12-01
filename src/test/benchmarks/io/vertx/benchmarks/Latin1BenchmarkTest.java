a52699d296564c79c1759b349d43d7dd8c1888a4
A.Fink <andrey.fink@platform-s.com>
2023-12-02 01:28:16 +0300

    fix micro optimization 🤦‍♂️

--- src/test/benchmarks/io/vertx/benchmarks/Latin1BenchmarkTest.java
+++ src/test/benchmarks/io/vertx/benchmarks/Latin1BenchmarkTest.java
@@ -2,15 +2,8 @@ package io.vertx.benchmarks;
 
 import org.junit.Test;
 import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Threads;
-import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -22,11 +15,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.nio.charset.StandardCharsets.*;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 /**
  Task:
@@ -88,7 +82,7 @@ public class Latin1BenchmarkTest extends BenchmarkBase {
   public static byte[] latinBytesV1b (String str){
     int len = str.length();
     byte[] bytes = new byte[len];
-    for (int i = 0; i < str.length(); i++){
+    for (int i = 0; i < len; i++){
       char c = str.charAt(i);
       bytes[i] = (byte)(c & 0xFF);
     }
