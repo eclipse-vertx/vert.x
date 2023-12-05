7b9bc946b08cf6aafb80f20fa55ac44fe4928b88
Nils Renaud <renaud.nils@gmail.com>
2023-12-05 09:24:24 +0100

    4982 Add unit test to test concurrent writing from separate thread
    Signed-off-by: Nils Renaud <renaud.nils@gmail.com>

--- src/test/java/io/vertx/core/http/HttpTest.java
+++ src/test/java/io/vertx/core/http/HttpTest.java
@@ -6673,4 +6673,39 @@ public abstract class HttpTest extends HttpTestBase {
     } catch (IllegalArgumentException ignore) {
     }
   }
+
+  @Test
+  public void testConcurrentWrites() throws Exception {
+    waitFor(1);
+    ExecutorService executor = Executors.newFixedThreadPool(1);
+    AtomicReference<String> received = new AtomicReference<>();
+    server.requestHandler(req -> req.body()
+                                    .onSuccess(buffer -> {
+                                      received.set(buffer.toString());
+                                      req.response().end();
+                                    }));
+    startServer(testAddress);
+    client.close();
+    client = vertx.createHttpClient(createBaseClientOptions());
+    client.request(requestOptions)
+          .compose(req -> {
+            req.setChunked(true);
+            Future<Void> future = req.sendHead();
+            if (this instanceof Http1xTest) {
+              while (!future.isComplete()) { } // Wait for the header to be sent.
+            }
+            AtomicBoolean latch = new AtomicBoolean(false);
+            executor.submit(() -> {
+              req.write("msg1");
+              latch.set(true); // Release Event-loop thread
+            });
+            while (!latch.get()) { } // Active wait for the event to be published
+            req.write("msg2");
+            req.end();
+            return req.response();
+          })
+          .onComplete(onSuccess(resp -> complete()));
+    await();
+    assertEquals("msg1msg2", received.get());
+  }
 }
