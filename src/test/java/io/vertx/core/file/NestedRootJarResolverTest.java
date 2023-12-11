1e56721ea0f6cb81a559a5feefd7871f4d153712
Julien Viet <julien@julienviet.com>
2023-12-11 17:18:31 +0100

    Change the analyze of the list of entries of a jar URL and use !/ as separator regardless of the prefix (.zip,.war,.jar) we used to include, in order to deal with custom JarFile implementations.
    The jar URL unpacking for support nested directory was also not correctly implemented although working due to the previous way to analyze the list of entries of a jar URL. The proper way to support nested directories is to unpack the nested directory as a resource and then copy it (relocate) to the correct cache path, e.g. unpack jar:file:/path/to/nesting.jar!/xxx-inf/classes and then copy xxx-inf/classes to the root of the cache, so the subsequent lookup for the nested part designates the correct file. This has been implemented in order to support this properly in a way compatible with the list of entries correction.

--- src/test/java/io/vertx/core/file/NestedRootJarResolverTest.java
+++ src/test/java/io/vertx/core/file/NestedRootJarResolverTest.java
@@ -12,16 +12,24 @@
 package io.vertx.core.file;
 
 import io.vertx.test.core.TestUtils;
+import junit.framework.AssertionFailedError;
 import org.junit.Assert;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.util.function.Function;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 /**
+ * what we are trying to resolve:
+ * - webroot/hello.txt -> jar:file:/path/to/file.jar!/BOOT-INF/classes!/webroot/hello.txt
+ * - BOOT-INF/classes -> jar:file:/path/to/file.jar!/BOOT-INF/classes
+ *
  * @author Thomas Segismont
  */
 public class NestedRootJarResolverTest extends FileResolverTestBase {
@@ -30,13 +38,27 @@ public class NestedRootJarResolverTest extends FileResolverTestBase {
   protected ClassLoader resourcesLoader(File baseDir) throws Exception {
     File nestedFiles = Files.createTempFile(TestUtils.MAVEN_TARGET_DIR.toPath(), "", "nestedroot.jar").toFile();
     Assert.assertTrue(nestedFiles.delete());
-    ZipFileResolverTest.getFiles(baseDir, nestedFiles, ZipOutputStream::new, name -> new ZipEntry("nested-inf/classes/" + name));
+    ZipFileResolverTest.getFiles(baseDir, nestedFiles, new Function<OutputStream, ZipOutputStream>() {
+      @Override
+      public ZipOutputStream apply(OutputStream outputStream) {
+        ZipOutputStream zip = new ZipOutputStream(outputStream);
+        try {
+          zip.putNextEntry(new ZipEntry("nested-inf/classes/"));
+          zip.closeEntry();
+        } catch (IOException e) {
+          throw new AssertionFailedError();
+        }
+        return zip;
+      }
+    }, name -> new ZipEntry("nested-inf/classes/" + name));
     URL webrootURL = nestedFiles.toURI().toURL();
     return new ClassLoader(Thread.currentThread().getContextClassLoader()) {
       @Override
       public URL getResource(String name) {
         try {
-          if (name.startsWith("webroot")) {
+          if (name.equals("nested-inf/classes")) {
+            return new URL("jar:" + webrootURL + "!/nested-inf/classes");
+          } else if (name.startsWith("webroot")) {
             return new URL("jar:" + webrootURL + "!/nested-inf/classes!/" + name);
           } else if (name.equals("afile.html") || name.equals("afile with spaces.html") || name.equals("afilewithspaceatend ")) {
             return new URL("jar:" + webrootURL + "!/nested-inf/classes!/" + name);
