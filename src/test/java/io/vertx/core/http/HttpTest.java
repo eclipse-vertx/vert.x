dfd679e905c96e1a36c44786cef0aed25bdc0115
Julien Viet <julien@julienviet.com>
2023-12-12 14:29:34 +0100

    Improve HttpTest#testConcurrentWrite

--- src/test/java/io/vertx/core/http/HttpTest.java
+++ src/test/java/io/vertx/core/http/HttpTest.java
@@ -6616,33 +6616,20 @@ public abstract class HttpTest extends HttpTestBase {
     client.close();
     client = vertx.createHttpClient(createBaseClientOptions());
     client.request(requestOptions)
-          .compose(req -> {
-            req.setChunked(true);
-            Future<Void> future = req.sendHead();
-            if (this instanceof Http1xTest) {
-              // Wait for the header to be sent.
-              while (!future.isComplete()) {
-                AtomicInteger count = new AtomicInteger(1000);
-                while (count.decrementAndGet() >= 0) {
-                }
-              }
-            }
-            AtomicBoolean latch = new AtomicBoolean(false);
-            new Thread(() -> {
-              req.write("msg1");
-              latch.set(true); // Release Event-loop thread
-            }).start();
-            while (!latch.get()) {
-              AtomicInteger count = new AtomicInteger(1000);
-              while (count.decrementAndGet() >= 0) {
-                // Active wait for the event to be published
-              }
-            }
-            req.write("msg2");
-            req.end();
-            return req.response();
-          })
-          .onComplete(onSuccess(resp -> complete()));
+      .compose(req -> req.setChunked(true).sendHead().compose(v -> {
+        AtomicBoolean latch = new AtomicBoolean(false);
+        new Thread(() -> {
+          req.write("msg1");
+          latch.set(true); // Release Event-loop thread
+        }).start();
+        // Active wait for the event to be published
+        while (!latch.get()) {
+        }
+        req.write("msg2");
+        req.end();
+        return req.response();
+      }))
+      .onComplete(onSuccess(resp -> complete()));
     await();
     assertEquals("msg1msg2", received.get());
   }
