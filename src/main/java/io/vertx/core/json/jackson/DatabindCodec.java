4bdf2238e7b944acda773f2b1b39db74960328bd
mariofusco <mario.fusco@gmail.com>
2024-02-08 10:58:06 +0100

    Improve JsonFactory creation and propagate its usage to DatabindCodec

--- src/main/java/io/vertx/core/json/jackson/DatabindCodec.java
+++ src/main/java/io/vertx/core/json/jackson/DatabindCodec.java
@@ -33,7 +33,7 @@ import java.util.Map;
  */
 public class DatabindCodec extends JacksonCodec {
 
-  private static final ObjectMapper mapper = new ObjectMapper();
+  private static final ObjectMapper mapper = new ObjectMapper(JacksonCodec.factory);
 
   static {
     initialize();
