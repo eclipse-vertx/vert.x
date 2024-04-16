212115e5334310a8c5c1e5a6ecb16d3f656c2072
yinhang <pigbayspy@outlook.com>
2024-04-16 22:23:12 +0800

    test: rename test class name

---
+++ src/test/java/io/vertx/core/http/MimeTypeCaseInsensitiveTest.java
@@ -0,0 +1,21 @@
+package io.vertx.core.http;
+
+import io.vertx.core.http.impl.MimeMapping;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MimeTypeCaseInsensitiveTest {
+  @Test
+  public void testGetMimeTypeFromPath() {
+    String filepath = "/Users/leo/Pictures/IMG_4157.jpg";
+    String mimeType = MimeMapping.getMimeTypeForFilename(filepath);
+    Assert.assertEquals("get wrong mime type", "image/jpeg", mimeType);
+  }
+
+  @Test
+  public void testGetMimeTypeCaseInsensitive() {
+    String filepath = "/Users/leo/Pictures/IMG_4157.JPG";
+    String mimeType = MimeMapping.getMimeTypeForFilename(filepath);
+    Assert.assertEquals("get wrong mime type", "image/jpeg", mimeType);
+  }
+}
