6a957f77249624e081d5a145942621ac02ef3770
Julien Viet <julien@julienviet.com>
2024-06-04 17:29:42 +0200

    Fix a few bugs related to non event-loop thread writes.
    - Incorrect HTTP server metrics report for non event-loop thread writes #5222
    - HTTP/2 push is not flushed when written from a non event-loop thread #5223

--- src/main/java/io/vertx/core/http/impl/Http2ServerStream.java
+++ src/main/java/io/vertx/core/http/impl/Http2ServerStream.java
@@ -10,6 +10,7 @@
  */
 package io.vertx.core.http.impl;
 
+import io.netty.channel.EventLoop;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpHeaderValues;
 import io.netty.handler.codec.http2.Http2Headers;
@@ -18,12 +19,10 @@ import io.vertx.core.Promise;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpFrame;
 import io.vertx.core.http.HttpMethod;
-import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.StreamPriority;
 import io.vertx.core.http.impl.headers.Http2HeadersAdaptor;
 import io.vertx.core.impl.ContextInternal;
 import io.vertx.core.net.HostAndPort;
-import io.vertx.core.net.impl.HostAndPortImpl;
 import io.vertx.core.spi.metrics.HttpServerMetrics;
 import io.vertx.core.spi.metrics.Metrics;
 import io.vertx.core.spi.observability.HttpRequest;
@@ -238,13 +237,23 @@ class Http2ServerStream extends VertxHttp2Stream<Http2ServerConnection> {
     return metric;
   }
 
-  public HttpServerRequest routed(String route) {
+  public void routed(String route) {
     if (METRICS_ENABLED) {
-      HttpServerMetrics metrics = conn.metrics();
-      if (metrics != null && !responseEnded) {
-        metrics.requestRouted(metric, route);
+      EventLoop eventLoop = vertx.getOrCreateContext().nettyEventLoop();
+      synchronized (this) {
+        if (!eventLoop.inEventLoop()) {
+          eventLoop.execute(() -> routedInternal(route));
+          return;
+        }
       }
+      routedInternal(route);
     }
-    return null;
+  }
+
+  private void routedInternal(String route) {
+    HttpServerMetrics metrics = conn.metrics();
+    if (metrics != null && !responseEnded) {
+      metrics.requestRouted(metric, route);
+    }
   }
 }
