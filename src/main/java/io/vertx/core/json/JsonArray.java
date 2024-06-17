392a0014b14c6f4b843a18393c3f03f58ad82cec
Julien Viet <julien@julienviet.com>
2024-06-17 16:50:25 +0200

    The code to compare two numbers in json object/array does not correctly compare BigInteger/BigDecimal values.
    Fix this issue and also simplify / refactor this code to be more compact and reused between object/array

--- src/main/java/io/vertx/core/json/JsonArray.java
+++ src/main/java/io/vertx/core/json/JsonArray.java
@@ -20,6 +20,7 @@ import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
+import static io.vertx.core.json.JsonObject.compareObjects;
 import static io.vertx.core.json.impl.JsonUtil.*;
 import static java.time.format.DateTimeFormatter.ISO_INSTANT;
 
@@ -644,42 +645,7 @@ public class JsonArray implements Iterable<Object>, ClusterSerializable, Shareab
     for (int i = 0; i < this.size(); i++) {
       Object thisValue = this.getValue(i);
       Object otherValue = other.getValue(i);
-      // identity check
-      if (thisValue == otherValue) {
-        continue;
-      }
-      // special case for numbers
-      if (thisValue instanceof Number && otherValue instanceof Number && thisValue.getClass() != otherValue.getClass()) {
-        Number n1 = (Number) thisValue;
-        Number n2 = (Number) otherValue;
-        // floating point values
-        if (thisValue instanceof Float || thisValue instanceof Double || otherValue instanceof Float || otherValue instanceof Double) {
-          // compare as floating point double
-          if (n1.doubleValue() == n2.doubleValue()) {
-            // same value check the next entry
-            continue;
-          }
-        }
-        if (thisValue instanceof Integer || thisValue instanceof Long || otherValue instanceof Integer || otherValue instanceof Long) {
-          // compare as integer long
-          if (n1.longValue() == n2.longValue()) {
-            // same value check the next entry
-            continue;
-          }
-        }
-      }
-      // special case for char sequences
-      if (thisValue instanceof CharSequence && otherValue instanceof CharSequence && thisValue.getClass() != otherValue.getClass()) {
-        CharSequence s1 = (CharSequence) thisValue;
-        CharSequence s2 = (CharSequence) otherValue;
-
-        if (Objects.equals(s1.toString(), s2.toString())) {
-          // same value check the next entry
-          continue;
-        }
-      }
-      // fallback to standard object equals checks
-      if (!Objects.equals(thisValue, otherValue)) {
+      if (thisValue != otherValue && !compareObjects(thisValue, otherValue)) {
         return false;
       }
     }
