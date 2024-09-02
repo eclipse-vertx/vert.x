6349832b1a1e3770da83a43e587f2c8e4ddbcf84
imz87 <zolfaghari19@gmail.com>
2024-09-02 18:11:25 +0330

    feat: extract streams from Http2Connection

---
+++ vertx-core/src/main/java/io/vertx/core/http/impl/HttpStream.java
@@ -0,0 +1,203 @@
+package io.vertx.core.http.impl;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.core.MultiMap;
+import io.vertx.core.VertxException;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpFrame;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.http.HttpVersion;
+import io.vertx.core.http.StreamPriorityBase;
+import io.vertx.core.http.impl.headers.HeadersMultiMap;
+import io.vertx.core.http.impl.headers.VertxHttpHeaders;
+import io.vertx.core.internal.ContextInternal;
+import io.vertx.core.net.impl.ConnectionBase;
+import io.vertx.core.spi.metrics.ClientMetrics;
+import io.vertx.core.spi.tracing.VertxTracer;
+
+import java.util.Map;
+
+abstract class HttpStream<C extends ConnectionBase, S> extends VertxHttpStreamBase<C, S> {
+
+  private final boolean push;
+  private HttpResponseHead response;
+  protected Object metric;
+  protected Object trace;
+  protected boolean requestEnded;
+  private boolean responseEnded;
+  protected Handler<HttpResponseHead> headHandler;
+  protected Handler<Buffer> chunkHandler;
+  protected Handler<MultiMap> endHandler;
+  protected Handler<StreamPriorityBase> priorityHandler;
+  protected Handler<Void> drainHandler;
+  protected Handler<Void> continueHandler;
+  protected Handler<MultiMap> earlyHintsHandler;
+  protected Handler<HttpFrame> unknownFrameHandler;
+  protected Handler<Throwable> exceptionHandler;
+  protected Handler<HttpClientPush> pushHandler;
+  protected Handler<Void> closeHandler;
+  protected long writeWindow;
+  protected final long windowSize;
+
+  protected abstract long getWindowSize();
+  protected abstract HttpVersion version();
+  protected abstract void recycle();
+  protected abstract void metricsEnd(HttpStream<?, ?> stream);
+
+  HttpStream(C conn, ContextInternal context, boolean push) {
+    super(conn, context);
+
+    this.push = push;
+    this.windowSize = getWindowSize();
+  }
+
+  void onContinue() {
+    context.emit(null, v -> handleContinue());
+  }
+
+  void onEarlyHints(MultiMap headers) {
+    context.emit(null, v -> handleEarlyHints(headers));
+  }
+
+  abstract void handleContinue();
+
+  abstract void handleEarlyHints(MultiMap headers);
+
+  public Object metric() {
+    return metric;
+  }
+
+  public Object trace() {
+    return trace;
+  }
+
+  @Override
+  void doWriteData(ByteBuf chunk, boolean end, Handler<AsyncResult<Void>> handler) {
+    super.doWriteData(chunk, end, handler);
+  }
+
+  @Override
+  void doWriteHeaders(VertxHttpHeaders headers, boolean end, boolean checkFlush, Handler<AsyncResult<Void>> handler) {
+    isConnect = "CONNECT".contentEquals(headers.method());
+    super.doWriteHeaders(headers, end, checkFlush, handler);
+  }
+
+  @Override
+  protected void doWriteReset(long code) {
+    if (!requestEnded || !responseEnded) {
+      super.doWriteReset(code);
+    }
+  }
+
+  protected void endWritten() {
+    requestEnded = true;
+    if (conn.metrics() != null) {
+      metrics().requestEnd(metric, bytesWritten());
+    }
+  }
+
+  protected ClientMetrics metrics() {
+    return (ClientMetrics) conn.metrics();
+  }
+
+  @Override
+  void onEnd(MultiMap trailers) {
+    metricsEnd(this);
+    responseEnded = true;
+    super.onEnd(trailers);
+  }
+
+  @Override
+  void onReset(long code) {
+    if (metrics() != null) {
+      metrics().requestReset(metric);
+    }
+    super.onReset(code);
+  }
+
+  @Override
+  void onHeaders(VertxHttpHeaders headers, StreamPriorityBase streamPriority) {
+    if (streamPriority != null) {
+      priority(streamPriority);
+    }
+    if (response == null) {
+      int status;
+      String statusMessage;
+      try {
+        status = Integer.parseInt(String.valueOf(headers.status()));
+        statusMessage = HttpResponseStatus.valueOf(status).reasonPhrase();
+      } catch (Exception e) {
+        handleException(e);
+        writeReset(0x01 /* PROTOCOL_ERROR */);
+        return;
+      }
+      if (status == 100) {
+        onContinue();
+        return;
+      } else if (status == 103) {
+        MultiMap headersMultiMap = HeadersMultiMap.httpHeaders();
+        removeStatusHeaders(headers);
+        for (Map.Entry<CharSequence, CharSequence> header : headers.getIterable()) {
+          headersMultiMap.add(header.getKey(), header.getValue());
+        }
+        onEarlyHints(headersMultiMap);
+        return;
+      }
+      response = new HttpResponseHead(
+        version(),
+        status,
+        statusMessage,
+        headers.toHeaderAdapter());
+      removeStatusHeaders(headers);
+
+      if (metrics() != null) {
+        metrics().responseBegin(metric, response);
+      }
+
+      if (headHandler != null) {
+        context.emit(response, headHandler);
+      }
+    }
+  }
+
+  private void removeStatusHeaders(VertxHttpHeaders headers) {
+    headers.remove(HttpHeaders.PSEUDO_STATUS);
+  }
+
+  @Override
+  void onClose() {
+    if (metrics() != null) {
+      if (!requestEnded || !responseEnded) {
+        metrics().requestReset(metric);
+      }
+    }
+    VertxTracer tracer = context.tracer();
+    if (tracer != null && trace != null) {
+      VertxException err;
+      if (responseEnded && requestEnded) {
+        err = null;
+      } else {
+        err = HttpUtils.STREAM_CLOSED_EXCEPTION;
+      }
+      tracer.receiveResponse(context, response, trace, err, HttpUtils.CLIENT_RESPONSE_TAG_EXTRACTOR);
+    }
+    if (!responseEnded) {
+      // NOT SURE OF THAT
+      onException(HttpUtils.STREAM_CLOSED_EXCEPTION);
+    }
+    super.onClose();
+    // commented to be used later when we properly define the HTTP/2 connection expiration from the pool
+    // boolean disposable = conn.streams.isEmpty();
+    if (!push) {
+      recycle();
+    } /* else {
+        conn.listener.onRecycle(0, disposable);
+      } */
+    if (closeHandler != null) {
+      closeHandler.handle(null);
+    }
+  }
+}
---
+++ vertx-core/src/main/java/io/vertx/core/http/impl/HttpStreamImpl.java
@@ -0,0 +1,334 @@
+package io.vertx.core.http.impl;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.EventLoop;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.core.MultiMap;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpFrame;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.StreamPriorityBase;
+import io.vertx.core.http.StreamResetException;
+import io.vertx.core.http.impl.headers.VertxHttpHeaders;
+import io.vertx.core.internal.ContextInternal;
+import io.vertx.core.net.impl.ConnectionBase;
+import io.vertx.core.spi.tracing.SpanKind;
+import io.vertx.core.spi.tracing.VertxTracer;
+import io.vertx.core.streams.WriteStream;
+import io.vertx.core.tracing.TracingPolicy;
+
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+abstract class HttpStreamImpl<C extends ConnectionBase, S> extends HttpStream<C, S> implements HttpClientStream {
+
+  protected abstract boolean isTryUseCompression();
+
+  abstract int lastStreamCreated();
+
+  protected abstract void createStreamInternal(int id, boolean b, Handler<AsyncResult<S>> onComplete) throws HttpException;
+
+  protected abstract TracingPolicy getTracingPolicy();
+
+  abstract VertxHttpHeaders createHttpHeadersWrapper();
+
+  HttpStreamImpl(C conn, ContextInternal context, boolean push) {
+    super(conn, context, push);
+  }
+
+  @Override
+  public void closeHandler(Handler<Void> handler) {
+    closeHandler = handler;
+  }
+
+  @Override
+  public void continueHandler(Handler<Void> handler) {
+    continueHandler = handler;
+  }
+
+  @Override
+  public void earlyHintsHandler(Handler<MultiMap> handler) {
+    earlyHintsHandler = handler;
+  }
+
+  @Override
+  public void unknownFrameHandler(Handler<HttpFrame> handler) {
+    unknownFrameHandler = handler;
+  }
+
+  @Override
+  public void pushHandler(Handler<HttpClientPush> handler) {
+    pushHandler = handler;
+  }
+
+  @Override
+  public HttpStreamImpl<?, ?> drainHandler(Handler<Void> handler) {
+    drainHandler = handler;
+    return this;
+  }
+
+  @Override
+  public HttpStreamImpl<?, ?> exceptionHandler(Handler<Throwable> handler) {
+    exceptionHandler = handler;
+    return this;
+  }
+
+  @Override
+  public WriteStream<Buffer> setWriteQueueMaxSize(int maxSize) {
+    return this;
+  }
+
+  @Override
+  public boolean writeQueueFull() {
+    return !isNotWritable();
+  }
+
+  @Override
+  public synchronized boolean isNotWritable() {
+    return writeWindow > windowSize;
+  }
+
+  @Override
+  public void headHandler(Handler<HttpResponseHead> handler) {
+    headHandler = handler;
+  }
+
+  @Override
+  public void chunkHandler(Handler<Buffer> handler) {
+    chunkHandler = handler;
+  }
+
+  @Override
+  public void priorityHandler(Handler<StreamPriorityBase> handler) {
+    priorityHandler = handler;
+  }
+
+  @Override
+  public void endHandler(Handler<MultiMap> handler) {
+    endHandler = handler;
+  }
+
+  @Override
+  public StreamPriorityBase priority() {
+    return super.priority();
+  }
+
+  @Override
+  public void updatePriority(StreamPriorityBase streamPriority) {
+    super.updatePriority(streamPriority);
+  }
+
+  @Override
+  public HttpVersion version() {
+    return HttpVersion.HTTP_2;
+  }
+
+  @Override
+  void handleEnd(MultiMap trailers) {
+    if (endHandler != null) {
+      endHandler.handle(trailers);
+    }
+  }
+
+  @Override
+  void handleData(Buffer buf) {
+    if (chunkHandler != null) {
+      chunkHandler.handle(buf);
+    }
+  }
+
+  @Override
+  void handleReset(long errorCode) {
+    handleException(new StreamResetException(errorCode));
+  }
+
+  @Override
+  void handleWriteQueueDrained() {
+    Handler<Void> handler = drainHandler;
+    if (handler != null) {
+      context.dispatch(null, handler);
+    }
+  }
+
+  @Override
+  void handleWritabilityChanged(boolean writable) {
+  }
+
+  @Override
+  void handleCustomFrame(HttpFrame frame) {
+    if (unknownFrameHandler != null) {
+      unknownFrameHandler.handle(frame);
+    }
+  }
+
+
+  @Override
+  void handlePriorityChange(StreamPriorityBase streamPriority) {
+    if (priorityHandler != null) {
+      priorityHandler.handle(streamPriority);
+    }
+  }
+
+  void handleContinue() {
+    if (continueHandler != null) {
+      continueHandler.handle(null);
+    }
+  }
+
+  void handleEarlyHints(MultiMap headers) {
+    if (earlyHintsHandler != null) {
+      earlyHintsHandler.handle(headers);
+    }
+  }
+
+  void handleException(Throwable exception) {
+    if (exceptionHandler != null) {
+      exceptionHandler.handle(exception);
+    }
+  }
+
+  @Override
+  public void writeHead(HttpRequestHead request, boolean chunked, ByteBuf buf, boolean end, StreamPriorityBase priority,
+                        boolean connect, Handler<AsyncResult<Void>> handler) {
+    priority(priority);
+    ContextInternal ctx = conn.getContext();
+    EventLoop eventLoop = ctx.nettyEventLoop();
+    synchronized (this) {
+      if (shouldQueue(eventLoop)) {
+        queueForWrite(eventLoop, () -> writeHeaders(request, buf, end, priority, connect, handler));
+        return;
+      }
+    }
+    writeHeaders(request, buf, end, priority, connect, handler);
+  }
+
+  private void writeHeaders(HttpRequestHead request, ByteBuf buf, boolean end, StreamPriorityBase priority,
+                            boolean connect,
+                            Handler<AsyncResult<Void>> handler) {
+    VertxHttpHeaders headers = createHttpHeadersWrapper();
+    headers.method(request.method.name());
+    boolean e;
+    if (request.method == HttpMethod.CONNECT) {
+      if (request.authority == null) {
+        throw new IllegalArgumentException("Missing :authority / host header");
+      }
+      headers.authority(request.authority);
+      // don't end stream for CONNECT
+      e = false;
+    } else {
+      headers.path(request.uri);
+      headers.scheme(conn.isSsl() ? "https" : "http");
+      if (request.authority != null) {
+        headers.authority(request.authority);
+      }
+      e = end;
+    }
+    if (request.headers != null && request.headers.size() > 0) {
+      for (Map.Entry<String, String> header : request.headers) {
+        headers.add(HttpUtils.toLowerCase(header.getKey()), header.getValue());
+      }
+    }
+    if (isTryUseCompression() && headers.get(HttpHeaderNames.ACCEPT_ENCODING) == null) {
+      headers.set(HttpHeaderNames.ACCEPT_ENCODING, Http1xClientConnection.determineCompressionAcceptEncoding());
+    }
+    try {
+      createStream(request, headers, handler);
+    } catch (HttpException ex) {
+      if (handler != null) {
+        handler.handle(context.failedFuture(ex));
+      }
+      handleException(ex);
+      return;
+    }
+    if (buf != null) {
+      doWriteHeaders(headers, false, false, null);
+      doWriteData(buf, e, handler);
+    } else {
+      doWriteHeaders(headers, e, true, handler);
+    }
+  }
+
+  private void createStream(HttpRequestHead head, VertxHttpHeaders headers,
+                            Handler<AsyncResult<Void>> handler) throws HttpException {
+    int id = lastStreamCreated();
+    if (id == 0) {
+      id = 1;
+    } else {
+      id += 2;
+    }
+    head.id = id;
+    head.remoteAddress = conn.remoteAddress();
+    createStreamInternal(id, false, streamX -> {
+      init(streamX.result());
+      if (metrics() != null) {
+        metric = metrics().requestBegin(headers.path().toString(), head);
+      }
+      VertxTracer tracer = context.tracer();
+      if (tracer != null) {
+        BiConsumer<String, String> headers_ =
+          (key, val) -> headers.add(key, val);
+        String operation = head.traceOperation;
+        if (operation == null) {
+          operation = headers.method().toString();
+        }
+        trace = tracer.sendRequest(context, SpanKind.RPC, getTracingPolicy(), head, operation, headers_,
+          HttpUtils.CLIENT_HTTP_REQUEST_TAG_EXTRACTOR);
+      }
+    });
+  }
+
+  @Override
+  public void writeBuffer(ByteBuf buf, boolean end, Handler<AsyncResult<Void>> listener) {
+    if (buf != null) {
+      int size = buf.readableBytes();
+      synchronized (this) {
+        writeWindow += size;
+      }
+      if (listener != null) {
+        Handler<AsyncResult<Void>> prev = listener;
+        listener = ar -> {
+          Handler<Void> drainHandler;
+          synchronized (this) {
+            boolean full = writeWindow > windowSize;
+            writeWindow -= size;
+            if (full && writeWindow <= windowSize) {
+              drainHandler = this.drainHandler;
+            } else {
+              drainHandler = null;
+            }
+          }
+          if (drainHandler != null) {
+            drainHandler.handle(null);
+          }
+          prev.handle(ar);
+        };
+      }
+    }
+    writeData(buf, end, listener);
+  }
+
+  @Override
+  public ContextInternal getContext() {
+    return context;
+  }
+
+  @Override
+  public void doSetWriteQueueMaxSize(int size) {
+  }
+
+  @Override
+  public void reset(Throwable cause) {
+    long code;
+    if (cause instanceof StreamResetException) {
+      code = ((StreamResetException) cause).getCode();
+    } else if (cause instanceof java.util.concurrent.TimeoutException) {
+      code = 0x08L; // CANCEL
+    } else {
+      code = 0L;
+    }
+    conn.context.emit(code, this::writeReset);
+  }
+
+}
