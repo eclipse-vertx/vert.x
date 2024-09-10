9801071ef7908667708d96f1c26d5d06b58d631c
Julien Viet <julien@julienviet.com>
2024-09-10 19:56:54 +0200

    Remove most Netty deprecation usage, at least those that prevents integrating io_uring in vertx-core.

--- vertx-core/src/test/java/io/vertx/it/transport/CustomTransport.java
+++ vertx-core/src/test/java/io/vertx/it/transport/CustomTransport.java
@@ -1,15 +1,10 @@
 package io.vertx.it.transport;
 
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelFactory;
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.ServerChannel;
+import io.netty.channel.*;
 import io.netty.channel.socket.DatagramChannel;
 import io.netty.channel.socket.InternetProtocolFamily;
 import io.vertx.core.impl.transports.JDKTransport;
 import io.vertx.core.spi.transport.Transport;
-
-import java.util.concurrent.ThreadFactory;
 
 public class CustomTransport implements Transport {
 
@@ -24,8 +19,8 @@ public class CustomTransport implements Transport {
   }
 
   @Override
-  public EventLoopGroup eventLoopGroup(int type, int nThreads, ThreadFactory threadFactory, int ioRatio) {
-    return JDKTransport.INSTANCE.eventLoopGroup(type, nThreads, threadFactory, ioRatio);
+  public IoHandlerFactory ioHandlerFactory() {
+    return JDKTransport.INSTANCE.ioHandlerFactory();
   }
 
   @Override
