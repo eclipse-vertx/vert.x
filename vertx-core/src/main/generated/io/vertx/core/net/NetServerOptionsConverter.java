c1012c25a85c2ae1af858ff2b96913ef092db3bc
Julien Viet <julien@julienviet.com>
2024-10-07 23:42:27 +0200

    Regen converters

--- vertx-core/src/main/generated/io/vertx/core/net/NetServerOptionsConverter.java
+++ vertx-core/src/main/generated/io/vertx/core/net/NetServerOptionsConverter.java
@@ -4,16 +4,12 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.core.json.JsonArray;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
-import java.util.Base64;
 
 /**
  * Converter and mapper for {@link io.vertx.core.net.NetServerOptions}.
  * NOTE: This class has been automatically generated from the {@link io.vertx.core.net.NetServerOptions} original class using Vert.x codegen.
  */
 public class NetServerOptionsConverter {
-
-  private static final Base64.Decoder BASE64_DECODER = Base64.getUrlDecoder();
-  private static final Base64.Encoder BASE64_ENCODER = Base64.getUrlEncoder().withoutPadding();
 
    static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, NetServerOptions obj) {
     for (java.util.Map.Entry<String, Object> member : json) {
