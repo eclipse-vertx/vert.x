df9cc4a048125a238a38de08dd01c8d0a4a0cf8e
imz87 <zolfaghari19@gmail.com>
2024-10-28 12:28:01 +0330

    feat: resolve merge issue

--- vertx-core/src/main/generated/io/vertx/core/http/StreamPriorityBaseConverter.java
+++ vertx-core/src/main/generated/io/vertx/core/http/StreamPriorityBaseConverter.java
@@ -4,16 +4,12 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.core.json.JsonArray;
 import java.time.Instant;
 import java.time.format.DateTimeFormatter;
-import java.util.Base64;
 
 /**
  * Converter and mapper for {@link io.vertx.core.http.StreamPriorityBase}.
  * NOTE: This class has been automatically generated from the {@link io.vertx.core.http.StreamPriorityBase} original class using Vert.x codegen.
  */
 public class StreamPriorityBaseConverter {
-
-  private static final Base64.Decoder BASE64_DECODER = Base64.getUrlDecoder();
-  private static final Base64.Encoder BASE64_ENCODER = Base64.getUrlEncoder().withoutPadding();
 
    static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, StreamPriorityBase obj) {
     for (java.util.Map.Entry<String, Object> member : json) {
