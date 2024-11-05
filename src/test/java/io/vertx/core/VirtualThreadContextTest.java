a87afd222afa3505216af0741be34357588cb640
Julien Viet <julien@julienviet.com>
2024-11-05 09:20:22 +0100

    Future.await should interrupt the current thread when the worker executor is closed.
    Motivation:
    
    Future.await incorrectly performs a no-op when the worker executor is closed (returns a null latch), which reports a failure that might not exist.
    
    Changes:
    
    When the worker executor returns null, throw an interrupted exception.

--- src/test/java/io/vertx/core/VirtualThreadContextTest.java
+++ src/test/java/io/vertx/core/VirtualThreadContextTest.java
@@ -349,4 +349,31 @@ public class VirtualThreadContextTest extends VertxTestBase {
     }
     f.toCompletionStage().toCompletableFuture().get(20, TimeUnit.SECONDS);
   }
+
+  @Test
+  public void testAwaitWhenClosed() throws Exception {
+    Assume.assumeTrue(isVirtualThreadAvailable());
+    ContextInternal ctx = vertx.createVirtualThreadContext();
+    CountDownLatch latch = new CountDownLatch(1);
+    ctx.runOnContext(v -> {
+      latch.countDown();
+      try {
+        new CountDownLatch(1).await();
+        fail();
+      } catch (InterruptedException expected) {
+        assertFalse(Thread.currentThread().isInterrupted());
+      }
+      try {
+        Promise.promise().future().await();
+        fail();
+      } catch (Exception e) {
+        assertEquals(InterruptedException.class, e.getClass());
+        testComplete();
+      }
+    });
+    awaitLatch(latch);
+    // Interrupts virtual thread
+    ctx.close();
+    await();
+  }
 }
