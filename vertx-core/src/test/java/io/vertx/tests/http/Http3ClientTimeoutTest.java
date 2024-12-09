310d5f944f0233176479e7b174c6b927da90c896
imz87 <zolfaghari19@gmail.com>
2024-12-09 19:03:04 +0330

    feat: add HttpClientTimeoutTest

---
+++ vertx-core/src/test/java/io/vertx/tests/http/Http3ClientTimeoutTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2011-2023 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
+package io.vertx.tests.http;
+
+import io.vertx.core.http.Http3Settings;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.test.http.HttpTestBase;
+
+public class Http3ClientTimeoutTest extends HttpClientTimeoutTest {
+
+  @Override
+  protected HttpServerOptions createBaseServerOptions() {
+    return HttpOptionsFactory
+      .createHttp3ServerOptions(HttpTestBase.DEFAULT_HTTPS_PORT, HttpTestBase.DEFAULT_HTTPS_HOST)
+      .setInitialHttp3Settings(new Http3Settings().setMaxFieldSectionSize(50000));
+  }
+
+  @Override
+  protected HttpClientOptions createBaseClientOptions() {
+    return HttpOptionsFactory.createHttp3ClientOptions();
+  }
+}
