b565094c1e392049367dc5a7d7f360a27bc3f9bc
imz87 <zolfaghari19@gmail.com>
2024-12-09 19:13:42 +0330

    feat: add Http3ClientTest

---
+++ vertx-core/src/test/java/io/vertx/tests/http/Http3ClientTest.java
@@ -0,0 +1,42 @@
+package io.vertx.tests.http;
+
+import io.netty.channel.EventLoopGroup;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.HttpServerOptions;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class Http3ClientTest extends HttpClientTest {
+  @Override
+  public void setUp() throws Exception {
+    eventLoopGroups.clear();
+    serverOptions = HttpOptionsFactory.createHttp3ServerOptions(DEFAULT_HTTPS_PORT, DEFAULT_HTTPS_HOST);
+    clientOptions = HttpOptionsFactory.createHttp3ClientOptions();
+    super.setUp();
+  }
+
+  @Override
+  protected void configureDomainSockets() throws Exception {
+    // Nope
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    super.tearDown();
+    for (EventLoopGroup eventLoopGroup : eventLoopGroups) {
+      eventLoopGroup.shutdownGracefully(0, 10, TimeUnit.SECONDS);
+    }
+  }
+
+  @Override
+  protected HttpServerOptions createBaseServerOptions() {
+    return serverOptions;
+  }
+
+  @Override
+  protected HttpClientOptions createBaseClientOptions() {
+    return clientOptions;
+  }
+
+}
