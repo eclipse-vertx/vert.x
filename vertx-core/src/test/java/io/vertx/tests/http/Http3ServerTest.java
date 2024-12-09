5b96ae400b3cb22cedb7950cd5d58208871da661
imz87 <zolfaghari19@gmail.com>
2024-12-09 19:13:04 +0330

    feat: add Http3ServerTest

--- vertx-core/src/test/java/io/vertx/tests/http/Http3ServerTest.java
+++ vertx-core/src/test/java/io/vertx/tests/http/Http3ServerTest.java
@@ -1,14 +1,3 @@
-/*
- * Copyright (c) 2011-2019 Contributors to the Eclipse Foundation
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
- * which is available at https://www.apache.org/licenses/LICENSE-2.0.
- *
- * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
- */
-
 package io.vertx.tests.http;
 
 import io.netty.channel.EventLoopGroup;
@@ -17,13 +6,14 @@ import io.vertx.core.http.HttpServerOptions;
 
 import java.util.concurrent.TimeUnit;
 
-public class Http2ServerTest extends HttpServerTest {
+
+public class Http3ServerTest extends HttpServerTest {
 
   @Override
   public void setUp() throws Exception {
     eventLoopGroups.clear();
-    serverOptions = HttpOptionsFactory.createHttp2ServerOptions(DEFAULT_HTTPS_PORT, DEFAULT_HTTPS_HOST);
-    clientOptions = HttpOptionsFactory.createHttp2ClientOptions();
+    serverOptions = HttpOptionsFactory.createHttp3ServerOptions(DEFAULT_HTTPS_PORT, DEFAULT_HTTPS_HOST);
+    clientOptions = HttpOptionsFactory.createHttp3ClientOptions();
     super.setUp();
   }
 
