7d16f4217943d05e22abd17c9c042772be6852ed
Julien Viet <julien@julienviet.com>
2024-12-18 08:43:33 +0100

    Move Context methods getLocal/removeLocal/putLocal to ContextInternal and deprecate them for removal.
    Motivation:
    
    Context local methods should not have been part of the contract of Context initially. This API has been replaced by the ContextLocal API which should be used instead. This API is not going away in 5.0 but we want to get the opportunity to remove it at any time.

--- vertx-core/src/main/java/io/vertx/core/Context.java
+++ vertx-core/src/main/java/io/vertx/core/Context.java
@@ -218,33 +218,6 @@ public interface Context {
   boolean remove(Object key);
 
   /**
-   * Get some local data from the context.
-   *
-   * @param key  the key of the data
-   * @param <T>  the type of the data
-   * @return the data
-   */
-  <T> T getLocal(Object key);
-
-  /**
-   * Put some local data in the context.
-   * <p>
-   * This can be used to share data between different handlers that share a context
-   *
-   * @param key  the key of the data
-   * @param value  the data
-   */
-  void putLocal(Object key, Object value);
-
-  /**
-   * Remove some local data from the context.
-   *
-   * @param key  the key to remove
-   * @return true if removed successfully, false otherwise
-   */
-  boolean removeLocal(Object key);
-
-  /**
    * @return The Vertx instance that created the context
    */
   Vertx owner();
