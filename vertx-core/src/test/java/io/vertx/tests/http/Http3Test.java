73dbe0f26d488b14d8eb260ab1a030a6597e9e5f
imz87 <zolfaghari19@gmail.com>
2024-12-19 12:51:19 +0330

    feat: prepare for more tests

--- vertx-core/src/test/java/io/vertx/tests/http/Http3Test.java
+++ vertx-core/src/test/java/io/vertx/tests/http/Http3Test.java
@@ -76,17 +76,22 @@ public class Http3Test extends HttpCommonTest {
   @Ignore
   public void testClientDrainHandler() throws Exception {
     //TODO: resolve this test issue.
+    super.testClientDrainHandler();
   }
 
   @Test
   @Ignore
   public void testServerDrainHandler() throws Exception {
     //TODO: resolve this test issue.
+    super.testServerDrainHandler();
   }
 
   @Ignore("This test is ignored because UDP is based on a single connectionless protocol.")
   @Test
-  public void testCloseMulti() {}
+  public void testCloseMulti() throws Exception {
+    //TODO: resolve this test issue.
+    super.testCloseMulti();
+  }
 
   @Ignore
   @Test
@@ -140,202 +145,242 @@ public class Http3Test extends HttpCommonTest {
     await();
   }
 
+  @Test
+  @Ignore
+  @Override
+  public void testEventHandlersNotHoldingLockOnClose() throws Exception {
+    //TODO: resolve this test issue.
+    super.testEventHandlersNotHoldingLockOnClose();
+  }
 
   @Test
   @Ignore
   public void testRstFloodProtection() throws Exception {
     //TODO: resolve this test issue.
+    super.testRstFloodProtection();
   }
 
   @Test
   @Ignore
   public void testUnsupportedAlpnVersion() throws Exception {
     //TODO: resolve this test issue.
+    super.testUnsupportedAlpnVersion();
   }
 
   @Ignore
   @Test
   public void testConnectionCloseEvictsConnectionFromThePoolBeforeStreamsAreClosed() throws Exception {
     //TODO: resolve this test issue.
+    super.testConnectionCloseEvictsConnectionFromThePoolBeforeStreamsAreClosed();
   }
 
   @Test
   @Ignore
   public void testDeliverPausedBufferWhenResume() throws Exception {
     //TODO: resolve this test issue.
+    super.testDeliverPausedBufferWhenResume();
   }
 
   @Test
   @Ignore
   public void testDeliverPausedBufferWhenResumeOnOtherThread() throws Exception {
     //TODO: resolve this test issue.
+    super.testDeliverPausedBufferWhenResumeOnOtherThread();
   }
 
   @Test
   @Ignore
   public void testPausedHttpServerRequest() throws Exception {
     //TODO: resolve this test issue.
+    super.testPausedHttpServerRequest();
   }
 
   @Test
   @Ignore
   public void testClientReadStreamInWorker() throws Exception {
     //TODO: resolve this test issue.
+    super.testClientReadStreamInWorker();
   }
 
   @Test
   @Ignore
   public void testDumpManyRequestsOnQueue() throws Exception {
     //TODO: resolve this test issue.
+    super.testDumpManyRequestsOnQueue();
   }
 
   @Test
   @Ignore
   public void testServerLogging() throws Exception {
     //TODO: resolve this test issue.
+    super.testServerLogging();
   }
 
   @Test
   @Ignore
   public void testClientLogging() throws Exception {
     //TODO: resolve this test issue.
+    super.testClientLogging();
   }
 
   @Test
   @Ignore
   public void testClientDecompressionError() throws Exception {
     //TODO: resolve this test issue.
+    super.testClientDecompressionError();
   }
 
   @Test
   @Ignore
   public void testDisableIdleTimeoutInPool() throws Exception {
     //TODO: resolve this test issue.
+    super.testDisableIdleTimeoutInPool();
   }
 
   @Test
   @Ignore
   public void testNetSocketConnectSuccessClientInitiatesCloseImmediately() throws Exception {
     //TODO: resolve this test issue.
+    super.testNetSocketConnectSuccessClientInitiatesCloseImmediately();
   }
 
   @Test
   @Ignore
   public void testNetSocketConnectSuccessServerInitiatesCloseOnReply() throws Exception {
     //TODO: resolve this test issue.
+    super.testNetSocketConnectSuccessServerInitiatesCloseOnReply();
   }
 
   @Test
   @Ignore
   public void testResetClientRequestResponseInProgress() throws Exception {
     //TODO: resolve this test issue.
+    super.testResetClientRequestResponseInProgress();
   }
 
   @Test
   @Ignore
   public void testClientRequestWithLargeBodyInSmallChunksChunked() throws Exception {
     //TODO: resolve this test issue.
+    super.testClientRequestWithLargeBodyInSmallChunksChunked();
   }
 
   @Test
   @Ignore
   public void testClientRequestWithLargeBodyInSmallChunksChunkedWithHandler() throws Exception {
     //TODO: resolve this test issue.
+    super.testClientRequestWithLargeBodyInSmallChunksChunkedWithHandler();
   }
 
   @Test
   @Ignore
   public void testHAProxyProtocolIdleTimeout() throws Exception {
     //TODO: Resolve this test issue. It fails on 5.x version, regardless of HTTP/3.
+    super.testHAProxyProtocolIdleTimeout();
   }
 
   @Test
   @Ignore
   public void testHAProxyProtocolIdleTimeoutNotHappened() throws Exception {
     //TODO: Resolve this test issue. It fails on 5.x version, regardless of HTTP/3.
+    super.testHAProxyProtocolIdleTimeoutNotHappened();
   }
 
   @Test
   @Ignore
   public void testHAProxyProtocolVersion1TCP4() throws Exception {
     //TODO: Resolve this test issue. It fails on 5.x version, regardless of HTTP/3.
+    super.testHAProxyProtocolVersion1TCP4();
   }
 
   @Test
   @Ignore
   public void testHAProxyProtocolVersion1TCP6() throws Exception {
     //TODO: Resolve this test issue. It fails on 5.x version, regardless of HTTP/3.
+    super.testHAProxyProtocolVersion1TCP6();
   }
 
   @Test
   @Ignore
   public void testHAProxyProtocolVersion1Unknown() throws Exception {
     //TODO: Resolve this test issue. It fails on 5.x version, regardless of HTTP/3.
+    super.testHAProxyProtocolVersion1Unknown();
   }
 
   @Test
   @Ignore
   public void testHAProxyProtocolVersion2TCP4() throws Exception {
     //TODO: Resolve this test issue. It fails on 5.x version, regardless of HTTP/3.
+    super.testHAProxyProtocolVersion2TCP4();
   }
 
   @Test
   @Ignore
   public void testHAProxyProtocolVersion2TCP6() throws Exception {
     //TODO: Resolve this test issue. It fails on 5.x version, regardless of HTTP/3.
+    super.testHAProxyProtocolVersion2TCP6();
   }
 
   @Test
   @Ignore
   public void testHAProxyProtocolVersion2UnixSocket() throws Exception {
     //TODO: Resolve this test issue. It fails on 5.x version, regardless of HTTP/3.
+    super.testHAProxyProtocolVersion2UnixSocket();
   }
 
   @Test
   @Ignore
   public void testHAProxyProtocolVersion2Unknown() throws Exception {
     //TODO: Resolve this test issue. It fails on 5.x version, regardless of HTTP/3.
+    super.testHAProxyProtocolVersion2Unknown();
   }
 
   @Test
   @Ignore
   public void testHAProxyProtocolVersion2UDP4() throws Exception {
     //TODO: Resolve this test issue. It fails on 5.x version, regardless of HTTP/3.
+    super.testHAProxyProtocolVersion2UDP4();
   }
 
   @Test
   @Ignore
   public void testHAProxyProtocolVersion2UDP6() throws Exception {
     //TODO: Resolve this test issue. It fails on 5.x version, regardless of HTTP/3.
+    super.testHAProxyProtocolVersion2UDP6();
   }
 
   @Test
   @Ignore
   public void testHAProxyProtocolVersion2UnixDataGram() throws Exception {
     //TODO: Resolve this test issue. It fails on 5.x version, regardless of HTTP/3.
+    super.testHAProxyProtocolVersion2UnixDataGram();
   }
 
   @Test
   @Ignore
   public void testHAProxyProtocolEmptyHeader() throws Exception {
     //TODO: Resolve this test issue. It fails on 5.x version, regardless of HTTP/3.
+    super.testHAProxyProtocolEmptyHeader();
   }
 
   @Test
   @Ignore
   public void testHAProxyProtocolIllegalHeader() throws Exception {
     //TODO: Resolve this test issue. It fails on 5.x version, regardless of HTTP/3.
+    super.testHAProxyProtocolIllegalHeader();
   }
 
   @Test
   @Ignore
   public void testDnsClientSideLoadBalancingDisabled() throws Exception {
     //TODO: Resolve this test issue. It fails on 5.x version, regardless of HTTP/3.
+    super.testDnsClientSideLoadBalancingDisabled();
   }
 
   @Test
   @Ignore
   public void testDnsClientSideLoadBalancingEnabled() throws Exception {
     //TODO: Resolve this test issue. It fails on 5.x version, regardless of HTTP/3.
+    super.testDnsClientSideLoadBalancingEnabled();
   }
 }
