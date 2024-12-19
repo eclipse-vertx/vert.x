e11ee0c08ecc0c935c48138147c628a3c6d1f33d
imz87 <zolfaghari19@gmail.com>
2024-12-19 18:00:55 +0330

    feat: add server file upload test

---
+++ vertx-core/src/test/java/io/vertx/tests/http/fileupload/Http3ServerFileUploadTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2011-2019 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
+package io.vertx.tests.http.fileupload;
+
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.HttpServerOptions;
+import io.vertx.tests.http.HttpOptionsFactory;
+
+/**
+ * @author <a href="mailto:zolfaghari19@gmail.com">Iman Zolfaghari</a>
+ */
+public class Http3ServerFileUploadTest extends HttpServerFileUploadTest {
+
+  @Override
+  protected HttpServerOptions createBaseServerOptions() {
+    return HttpOptionsFactory.createHttp3ServerOptions(DEFAULT_HTTP_PORT, DEFAULT_HTTP_HOST);
+  }
+
+  @Override
+  protected HttpClientOptions createBaseClientOptions() {
+    return HttpOptionsFactory.createHttp3ClientOptions();
+  }
+
+}
