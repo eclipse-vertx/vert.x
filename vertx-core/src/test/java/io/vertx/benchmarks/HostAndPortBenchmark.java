ff2c06ebaba2acf000bfa20b1c24f925abfeaaa3
Thomas Segismont <tsegismont@gmail.com>
2025-01-06 11:24:09 +0100

    Move HostAndPortBenchmark to the benchmarks package
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- vertx-core/src/test/java/io/vertx/benchmarks/HostAndPortBenchmark.java
+++ vertx-core/src/test/java/io/vertx/benchmarks/HostAndPortBenchmark.java
@@ -9,21 +9,12 @@
  * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
  */
 
-package io.vertx.core.net.impl;
+package io.vertx.benchmarks;
+
+import io.vertx.core.net.impl.HostAndPortImpl;
+import org.openjdk.jmh.annotations.*;
 
 import java.util.concurrent.TimeUnit;
-
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Param;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Warmup;
 
 @State(Scope.Thread)
 @BenchmarkMode(Mode.AverageTime)
