9bebdae1c7b7614db5654a5f1b8e086d345d5db0
Julien Viet <julien@julienviet.com>
2025-01-29 17:48:09 +0100

    Fix racy test

--- vertx-core/src/test/java/io/vertx/tests/worker/NamedWorkerPoolTest.java
+++ vertx-core/src/test/java/io/vertx/tests/worker/NamedWorkerPoolTest.java
@@ -13,6 +13,7 @@ package io.vertx.tests.worker;
 
 import io.vertx.core.*;
 import io.vertx.core.impl.VertxThread;
+import io.vertx.test.core.Repeat;
 import io.vertx.test.core.TestUtils;
 import io.vertx.test.core.VertxTestBase;
 import org.junit.Test;
@@ -127,6 +128,7 @@ public class NamedWorkerPoolTest extends VertxTestBase {
     await();
   }
 
+  @Repeat(times = 1000)
   @Test
   public void testUseDifferentExecutorWithSameTaskQueue() {
     int count = 10;
