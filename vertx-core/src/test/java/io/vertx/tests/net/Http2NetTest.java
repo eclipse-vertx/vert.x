584c7b45545ff2bd0a0e16d507882e0f924e153f
imz87 <zolfaghari19@gmail.com>
2025-02-10 10:44:25 +0330

    feat: add author

--- vertx-core/src/test/java/io/vertx/tests/net/Http2NetTest.java
+++ vertx-core/src/test/java/io/vertx/tests/net/Http2NetTest.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2011-2019 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
 package io.vertx.tests.net;
 
 import io.vertx.core.http.HttpClientOptions;
@@ -8,6 +18,9 @@ import io.vertx.tests.http.HttpOptionsFactory;
 
 import static io.vertx.test.http.HttpTestBase.*;
 
+/**
+ * @author <a href="mailto:zolfaghari19@gmail.com">Iman Zolfaghari</a>
+ */
 public class Http2NetTest extends NetTest {
 
   protected NetServerOptions createNetServerOptions() {
