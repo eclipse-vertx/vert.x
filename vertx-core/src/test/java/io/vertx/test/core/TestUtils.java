85f23e4ed3522c232d73b9adb1731f058c630edb
Julien Viet <julien@julienviet.com>
2025-03-05 13:40:47 +0100

    Minor cleanup

--- vertx-core/src/test/java/io/vertx/test/core/TestUtils.java
+++ vertx-core/src/test/java/io/vertx/test/core/TestUtils.java
@@ -31,8 +31,6 @@ import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 import java.util.zip.GZIPOutputStream;
 
-import javax.security.cert.X509Certificate;
-
 import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.http2.Http2CodecUtil;
 import io.netty.util.NetUtil;
@@ -485,12 +483,6 @@ public class TestUtils {
       return names.isEmpty() ? null : names.get(0);
     }
     return null;
-  }
-
-  public static String cnOf(X509Certificate cert) throws Exception {
-    String dn = cert.getSubjectDN().getName();
-    List<String> names = KeyStoreHelper.getX509CertificateCommonNames(dn);
-    return names.isEmpty() ? null : names.get(0);
   }
 
   /**
