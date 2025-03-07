68894bd6d2bdb61694453adffe3beb1ba467611c
Julien Viet <julien@julienviet.com>
2025-03-07 13:14:35 +0100

    Mitigate the usage of NoStackTraceThrowable.
    Motivation:
    
    NoStackTraceThrowable currently extends Throwable and has been moved to the implementation package not exported in module-info.
    
    There are several concerns we need to solve:
    
    1. Virtual threads and Kotlin coroutines need to catch a throwable which is not a recommended practice, e.g. an Exception catch declaration let the NoStackTraceThrowable go through.
    2. Users relying on NoStackTraceThrowable developing a modular application have to catch Throwable instead since the type is not exported.
    
    We should provide a transitory path to remove NoStackTraceThrowable in Vert.x 6.
    
    Changes:
    
    NoStackTraceThrowable extends VertxException instead of Throwable.
    
    Users catching NoStackTraceThrowable (which is deprecated) can instead catch VertxException and achieve the same effect.

--- vertx-core/src/test/java/io/vertx/tests/virtualthread/VirtualThreadContextTest.java
+++ vertx-core/src/test/java/io/vertx/tests/virtualthread/VirtualThreadContextTest.java
@@ -10,7 +10,6 @@
  */
 package io.vertx.tests.virtualthread;
 
-import io.vertx.codegen.annotations.Nullable;
 import io.vertx.core.Context;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
