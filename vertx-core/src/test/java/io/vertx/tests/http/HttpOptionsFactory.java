3e8e9e15a866a4e575d283e46772d0e361974d7d
imz87 <zolfaghari19@gmail.com>
2025-03-14 12:22:41 +0330

    refactor: add import

--- vertx-core/src/test/java/io/vertx/tests/http/HttpOptionsFactory.java
+++ vertx-core/src/test/java/io/vertx/tests/http/HttpOptionsFactory.java
@@ -11,6 +11,7 @@
 
 package io.vertx.tests.http;
 
+import io.netty.incubator.codec.http3.Http3;
 import io.vertx.core.http.HttpClientOptions;
 import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.http.HttpVersion;
