6abbacd0d23718565d3ca48cf8e7c3f2be0620ac
imz87 <zolfaghari19@gmail.com>
2025-04-08 16:08:39 +0330

    refactor: add license

--- vertx-core/src/test/java/io/vertx/test/core/ReflectionUtils.java
+++ vertx-core/src/test/java/io/vertx/test/core/ReflectionUtils.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2011-2019 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
+
 package io.vertx.test.core;
 
 import java.lang.reflect.Field;
@@ -8,6 +19,9 @@ import java.lang.reflect.Modifier;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * @author <a href="mailto:zolfaghari19@gmail.com">Iman Zolfaghari</a>
+ */
 public class ReflectionUtils {
   public static List<Field> getAccessibleFields(Object instance) {
     List<Field> fields = new LinkedList<>();
