cef962fd5eca4054f657604514a8d2142881ccdd
Julien Viet <julien@julienviet.com>
2025-05-13 23:16:47 +0200

    Rewrite the node selector implementation.
    Motivation:
    
    The node selector implementation can be simplified to use an drain/loop pattern approach which simplifies its implementation. The current implementation already has a queue approach with a linked list of waiters but it mandates to perform CAS like operations in the entries map.
    
    Instead we can use a wip/mpsc queue node entry which is equivalent but simpler to understand.

--- vertx-core/src/main/java/io/vertx/core/eventbus/impl/clustered/DefaultNodeSelector.java
+++ vertx-core/src/main/java/io/vertx/core/eventbus/impl/clustered/DefaultNodeSelector.java
@@ -10,8 +10,8 @@
  */
 package io.vertx.core.eventbus.impl.clustered;
 
+import io.netty.util.internal.PlatformDependent;
 import io.vertx.core.Completable;
-import io.vertx.core.Promise;
 import io.vertx.core.eventbus.impl.clustered.selector.*;
 import io.vertx.core.spi.cluster.ClusteredNode;
 import io.vertx.core.spi.cluster.RegistrationInfo;
@@ -20,6 +20,7 @@ import io.vertx.core.spi.cluster.RegistrationUpdateEvent;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Node selector implementation that preserves the ordering of select operations.
@@ -27,7 +28,131 @@ import java.util.concurrent.ConcurrentMap;
 public class DefaultNodeSelector implements NodeSelector {
 
   private ClusteredNode clusterManager;
-  private final ConcurrentMap<String, Entry> entries = new ConcurrentHashMap<>();
+  private final ConcurrentMap<String, Node> entries = new ConcurrentHashMap<>();
+
+  private interface Op<T> {
+    Op<String> SEND = RoundRobinSelector::selectForSend;
+    Op<Iterable<String>> PUBLISH = RoundRobinSelector::selectForPublish;
+    T select(RoundRobinSelector selector);
+  }
+
+  private static class Node {
+
+    final AtomicInteger wip = new AtomicInteger(1);
+    final Queue<Action> queue = PlatformDependent.newMpscQueue();
+    Object value;
+
+    private void signal(Object value, int amount) {
+      while (amount > 0) {
+        for (int i = 0;i < amount;i++) {
+          Action a = queue.poll();
+          assert a != null;
+          if (a instanceof Select) {
+            Select<?> s = (Select<?>) a;
+            if (value instanceof RoundRobinSelector) {
+              s.resolve((RoundRobinSelector) value);
+            } else {
+              s.fail((Throwable) value);
+            }
+          } else if (a instanceof Update) {
+            value = ((Update)a).selector;
+          } else {
+            throw new UnsupportedOperationException();
+          }
+        }
+        // We write the value before writing wip to ensure visibility since wip is read
+        this.value = value;
+        amount = wip.addAndGet(-amount);
+      }
+    }
+  }
+
+  private interface Action {
+  }
+
+  private static class Update implements Action {
+    // null means delete
+    final RoundRobinSelector selector;
+    Update(RoundRobinSelector selector) {
+      this.selector = selector;
+    }
+  }
+
+  private static class Select<T> implements Action {
+    final Op<T> op;
+    final Completable<T> callback;
+    Select(Op<T> op, Completable<T> callback) {
+      this.op = op;
+      this.callback = callback;
+    }
+    void resolve(RoundRobinSelector selector) {
+      callback.succeed(op.select(selector));
+    }
+    void fail(Throwable err) {
+      callback.fail(err);
+    }
+  }
+
+  private <T> void selectFor(String address, Op<T> op, Completable<T> callback) {
+    Node node = entries.get(address);
+    if (node == null) {
+      node = new Node();
+      node.queue.add(new Select<>(op, callback));
+      Node phantom = entries.putIfAbsent(address, node);
+      if (phantom != null) {
+        node = phantom;
+      } else {
+        // Obtained ownership
+        initializeNode(node, address);
+        return;
+      }
+    }
+    if (node.wip.get() == 0) {
+      // wip == 0 implies we can safely read a value
+      Object v = node.value;
+      if (v instanceof RoundRobinSelector) {
+        callback.succeed(op.select((RoundRobinSelector) v));
+      } else {
+        callback.fail((Throwable) v);
+      }
+    } else {
+      node.queue.add(new Select<>(op, callback));
+      int amount = node.wip.incrementAndGet();
+      if (amount == 1) {
+        // Race case obtained incidentally ownership
+        // it happens when a concurrent signal operation finished after we enqueued our select
+        // we can read the node value (since amount was 0) and signal
+        node.signal(node.value, amount);
+      }
+    }
+  }
+
+  private void initializeNode(Node node, String address) {
+    clusterManager.getRegistrations(address, (res, err) -> {
+      if (err == null) {
+        succeed(node, address, res);
+      } else {
+        fail(node, address, err);
+      }
+    });
+  }
+
+  private void succeed(Node node, String address, List<RegistrationInfo> registrations) {
+    List<String> accessible = computeAccessible(registrations);
+    RoundRobinSelector selector = data(accessible);
+    if (selector != null) {
+      node.signal(selector, node.wip.get());
+    } else {
+      if (entries.remove(address, node)) {
+        node.signal(NullRoundRobinSelector.INSTANCE, node.wip.get());
+      }
+    }
+  }
+
+  private void fail(Node node, String address, Throwable cause) {
+    entries.remove(address, node);
+    node.signal(cause, node.wip.get());
+  }
 
   @Override
   public void init(ClusteredNode clusterManager) {
@@ -49,114 +174,19 @@ public class DefaultNodeSelector implements NodeSelector {
     return entries.containsKey(address);
   }
 
-  private interface Op<T> {
-    Op<String> SEND = RoundRobinSelector::selectForSend;
-    Op<Iterable<String>> PUBLISH = RoundRobinSelector::selectForPublish;
-    Op<Object> NOOP = selector -> null;
-    T selectWith(RoundRobinSelector selector);
-  }
-
-  private <T> void selectFor(String address, Op<T> op, Completable<T> promise) {
-    while (true) {
-      Entry entry = entries.get(address);
-      if (entry == null) {
-        WaiterEntry<T> head = new WaiterEntry<>(promise, op);
-        Entry phantom = entries.putIfAbsent(address, head);
-        if (phantom == null) {
-          initialize(head, address, op);
-          break;
-        }
-      } else if (entry instanceof WaiterEntry) {
-        WaiterEntry<T> next = new WaiterEntry<>(promise, op, (WaiterEntry<?>) entry);
-        if (entries.replace(address, entry, next)) {
-          break;
-        }
-      } else if (entry instanceof SelectorEntry) {
-        SelectorEntry re = (SelectorEntry) entry;
-        promise.succeed(op.selectWith(re.selector));
-        break;
-      }
-    }
-  }
-
-  private <T> void initialize(WaiterEntry<?> head, String address, Op<T> k) {
-    Promise<List<RegistrationInfo>> getPromise = Promise.promise();
-    clusterManager.getRegistrations(address, getPromise);
-    getPromise.future().onComplete(ar -> {
-      if (ar.succeeded()) {
-        succeed(head, address, ar.result(), k);
-      } else {
-        fail(head, address, ar.cause());
-      }
-    });
-  }
-
-  private void fail(WaiterEntry<?> head, String address, Throwable cause) {
-    // Check the entry is valid for the assumed head
-    Entry entry = entries.get(address);
-    if (entry instanceof WaiterEntry<?>) {
-      WaiterEntry<?> tail = (WaiterEntry<?>) entry;
-      if (tail.head == head) {
-        // Try remove
-        if (entries.remove(address, tail)) {
-          // Broadcast failure
-          while (tail != null) {
-            tail.waiter.fail(cause);
-            tail = tail.prev;
-          }
-        }
-      }
-    }
-  }
-
-  private <T> void succeed(WaiterEntry<?> head, String address, List<RegistrationInfo> registrations, Op<T> k) {
-    List<String> accessible = computeAccessible(registrations);
-    RoundRobinSelector selector = data(accessible);
-    while (true) {
-      Entry entry = entries.get(address);
-      if (entry == null) {
-        break;
-      } else if (entry instanceof WaiterEntry) {
-        WaiterEntry<?> tail = (WaiterEntry<?>) entry;
-        if (tail.head == head) {
-          if (selector != null) {
-            if (entries.replace(address, tail, WaiterEntry.NOOP)) {
-              broadcastToWaiters(tail, selector);
-              if (entries.replace(address, WaiterEntry.NOOP, new SelectorEntry(selector))) {
-                break;
-              } else {
-                // Another waiter has been added during broadcast, spin again
-                head = WaiterEntry.NOOP;
-              }
-            }
-          } else {
-            // No handlers
-            if (entries.remove(address, tail)) {
-              broadcastToWaiters((WaiterEntry<?>) entry, NullRoundRobinSelector.INSTANCE);
-              break;
-            }
-          }
-        } else {
-          break;
-        }
-      } else {
-        throw new UnsupportedOperationException("Does this case make sense " + entry);
-      }
-    }
-  }
-
   private RoundRobinSelector data(List<String> nodeIds) {
     if (nodeIds == null || nodeIds.isEmpty()) {
       return null;
-    }
-    Map<String, Weight> weights = computeWeights(nodeIds);
-    RoundRobinSelector selector;
-    if (isEvenlyDistributed(weights)) {
-      selector = new SimpleRoundRobinSelector(new ArrayList<>(weights.keySet()));
     } else {
-      selector = new WeightedRoundRobinSelector(weights);
+      Map<String, Weight> weights = computeWeights(nodeIds);
+      RoundRobinSelector selector;
+      if (isEvenlyDistributed(weights)) {
+        selector = new SimpleRoundRobinSelector(new ArrayList<>(weights.keySet()));
+      } else {
+        selector = new WeightedRoundRobinSelector(weights);
+      }
+      return selector;
     }
-    return selector;
   }
 
   private Map<String, Weight> computeWeights(List<String> nodeIds) {
@@ -206,78 +236,21 @@ public class DefaultNodeSelector implements NodeSelector {
   @Override
   public void registrationsUpdated(RegistrationUpdateEvent event) {
     String address = event.address();
-    while (true) {
-      Entry entry = entries.get(address);
-      if (entry == null) {
-        break;
-      } else if (entry instanceof WaiterEntry) {
-        throw new UnsupportedOperationException("Is this case valid ?");
-      } else {
-        SelectorEntry re = (SelectorEntry) entry;
-        List<String> accessible = computeAccessible(event.registrations());
-        RoundRobinSelector selector = data(accessible);
-        if (selector == null) {
-          // Un-registration
-          if (entries.remove(address, re)) {
-            break;
-          }
-        } else {
-          if (entries.replace(address, re, new SelectorEntry(selector))) {
-            break;
-          }
+    List<String> accessible = computeAccessible(event.registrations());
+    RoundRobinSelector selector = data(accessible);
+    if (selector != null) {
+      Node node = entries.get(address);
+      if (node != null) {
+        node.queue.add(new Update(selector));
+        int amount = node.wip.incrementAndGet();
+        if (amount == 1) {
+          node.signal(selector, amount);
         }
+      } else {
+        // ????
       }
-    }
-  }
-
-  private static abstract class Entry {
-  }
-
-  /**
-   * An entry waiting for an address to be resolved.
-   */
-  private static class WaiterEntry<T> extends Entry {
-
-    static final WaiterEntry<?> NOOP = new WaiterEntry<>((a, b) -> {}, Op.NOOP);
-
-    private final Completable<T> waiter;
-    private final WaiterEntry<?> prev;
-    private final WaiterEntry<?> head;
-    private final Op<T> op;
-    private WaiterEntry(Completable<T> waiter, Op<T> op) {
-      this.waiter = waiter;
-      this.prev = null;
-      this.op = op;
-      this.head = this;
-    }
-    private WaiterEntry(Completable<T> waiter, Op<T> op, WaiterEntry<?> prev) {
-      this.waiter = waiter;
-      this.prev = prev;
-      this.op = op;
-      this.head = prev.head;
-    }
-    void complete(RoundRobinSelector selector) {
-      waiter.succeed(op.selectWith(selector));
-    }
-  }
-
-  /**
-   * Terminal entry, no waiter entry should be added after a resolved entry is added to the list
-   */
-  private static class SelectorEntry extends Entry {
-    private final RoundRobinSelector selector;
-    private SelectorEntry(RoundRobinSelector selector) {
-      this.selector = selector;
-    }
-  }
-
-  private static void broadcastToWaiters(WaiterEntry<?> lastWaiter, RoundRobinSelector selector) {
-    List<WaiterEntry<?>> waiters = new ArrayList<>();
-    for (WaiterEntry<?> e = lastWaiter;e != null;e = e.prev) {
-      waiters.add(e);
-    }
-    for (int idx = waiters.size() - 1;idx >= 0;idx--) {
-      waiters.get(idx).complete(selector);
+    } else {
+      entries.remove(address);
     }
   }
 }
