bb1537a00f42dd53b57b92633808cea89609879e
Julien Viet <julien@julienviet.com>
2025-07-28 16:35:42 +0200

    Allow an HTTP/2 server to be protected against MadeYouReset attacks
    Motivation:
    
    HTTP/2 servers can be vulnerable to MadeYouReset attacks, to protect against this attach Netty can detect a RST flood attack and close the connection in response.
    
    Changes:
    
    Configure the HTTP/2 server connection builder encoder RST flood protection with the same setting than the decoder RST flood protection.
    
    In addition ignore headers frame when the server has received or sent a go away to avoid creating un-necessary request handler calls.

--- vertx-core/src/main/java/io/vertx/core/http/impl/VertxHttp2ConnectionHandlerBuilder.java
+++ vertx-core/src/main/java/io/vertx/core/http/impl/VertxHttp2ConnectionHandlerBuilder.java
@@ -48,6 +48,11 @@ class VertxHttp2ConnectionHandlerBuilder<C extends Http2ConnectionBase> extends 
   }
 
   @Override
+  protected VertxHttp2ConnectionHandlerBuilder<C> encoderEnforceMaxRstFramesPerWindow(int maxRstFramesPerWindow, int secondsPerWindow) {
+    return super.encoderEnforceMaxRstFramesPerWindow(maxRstFramesPerWindow, secondsPerWindow);
+  }
+
+  @Override
   protected VertxHttp2ConnectionHandlerBuilder<C> decoderEnforceMaxRstFramesPerWindow(int maxRstFramesPerWindow, int secondsPerWindow) {
     return super.decoderEnforceMaxRstFramesPerWindow(maxRstFramesPerWindow, secondsPerWindow);
   }
