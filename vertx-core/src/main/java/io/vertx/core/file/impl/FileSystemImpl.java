4ec8e779d1ca5721ad5b4066956db958e872c17b
doxlik <pirozhokkokosovich@gmail.com>
2025-08-13 20:52:22 +0400

    Avoiding extra byte copy inside readFileInternal
    Signed-off-by: doxlik <pirozhokkokosovich@gmail.com>

--- vertx-core/src/main/java/io/vertx/core/file/impl/FileSystemImpl.java
+++ vertx-core/src/main/java/io/vertx/core/file/impl/FileSystemImpl.java
@@ -11,16 +11,15 @@
 
 package io.vertx.core.file.impl;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.vertx.codegen.annotations.Nullable;
 import io.vertx.core.Future;
 import io.vertx.core.buffer.Buffer;
-import io.vertx.core.file.AsyncFile;
-import io.vertx.core.file.CopyOptions;
-import io.vertx.core.file.FileProps;
+import io.vertx.core.buffer.impl.BufferImpl;
+import io.vertx.core.file.*;
 import io.vertx.core.file.FileSystem;
 import io.vertx.core.file.FileSystemException;
-import io.vertx.core.file.FileSystemProps;
-import io.vertx.core.file.OpenOptions;
 import io.vertx.core.internal.ContextInternal;
 import io.vertx.core.internal.VertxInternal;
 
@@ -28,36 +27,13 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.nio.file.CopyOption;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.FileStore;
-import java.nio.file.FileVisitOption;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.LinkOption;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.StandardCopyOption;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.nio.file.attribute.FileAttribute;
-import java.nio.file.attribute.GroupPrincipal;
-import java.nio.file.attribute.PosixFileAttributeView;
-import java.nio.file.attribute.PosixFilePermission;
-import java.nio.file.attribute.PosixFilePermissions;
-import java.nio.file.attribute.UserPrincipal;
-import java.nio.file.attribute.UserPrincipalLookupService;
-import java.util.ArrayList;
-import java.util.EnumSet;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Objects;
-import java.util.Set;
+import java.nio.file.*;
+import java.nio.file.attribute.*;
+import java.util.*;
 import java.util.concurrent.Callable;
 import java.util.regex.Pattern;
 
 /**
- *
  * This class is thread-safe
  *
  * @author <a href="http://tfox.org">Tim Fox</a>
@@ -531,7 +507,7 @@ public class FileSystemImpl implements FileSystem {
             raf.setLength(len);
           }
         } catch (IOException e) {
-          throw new FileSystemException(getFileAccessErrorMessage("truncate", p) ,e);
+          throw new FileSystemException(getFileAccessErrorMessage("truncate", p), e);
         }
         return null;
       }
@@ -711,6 +687,7 @@ public class FileSystemImpl implements FileSystem {
           Files.delete(file);
           return FileVisitResult.CONTINUE;
         }
+
         public FileVisitResult postVisitDirectory(Path dir, IOException e) throws IOException {
           if (e == null) {
             Files.delete(dir);
@@ -876,7 +853,8 @@ public class FileSystemImpl implements FileSystem {
         try {
           Path target = resolveFile(path).toPath();
           byte[] bytes = Files.readAllBytes(target);
-          return Buffer.buffer(bytes);
+          ByteBuf bb = Unpooled.wrappedBuffer(bytes);
+          return new BufferImpl(bb);
         } catch (IOException e) {
           throw new FileSystemException(getFileAccessErrorMessage("read", path), e);
         }
