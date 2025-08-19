0f96503d7846a0ca73cc2099bd4729e715f91a44
Julien Viet <julien@julienviet.com>
2025-08-19 23:20:46 +0200

    Uncomment code

--- vertx-core/src/main/java/io/vertx/core/http/impl/Http2ConnectionBase.java
+++ vertx-core/src/main/java/io/vertx/core/http/impl/Http2ConnectionBase.java
@@ -142,12 +142,12 @@ abstract class Http2ConnectionBase extends ConnectionBase implements Http2FrameL
   void onStreamClosed(Http2Stream s) {
     VertxHttp2Stream stream = s.getProperty(streamKey);
     if (stream != null) {
-//      boolean active = chctx.channel().isActive();
-//      if (goAwayStatus != null) {
-//        stream.onException(new HttpClosedException(goAwayStatus));
-//      } else if (!active) {
-//        stream.onException(HttpUtils.STREAM_CLOSED_EXCEPTION);
-//      }
+      boolean active = chctx.channel().isActive();
+      if (goAwayStatus != null) {
+        stream.onException(new HttpClosedException(goAwayStatus));
+      } else if (!active) {
+        stream.onException(HttpUtils.STREAM_CLOSED_EXCEPTION);
+      }
       stream.onClose();
     }
   }
