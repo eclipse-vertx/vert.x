51e403bf901fb088bfecdd005ee617e56a8694cb
kunaljani1100 <168258319+kunaljani1100@users.noreply.github.com>
2025-09-14 13:15:35 -0700

    Remove rewritten import.

--- vertx-core/src/test/java/io/vertx/tests/future/FutureTest.java
+++ vertx-core/src/test/java/io/vertx/tests/future/FutureTest.java
@@ -21,6 +21,10 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
