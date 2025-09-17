109f8acdbb87c18e054153ed5e4fdc75723904be
Julien Viet <julien@julienviet.com>
2025-09-17 13:29:12 +0200

    Revert "Exclude test on windows that seems to hang forever"
    This reverts commit 4aefe4cd3d122b7caa05d21de6a309a7d0470485.

--- src/test/java/io/vertx/core/VirtualThreadDeploymentTest.java
+++ src/test/java/io/vertx/core/VirtualThreadDeploymentTest.java
@@ -13,7 +13,6 @@ package io.vertx.tests.deployment;
 import io.vertx.core.*;
 import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.*;
-import io.vertx.core.impl.Utils;
 import io.vertx.core.impl.VertxInternal;
 import io.vertx.test.core.VertxTestBase;
 import junit.framework.AssertionFailedError;
@@ -72,7 +71,7 @@ public class VirtualThreadDeploymentTest extends VertxTestBase {
 
   @Test
   public void testExecuteBlocking() {
-    Assume.assumeTrue(isVirtualThreadAvailable() && !Utils.isWindows());
+    Assume.assumeTrue(isVirtualThreadAvailable());
     vertx.deployVerticle(new AbstractVerticle() {
       @Override
       public void start() {
