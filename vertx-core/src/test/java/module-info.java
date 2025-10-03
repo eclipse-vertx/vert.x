36220782e4d9679e1c3d2194394513015fbac4a5
Julien Viet <julien@julienviet.com>
2025-10-03 16:19:46 +0200

    Make quic module optional in test module-info

--- vertx-core/src/test/java/module-info.java
+++ vertx-core/src/test/java/module-info.java
@@ -37,8 +37,8 @@ open module io.vertx.core.tests {
   requires io.netty.codec.http;
   requires static io.netty.codec.haproxy;
   requires io.netty.codec.http2;
-  requires io.netty.codec.classes.quic;
-  requires io.netty.codec.quic;
+  requires static io.netty.codec.classes.quic;
+  requires static io.netty.codec.quic;
   requires io.netty.resolver.dns;
   requires io.netty.handler.proxy;
   requires io.netty.codec.http3;
