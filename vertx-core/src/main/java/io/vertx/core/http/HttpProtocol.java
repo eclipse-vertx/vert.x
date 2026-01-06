c8eee01239ff87a248a6708ecc04e6488debc80f
Julien Viet <julien@julienviet.com>
2026-01-06 16:47:21 +0100

    Minor

--- vertx-core/src/main/java/io/vertx/core/http/HttpProtocol.java
+++ vertx-core/src/main/java/io/vertx/core/http/HttpProtocol.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-20256Contributors to the Eclipse Foundation
+ * Copyright (c) 2011-2026 Contributors to the Eclipse Foundation
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
--- vertx-core/src/main/java/io/vertx/core/http/impl/HttpParser.java
+++ vertx-core/src/main/java/io/vertx/core/http/impl/HttpParser.java
@@ -1,7 +1,21 @@
+/*
+ * Copyright (c) 2011-2026 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
 package io.vertx.core.http.impl;
 
 import static io.vertx.core.net.impl.Rfc5234Parser.*;
 
+/**
+ * Parser for HTTP as defined by <a href="https://www.rfc-editor.org/rfc/rfc9110.html#name-common-rules-for-defining-f">RFC 9110
+ * HTTP Semantics</a>
+ */
 public class HttpParser {
 
   public static int parseQuotedString(String s, int from, int to) {
