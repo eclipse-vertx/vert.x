448f7e5b7aacc8df8cdc7ec466760bc5382293a1
Lazar Bulic <lazarbulic@gmail.com>
2026-01-13 10:38:09 +0100

    Merge with master

--- vertx-core/src/test/java/io/vertx/tests/endpoint/DnsResolverTest.java
+++ vertx-core/src/test/java/io/vertx/tests/endpoint/DnsResolverTest.java
@@ -2,20 +2,16 @@ package io.vertx.tests.endpoint;
 
 import io.netty.handler.codec.dns.DnsRecord;
 import io.netty.handler.codec.dns.DnsRecordType;
-import io.vertx.core.Future;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.dns.AddressResolverOptions;
-import io.vertx.core.internal.VertxInternal;
-import io.vertx.core.net.SocketAddress;
-import io.vertx.core.spi.endpoint.EndpointBuilder;
-import io.vertx.core.spi.endpoint.EndpointResolver;
 import io.vertx.test.core.TestUtils;
 import io.vertx.test.core.VertxTestBase;
 import io.vertx.test.fakedns.MockDnsServer;
-import org.junit.Test;
 
 import java.net.InetSocketAddress;
-import java.util.*;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class DnsResolverTest extends VertxTestBase {
 
@@ -71,49 +67,10 @@ public class DnsResolverTest extends VertxTestBase {
       return set;
     });
     super.setUp();
-    resolver = (EndpointResolver) ((VertxInternal)vertx).nameResolver().endpointResolver(vertx);
   }
 
   public void tearDown() throws Exception {
     dnsServer.stop();
     super.tearDown();
-  }
-
-  @Test
-  public void testResolveMultipleAddresses() {
-    Future<List<SocketAddress>> fut = resolver.resolve(SocketAddress.inetSocketAddress(8080, nameToResolve), new EndpointBuilder<List<SocketAddress>, SocketAddress>() {
-      @Override
-      public EndpointBuilder<List<SocketAddress>, SocketAddress> addServer(SocketAddress server, String key) {
-        List<SocketAddress> list = new ArrayList<>();
-        list.add(server);
-        return new EndpointBuilder<>() {
-          @Override
-          public EndpointBuilder<List<SocketAddress>, SocketAddress> addServer(SocketAddress server, String key) {
-            list.add(server);
-            return this;
-          }
-          @Override
-          public List<SocketAddress> build() {
-            return list;
-          }
-        };
-      }
-
-      @Override
-      public List<SocketAddress> build() {
-        throw new IllegalStateException();
-      }
-    });
-    fut.onComplete(onSuccess(state -> {
-      assertEquals(2, state.size());
-      SocketAddress addr1 = state.get(0);
-      SocketAddress addr2 = state.get(1);
-      assertEquals("127.0.0.1", addr1.host());
-      assertEquals(8080, addr1.port());
-      assertEquals("127.0.0.2", addr2.host());
-      assertEquals(8080, addr2.port());
-      testComplete();
-    }));
-    await();
   }
 }
