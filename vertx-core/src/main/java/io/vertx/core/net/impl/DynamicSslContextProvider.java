1b17d3792067c5b9ec8427fec7d88ca04edd1436
Julien Viet <julien@julienviet.com>
2026-03-11 13:33:32 +0100

    Encapsulate server SSL options update logic in a single class for reuse purpose.

---
+++ vertx-core/src/main/java/io/vertx/core/net/impl/DynamicSslContextProvider.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2011-2019 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
+package io.vertx.core.net.impl;
+
+import io.vertx.core.Future;
+import io.vertx.core.http.ClientAuth;
+import io.vertx.core.internal.ContextInternal;
+import io.vertx.core.internal.tls.SslContextManager;
+import io.vertx.core.internal.tls.SslContextProvider;
+import io.vertx.core.net.ServerSSLOptions;
+
+/**
+ * Takes care of handling SSL options updates validating the options before
+ * applying the update.
+ *
+ * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
+ */
+public class DynamicSslContextProvider {
+
+  private final SslContextManager sslContextManager;
+  private volatile SslContextProvider sslContextProvider;
+  private Future<Boolean> updateInProgress;
+
+  public DynamicSslContextProvider(SslContextManager sslContextManager) {
+    this.sslContextManager = sslContextManager;
+  }
+
+  public SslContextProvider provider() {
+    return sslContextProvider;
+  }
+
+  public Future<Boolean> update(ServerSSLOptions options, ContextInternal ctx) {
+    return update(options, ctx, false);
+  }
+
+  /**
+   * Apply an {@code update} to the SSL context, the update will be applied after updates in progress have completed.
+   *
+   * @param update the update
+   * @param ctx the vertx context
+   * @param force force the update when options are equals
+   * @return a future signaling the update success
+   */
+  public Future<Boolean> update(ServerSSLOptions update, ContextInternal ctx, boolean force) {
+    Future<Boolean> fut;
+    synchronized (this) {
+      if (updateInProgress == null) {
+        fut = computeUpdate(update, ctx, force);
+      } else {
+        fut = updateInProgress.mapEmpty().transform(ar -> computeUpdate(update, ctx, force));
+      }
+      updateInProgress = fut;
+    }
+    return fut;
+  }
+
+  private Future<Boolean> computeUpdate(ServerSSLOptions options, ContextInternal ctx, boolean force) {
+    ServerSSLOptions sslOptions = options.copy();
+    ClientAuth clientAuth = sslOptions.getClientAuth();
+    if (clientAuth == null) {
+      clientAuth = ClientAuth.NONE;
+    }
+    Future<SslContextProvider> res = sslContextManager.resolveSslContextProvider(
+      sslOptions,
+      null,
+      clientAuth,
+      force,
+      ctx);
+    return res.map(update -> {
+      boolean updated;
+      synchronized (DynamicSslContextProvider.this) {
+        updated = sslContextProvider != update;
+        sslContextProvider = update;
+      }
+      return updated;
+    });
+  }
+}
