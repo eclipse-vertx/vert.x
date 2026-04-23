53650c4f61bb56e22ff083f9340bef0150fd0d19
Julien Viet <julien@julienviet.com>
2026-04-23 12:11:09 +0200

    Remove reflection for virtual threads.
    Motivation:
    
    Vert.x relies on reflection to support virtual thread with a Java 11 build.
    
    Recently multi-release jar support has been added to support Jackson V3.
    
    We can reuse this structure to handle virtual thread support as well.
    
    Changes:
    
    Replace reflective code by two implementations of the same class with different behaviors in JDK 11/21.

---
+++ vertx-core/src/main/java/io/vertx/core/impl/VirtualThreadSupport.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2011-2026 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
+package io.vertx.core.impl;
+
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * Pre Java 21 implementation : no virtual threads.
+ *
+ * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
+ */
+public class VirtualThreadSupport {
+
+  public static final ThreadFactory VIRTUAL_THREAD_FACTORY;
+  public static final boolean VIRTUAL_THREAD_AVAILABLE;
+
+  static {
+    VIRTUAL_THREAD_FACTORY = null;
+    VIRTUAL_THREAD_AVAILABLE = false;
+  }
+
+  /**
+   * @return whether the {@code thread} is virtual
+   */
+  public static boolean isVirtual(Thread thread) {
+    return false;
+  }
+}
