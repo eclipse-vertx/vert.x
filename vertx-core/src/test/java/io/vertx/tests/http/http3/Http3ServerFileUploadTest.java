87fa3f7584546269737f05803e2f7a3b43158a69
Thomas Segismont <tsegismont@gmail.com>
2026-05-13 16:49:57 +0200

    Intermittent failure in some Http3ServerFileUploadTest
    On my machine, takes about 40secs to complete testBrokenFormUploadLargeFileStreamToDisk and testBrokenFormUploadLargeFile.
    40secs correspond to QuicChannel timeout after the connection is closed.
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- vertx-core/src/test/java/io/vertx/tests/http/http3/Http3ServerFileUploadTest.java
+++ vertx-core/src/test/java/io/vertx/tests/http/http3/Http3ServerFileUploadTest.java
@@ -11,6 +11,8 @@
 package io.vertx.tests.http.http3;
 
 import io.vertx.tests.http.fileupload.HttpServerFileUploadTest;
+import org.junit.Ignore;
+import org.junit.Test;
 
 /**
  */
@@ -18,5 +20,23 @@ public class Http3ServerFileUploadTest extends HttpServerFileUploadTest {
 
   public Http3ServerFileUploadTest() {
     super(new Http3Config());
+  }
+
+  @Ignore("Intermittent failure with timeout on CI")
+  @Test
+  @Override
+  public void testBrokenFormUploadLargeFileStreamToDisk() {
+    // On my machine, takes about 40secs to complete
+    // 40secs correspond to QuicChannel timeout after the connection is closed
+    super.testBrokenFormUploadLargeFileStreamToDisk();
+  }
+
+  @Ignore("Intermittent failure with timeout on CI")
+  @Test
+  @Override
+  public void testBrokenFormUploadLargeFile() {
+    // On my machine, takes about 40secs to complete
+    // 40secs correspond to QuicChannel timeout after the connection is closed
+    super.testBrokenFormUploadLargeFile();
   }
 }
