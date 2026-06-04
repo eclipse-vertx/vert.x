d6c1fdd3eab004364b0b146d893e0e7637e858de
Julien Viet <julien@julienviet.com>
2026-06-04 16:24:39 +0200

    HttpServerOptions should initialize the default compression level.
    Motivation:
    
    During the update to the new HTTP server configuration object, the HttpServerOptions lost the initialization of the compression level.
    
    Changes:
    
    Configure the default compression level in HttpServerOptions#init

---
+++ vertx-core/src/test/java/io/vertx/tests/http/HttpServerConfigTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2011-2026 Contributors to the Eclipse Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ */
+package io.vertx.tests.http;
+
+import io.netty.handler.codec.compression.CompressionOptions;
+import io.netty.handler.codec.compression.DeflateOptions;
+import io.netty.handler.codec.compression.GzipOptions;
+import io.vertx.core.http.CompressionConfig;
+import io.vertx.core.http.HttpServerConfig;
+import io.vertx.core.http.HttpServerOptions;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class HttpServerConfigTest {
+
+  @Test
+  public void testCompressionFromDefaultOptions() {
+    HttpServerConfig config = new HttpServerConfig(new HttpServerOptions().setCompressionSupported(true));
+    CompressionConfig compressionConfig = config.getCompressionConfig();
+    List<CompressionOptions> compressors = compressionConfig.getCompressors();
+    assertEquals(2, compressors.size());
+    for (CompressionOptions compressor : compressors) {
+      if (compressor instanceof GzipOptions) {
+        GzipOptions gzipCompressor = (GzipOptions) compressor;
+        assertEquals(HttpServerOptions.DEFAULT_COMPRESSION_LEVEL, gzipCompressor.compressionLevel());
+      } else if (compressor instanceof DeflateOptions) {
+        DeflateOptions deflateCompressor = (DeflateOptions) compressor;
+        assertEquals(HttpServerOptions.DEFAULT_COMPRESSION_LEVEL, deflateCompressor.compressionLevel());
+      } else {
+        fail();
+      }
+    }
+  }
+}
