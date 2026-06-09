e738078202b55101f4e05be745b991008dcbe04d
Julien Viet <julien@julienviet.com>
2026-06-09 15:32:08 +0200

    Improve io.vertx.it.networklogging.NetTest : contains a race + modernize it.

--- vertx-core/src/test/java/io/vertx/it/networklogging/NetTest.java
+++ vertx-core/src/test/java/io/vertx/it/networklogging/NetTest.java
@@ -10,38 +10,36 @@
  */
 package io.vertx.it.networklogging;
 
-import io.vertx.core.buffer.Buffer;
 import io.vertx.core.net.NetClient;
 import io.vertx.core.net.NetClientOptions;
 import io.vertx.core.net.NetServer;
 import io.vertx.core.net.NetServerOptions;
 import io.vertx.core.net.NetSocket;
-import io.vertx.test.core.TestUtils;
-import io.vertx.test.core.VertxTestBase;
+import io.vertx.test.core.*;
 import io.vertx.test.netty.TestLoggerFactory;
 import org.junit.Test;
 
 import java.util.function.Predicate;
 
 
-public class NetTest extends VertxTestBase {
+public class NetTest extends VertxTestBase2 {
 
   @Test
-  public void testNoLogging() throws Exception {
-    testLogging(new NetServerOptions(), new NetClientOptions(), factory -> !factory.hasName("io.netty.handler.logging.LoggingHandler"));
+  public void testNoLogging(Checkpoint checkpoint) throws Exception {
+    testLogging(checkpoint, new NetServerOptions(), new NetClientOptions(), factory -> !factory.hasName("io.netty.handler.logging.LoggingHandler"));
   }
 
   @Test
-  public void testServerLogging() throws Exception {
-    testLogging(new NetServerOptions().setLogActivity(true), new NetClientOptions(), factory -> factory.hasName("io.netty.handler.logging.LoggingHandler"));
+  public void testServerLogging(Checkpoint checkpoint) throws Exception {
+    testLogging(checkpoint, new NetServerOptions().setLogActivity(true), new NetClientOptions(), factory -> factory.hasName("io.netty.handler.logging.LoggingHandler"));
   }
 
   @Test
-  public void testClientLogging() throws Exception {
-    testLogging(new NetServerOptions(), new NetClientOptions().setLogActivity(true), factory -> factory.hasName("io.netty.handler.logging.LoggingHandler"));
+  public void testClientLogging(Checkpoint checkpoint) throws Exception {
+    testLogging(checkpoint, new NetServerOptions(), new NetClientOptions().setLogActivity(true), factory -> factory.hasName("io.netty.handler.logging.LoggingHandler"));
   }
 
-  public void testLogging(NetServerOptions serverOptions, NetClientOptions clientOptions, Predicate<TestLoggerFactory> checker) throws Exception {
+  public void testLogging(Checkpoint checkpoint, NetServerOptions serverOptions, NetClientOptions clientOptions, Predicate<TestLoggerFactory> checker) throws Exception {
     NetServer server;
     NetClient client;
     server = vertx.createNetServer(serverOptions);
@@ -51,13 +49,13 @@ public class NetTest extends VertxTestBase {
         so.endHandler(v -> {
           so.end();
         });
-        so.end(Buffer.buffer("fizzbuzz"));
       });
       server.listen(0, "localhost").await();
-      NetSocket so = client.connect(server.actualPort(), "localhost").await();
-      so.closeHandler(v2 -> testComplete());
+      NetSocket so = client
+        .connect(server.actualPort(), "localhost")
+        .await();
+      so.closeHandler(v2 -> checkpoint.succeed());
       so.end().await();
-      await();
     });
   }
 }
